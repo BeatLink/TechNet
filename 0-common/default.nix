f4b6196fa1e3f1784d47c7e4e4613bd2f8cb1946
BeatLink <beatlink+git@simplelogin.co>
2024-09-09 18:57:31 -0500

    Update imports and ordering

--- 0-common/default.nix
+++ 0-common/default.nix
@@ -2,12 +2,12 @@
 { config, lib, pkgs, modulesPath, ... }: 
 {
     imports = [                                       
-        ./modules/avahi.nix
-        ./modules/fail2ban.nix
-        ./modules/locale.nix
-        ./modules/networking.nix
-        ./modules/software.nix
-        ./modules/ssh.nix
-        ./modules/users.nix
+        ./modules/0-locale.nix
+        ./modules/1-networking.nix
+        ./modules/2-software.nix
+        ./modules/3-users.nix
+        ./modules/4-ssh.nix
+        ./modules/5-avahi.nix
+        ./modules/6-fail2ban.nix
     ];
 }
