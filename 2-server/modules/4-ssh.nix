1c1ebdf94e50cd4e81a51463851dc5a577755865
BeatLink <beatlink+git@simplelogin.co>
2024-09-12 21:09:32 -0500

    Move default SSH to heimdall

---
+++ 2-server/modules/4-ssh.nix
@@ -0,0 +1,22 @@
+# Initrd SSH ##########################################################################################################################
+# Enables SSH in Boot for Remote LUKS Unlock
+#######################################################################################################################################
+{ config, lib, pkgs, ... }:{
+    boot.initrd = {
+        network.ssh = {                                             
+            port = 22;
+            enable = true;
+            authorizedKeys = [ 
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYQ9aXy5sS9PCKopaB58c8ZA/JGOEoBLMSg4a0n4aw7 beatlink@heimdall" 
+            ];
+            hostKeys = [
+                "/persistent/etc/ssh/ssh_host_ed25519_key"
+                "/persistent/etc/ssh/ssh_host_rsa_key"
+            ];
+        };
+        systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
+    };
+    users.users."beatlink".openssh.authorizedKeys.keys = [              # Sets the SSH key for the user
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYQ9aXy5sS9PCKopaB58c8ZA/JGOEoBLMSg4a0n4aw7 beatlink@heimdall"
+    ]; 
+}
