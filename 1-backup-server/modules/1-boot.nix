7b322de5d1d8f361703e3893020281f985c8f7fc
BeatLink <beatlink+git@simplelogin.co>
2024-09-12 21:14:10 -0500

    Add boot for backup-server

---
+++ 1-backup-server/modules/1-boot.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }: {
+    boot = {
+        loader = {
+            grub.enable = false;                            # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+            generic-extlinux-compatible.enable = true;      # Enables the generation of /boot/extlinux/extlinux.conf
+        };
+        initrd = {
+            availableKernelModules = [ "usbhid" ];
+            kernelModules = [ ];
+        };
+        kernelModules = [ ];
+        extraModulePackages = [ ];
+    };    
+}
