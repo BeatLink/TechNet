34b531d6e1ce0ddcd8260f97abd13a4223e2858f
BeatLink <beatlink+git@simplelogin.co>
2024-09-12 21:09:32 -0500

    Move default SSH to heimdall

--- 0-common/modules/4-ssh.nix
+++ 0-common/modules/4-ssh.nix
@@ -21,9 +21,6 @@
             }
         ];
     };
-    users.users."beatlink".openssh.authorizedKeys.keys = [              # Sets the SSH key for the user
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYQ9aXy5sS9PCKopaB58c8ZA/JGOEoBLMSg4a0n4aw7 beatlink@heimdall"
-    ]; 
     systemd.tmpfiles.rules = [                                          # Sets permissions for SSH folder 
         "d /etc 0755 root root"
         "d /etc/ssh 0755 root root"
