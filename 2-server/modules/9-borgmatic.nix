f2a05a54017098a6a5a49bf5fad8c175fad8356c
BeatLink <beatlink+git@simplelogin.co>
2024-09-15 15:53:23 -0500

    Update borgmatic

--- 2-server/modules/9-borgmatic.nix
+++ 2-server/modules/9-borgmatic.nix
@@ -5,58 +5,106 @@
 {
     services.borgmatic = {
         enable = true;
-        configurations = {
-            server-backups = {
-                location = {
-                    source_directories = [
-                        "/Storage/Services"
-                    ];
-                    exclude_patterns = [
-                        "/Storage/Files/Backups/Server"
-                    ];
-                    exclude_if_present = [
-                        ".nobackup"
-                        ".stversions"
-                        ".thumbnails"
-                    ];
-                    repositories = [
-                        "/Storage/Files/Backups/Server/Borgmatic"
-                        "ssh://beatlink@10.100.100.2/media/beatlink/Storage/Files/Backups/Server/Borgmatic"
-                    ];
-                    one_file_system = true;
+        settings = {
+            
+            # Sources ---------------------------------------------------------------------------------------------------------------------
+            source_directories = [
+                "/Storage/Services"
+                "/Storage/Scripts"
+            ];
+            exclude_patterns = [
+                "/Storage/Files/Backups/Server"
+            ];
+            exclude_if_present = [
+                ".nobackup"
+                ".stversions"
+                ".thumbnails"
+            ];
+
+            # Excludes --------------------------------------------------------------------------------------------------------------------
+            repositories = [
+                {
+                    label = "On Disk Backup"
+                    path = "/Storage/Files/Backups/Server/Borgmatic"
+                }
+                {
+                    label = "Backup Server"
+                    path = "ssh://borg@10.100.100.6/Storage/Backups/Server/Borgmatic"
+                }
+            ];
+
+            # Backup Settings -------------------------------------------------------------------------------------------------------------
+            compression = "lz4";
+            archive_name_format = "backup-{now}";
+            relocated_repo_access_is_ok = true;
+
+            # Retention -------------------------------------------------------------------------------------------------------------------
+            keep_hourly = 24;
+            keep_daily = 7;
+            keep_weekly = 4;
+            keep_monthly = 12;
+            keep_yearly = 3;
+
+            # Hooks -----------------------------------------------------------------------------------------------------------------------
+            before_backup = [
+                "echo Starting a backup job."
+                # "ping -q -c 1 10.100.100.6 > /dev/null || exit 75"
+            ];
+            after_backup = [
+                "echo Backup created."
+            ];
+            on_error = [
+                "echo Error while creating a backup."
+            ];
+
+            # Consistency Checks ----------------------------------------------------------------------------------------------------------
+            checks = [
+                {
+                    name = "repository"
+                    frequency = "always"
+                }
+                {
+                    name = "archives"
+                    frequency = "always"
+                }
+                {
+                    name = data
+                    frequency = "always"
+                }
+                {
+                    name = "extract"
+                    frequency = "always"
+                } 
+            ];
+            check_last = 3;
+
+            # Notifications ---------------------------------------------------------------------------------------------------------------
+            ntfy = {
+                topic = "borgmatic";
+                server = "http://ntfy"
+                start = {
+                    title = "Heimdall Borgmatic Backup Started";
+                    message = "Updates will follow"
+                    tags = [ "borgmatic" ]
+                    priority = "min"
                 };
-                storage = {
-                    compression = "lz4";
-                    archive_name_format = "backup-{now}";
-                    relocated_repo_access_is_ok = true;
+                finish = {
+                    title = "Heimdall Borgmatic Backup Completed";
+                    message = "Nice!"
+                    tags = ["borgmatic" "+1"]
+                    priority = ["min"]
                 };
-                retention = {
-                    keep_hourly = 1;
-                    keep_daily = 1;
-                    keep_weekly = 1;
-                    keep_monthly = 1;
-                    keep_yearly = 1;
-                    prefix = "backup-";
+                fail = {
+                    title = "Heimdall Borgmatic Backup Failed!"
+                    message = "You should probably fix it"
+                    tags = [ "borgmatic" "-1" "skull" ]
+                    priority = "max"
                 };
-                consistency = {
-                    checks = [
-                        "repository"
-                        "archives"
-                    ];
-                    check_last = 3;
-                    prefix = "backup-";
-                };
-                hooks = {
-                    before_backup = [
-                        "echo Starting a backup job."
-                    ];
-                    after_backup = [
-                        "echo Backup created."
-                    ];
-                    on_error = [
-                        "echo Error while creating a backup."
-                    ];
-                };
+                states = [
+                    "start"
+                    "finish"
+                    "fail"
+                ];
             };
         };
     };
