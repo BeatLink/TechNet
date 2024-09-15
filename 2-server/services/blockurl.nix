210a8994f21094a2afff1687d18a102c7bcbe133
BeatLink <beatlink+git@simplelogin.co>
2024-09-15 15:55:12 -0500

    Migrate blockurl to arion

--- 2-server/services/blockurl.nix
+++ 2-server/services/blockurl.nix
@@ -1,3 +1,5 @@
+# https://hub.docker.com/repository/docker/beatlink/blockurl
+# https://github.com/BeatLink/BlockURL
 
 { config, lib, pkgs, modulesPath, ... }: 
 {
@@ -7,10 +9,10 @@
             serviceName = "blockurl";
             settings = {
                 services = {
-                    blockurl = {
-                        image = "mariadb:latest";
-                        container_name = "blockurl"
-                        restart = "unless-stopped"
+                    blockurl.service = {
+                        image = "beatlink/blockurl:latest";
+                        container_name = "blockurl";
+                        restart = "unless-stopped";
                         volumes = [ 
                             "/Storage/Services/BlockURL/database:/app/database"
                         ];
