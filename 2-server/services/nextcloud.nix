4116555033d8a75ddf78df4ee0bf73eb0663f10d
BeatLink <beatlink+git@simplelogin.co>
2024-09-15 15:55:24 -0500

    add nextcloud service

---
+++ 2-server/services/nextcloud.nix
@@ -0,0 +1,31 @@
+# NextCloud ###############################################################################################################################
+#
+# NextCloud is the Personal Information Manager (PIM) hub of the TechNet. 
+# 
+# Features
+#   - Contacts
+#   - Calendar
+#   - Tasks
+#   - Phone Tracking
+#   - Files
+#
+###########################################################################################################################################
+
+{ config, lib, pkgs, modulesPath, ... }: 
+{
+    virtualisation.arion = {
+        backend = "docker";
+        projects.nextcloud = {
+            serviceName = "nextcloud";
+            settings = {
+                services = {
+                    db.services = {
+                        image = "mariadb:latest";
+                        container_name = ""
+                    }
+                }
+                imports = [ ./arion-compose.nix ];
+            };
+        };
+    };
+}
