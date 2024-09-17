0f0604afd6f29ddc8fa1f0d03a7316df28d67381
BeatLink <beatlink+git@simplelogin.co>
2024-09-16 19:31:21 -0500

    Update Blockurl

--- 2-server/services/blockurl.nix
+++ 2-server/services/blockurl.nix
@@ -3,31 +3,28 @@
 
 { config, lib, pkgs, modulesPath, ... }: 
 {
-    virtualisation.arion = {
-        backend = "docker";
-        projects.blockurl = {
-            serviceName = "blockurl";
-            settings = {
-                services = {
-                    blockurl.service = {
-                        image = "beatlink/blockurl:latest";
-                        container_name = "blockurl";
-                        restart = "unless-stopped";
-                        volumes = [ 
-                            "/Storage/Services/BlockURL/database:/app/database"
-                        ];
-                        expose = [
-                            "80" 
-                        ];
-                        networks = [
-                            "nginx_public"
-                        ];
-                    };
+    virtualisation.arion.projects.blockurl = {
+        serviceName = "blockurl";
+        settings = {
+            services = {
+                blockurl.service = {
+                    image = "beatlink/blockurl:latest";
+                    container_name = "blockurl";
+                    restart = "unless-stopped";
+                    volumes = [ 
+                        "/Storage/Services/BlockURL/database:/app/database"
+                    ];
+                    expose = [
+                        "80" 
+                    ];
+                    networks = [
+                        "nginx_public"
+                    ];
                 };
-                networks = {
-                    nginx_public = {
-                        external = true;
-                    };
+            };
+            networks = {
+                nginx_public = {
+                    external = true;
                 };
             };
         };
