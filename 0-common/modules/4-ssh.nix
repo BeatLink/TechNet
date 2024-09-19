84eec6538420f6aebe9227846c23acf16ba5d1a2
BeatLink <beatlink+git@simplelogin.co>
2024-09-19 18:15:46 -0500

    Update Common SSH

--- 0-common/modules/4-ssh.nix
+++ 0-common/modules/4-ssh.nix
@@ -1,29 +1,29 @@
-# SSH ---------------------------------------------------------------------------------------------------------------------------------
+# SSH #####################################################################################################################################
+#
+# SSH will be the primary way to remotely access devices.
+#
+# It will provide the following services:  
+#   - Remote Access  
+#   - File Access through SFTP
+#
+###########################################################################################################################################
+
 { config, lib, pkgs, modulesPath, ... }: 
 {
     services.openssh = {
         enable = true;                                                  # Enable the OpenSSH daemon.
         allowSFTP = true;                                               # Allows file transfers over SSH
         settings = {
-            PasswordAuthentication = true;                             # Enables password based login
-            KbdInteractiveAuthentication = true;                       # Enables keyboard based login
-            PermitRootLogin = "no";                                    # Disables root login
+            PasswordAuthentication = false;                             # Disables password based login
+            KbdInteractiveAuthentication = false;                       # Disables keyboard based login
+            PermitRootLogin = "no";                                     # Disables root login
         };
-        hostKeys = [
-            { 
-                type = "ed25519"; 
-                path = "/persistent/etc/ssh/ssh_host_ed25519_key"; 
-            }
-            { 
-                type = "rsa"; 
-                bits = 4096; 
-                path = "/persistent/etc/ssh/ssh_host_rsa_key"; 
-            }
-        ];
+        extraConfig = [
+            "MaxStartups 1"
+        ]
     };
     systemd.tmpfiles.rules = [                                          # Sets permissions for SSH folder 
         "d /etc 0755 root root"
         "d /etc/ssh 0755 root root"
     ];
-    networking.firewall.allowedTCPPorts = [ 22 ];                       # Enables SSH in the firewall
 }
