f014962e593563b69cf98ea5a54ccfa2b80a7e85
BeatLink <beatlink+git@simplelogin.co>
2024-09-19 20:24:47 -0500

    Update Backup Server Imports

--- 1-backup-server/default.nix
+++ 1-backup-server/default.nix
@@ -3,6 +3,10 @@
     imports = [
         ./modules/0-hardware-configuration.nix
         ./modules/1-boot.nix
-        ./modules/2-filesystem.nix
+        ./modules/2-networking.nix
+        ./modules/3-ssh.nix
+        ./modules/4-software.nix
+        ./modules/5-backup-drive.nix
+        ./modules/6-borg.nix
     ];
 }
