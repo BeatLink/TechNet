5cc93693d5747ae7a10957a583b8b1e5f073cbec
BeatLink <beatlink+git@simplelogin.co>
2024-09-19 20:01:41 -0500

    Update Boot

--- 1-backup-server/modules/1-boot.nix
+++ 1-backup-server/modules/1-boot.nix
@@ -1,15 +1,7 @@
 # Boot ####################################################################################################################################
 { config, lib, pkgs, ... }: {
-    boot = {
-        loader = {
-            grub.enable = false;                            # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-            generic-extlinux-compatible.enable = true;      # Enables the generation of /boot/extlinux/extlinux.conf
-        };
-        initrd = {
-            availableKernelModules = [ "usbhid" ];
-            kernelModules = [ ];
-        };
-        kernelModules = [ ];
-        extraModulePackages = [ ];
+    boot.loader = {
+        grub.enable = false;                            # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+        generic-extlinux-compatible.enable = true;      # Enables the generation of /boot/extlinux/extlinux.conf
     };    
 }
