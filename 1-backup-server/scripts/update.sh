7245166fb629a6877f9a3fa5fd417267204ecc2b
BeatLink <beatlink+git@simplelogin.co>
2024-09-20 07:28:11 -0500

    Move Scripts

--- 1-backup-server/scripts/update.sh
+++ 1-backup-server/scripts/update.sh
@@ -3,7 +3,7 @@
 # This scripts automatically updates Ragnarok from this flake
 #
 nixos-rebuild \
-    --flake ../#Ragnarok \
+    --flake ../../#Ragnarok \
     --build-host "pearson@10.100.100.5" \
     --target-host "pearson@10.100.100.5" \
     --use-remote-sudo  \
