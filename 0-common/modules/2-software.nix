c637f1bb3e7a7cb855e16abc6d543494338823f2
BeatLink <beatlink+git@simplelogin.co>
2024-09-19 20:19:09 -0500

    Update Common Software

--- 0-common/modules/2-software.nix
+++ 0-common/modules/2-software.nix
@@ -1,7 +1,16 @@
 # Software ----------------------------------------------------------------------------------------------------------------------------
 { config, lib, pkgs, modulesPath, ... }: 
 {
+    nix = {                                                             # Enables Flakes
+        package = pkgs.nixFlakes;
+        extraOptions = ''experimental-features = nix-command flakes'';
+        settings.trusted-users = [ "root" "beatlink" ];                 # Allows me to remote update by sending the flake over ssh
+    };
     nixpkgs.config.allowUnfree = true;                                  # Allow unfree packages
+    system.autoUpgrade = {
+        enable = true;
+        allowReboot = true;
+    };
     environment.systemPackages = with pkgs; [                           # Set packages installed on system
         wget                                                            # For downloading stuff
         curl                                                            # Also for downloading stuff
