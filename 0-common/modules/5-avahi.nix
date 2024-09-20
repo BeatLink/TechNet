aa62416fef8273da559de41795c150b994fc39c6
BeatLink <beatlink+git@simplelogin.co>
2024-09-19 19:51:10 -0500

    Remove redundant avahi firewall

--- 0-common/modules/5-avahi.nix
+++ 0-common/modules/5-avahi.nix
@@ -9,5 +9,4 @@
             workstation = true;                                         # Marks this device as a PC (Set to false for servers)
         };
     };
-    networking.firewall.allowedUDPPorts = [ 5353 ];                     # Enables Avahi port
 }
