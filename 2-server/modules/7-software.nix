ee49171c2714cb2735ab2f51496d245586d63b28
BeatLink <beatlink+git@simplelogin.co>
2024-09-19 20:22:42 -0500

    Move flakes to common software

--- 2-server/modules/7-software.nix
+++ 2-server/modules/7-software.nix
@@ -3,11 +3,6 @@
 #######################################################################################################################################                                                   
 { config, lib, pkgs, modulesPath, ... }: 
 {
-    nix = {                                                             # Enables Flakes
-        package = pkgs.nixFlakes;
-        extraOptions = ''experimental-features = nix-command flakes'';
-        settings.trusted-users = [ "root" "beatlink" ];                 # Allows me to remote update by sending the flake over ssh
-    };
     system.stateVersion = "23.11";                                      # Sets the base version. Don't change unless reinstalling everything
     system.autoUpgrade.allowReboot = lib.mkForce  false;                # Prevents rebooting since LUKS manual decryption is needed
     environment.systemPackages = with pkgs; [                           # Set packages installed on system
