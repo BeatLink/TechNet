f4187023b66c2923089bdf508b8396f6f9a2313c
BeatLink <beatlink+git@simplelogin.co>
2024-09-21 11:28:35 -0500

    Update Networking

--- 0-common/modules/1-networking.nix
+++ 0-common/modules/1-networking.nix
@@ -7,7 +7,6 @@
 { config, lib, pkgs, modulesPath, ... }: 
 {
     networking = {
-        domain = "TechNet";                                             # Sets the domain name
         firewall = {
             enable = true;                                              # Enable the Firewall
             logRefusedConnections = true;                               # Records any refused connections
