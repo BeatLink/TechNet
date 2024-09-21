360453535005a100933dcd141125bae460863347
BeatLink <beatlink+git@simplelogin.co>
2024-09-21 11:30:06 -0500

    Move scripts and secrets

--- 2-server/update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# This scripts automatically updates heimdall from this flake
-#
-nixos-rebuild switch --use-remote-sudo --flake ../#Heimdall --target-host "beatlink@192.168.0.2"
-
