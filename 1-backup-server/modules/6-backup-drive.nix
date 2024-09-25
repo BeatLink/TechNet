e869f7c58d327c2338882bf75644d1b5a7545391
BeatLink <beatlink+git@simplelogin.co>
2024-09-24 21:29:12 -0500

    Fix Backup Drive Permissions

--- 1-backup-server/modules/6-backup-drive.nix
+++ 1-backup-server/modules/6-backup-drive.nix
@@ -6,9 +6,16 @@
 
 { config, lib, pkgs, ... }: 
 {
-    systemd.tmpfiles.rules = [                                                      # Sets the mount point permissions
-        "d /Storage 1770 borg borg"
-    ];
+
+    systemd.tmpfiles.settings."Backup-Drive" = {                      # Sets the mount point permissions
+        "/Storage" = {
+            Z = {
+                user = "borg";
+                group = "borg";
+                mode = "0770";
+            };
+        };
+    };
     fileSystems."/Storage" = {                                                      # Mounts the drive
         device = "/dev/disk/by-uuid/4c6831d6-0d99-4d5a-805f-88d7878d6c4b";
         fsType = "ext4";
