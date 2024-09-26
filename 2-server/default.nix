f9284ce5b0891512d941a03c1bf0e30f91020ec5
BeatLink <beatlink+git@simplelogin.co>
2024-09-25 22:48:48 -0500

    Use default for imports

--- 2-server/default.nix
+++ 2-server/default.nix
@@ -4,17 +4,8 @@
 
 { config, lib, pkgs, ... }:
 {
-    imports = [                                   
-        ./modules/0-hardware-configuration.nix
-        ./modules/1-disk-config.nix
-        ./modules/2-boot.nix
-        ./modules/3-networking.nix
-        ./modules/4-ssh.nix
-        ./modules/5-impermanence.nix
-        ./modules/6-data-drive.nix
-        ./modules/7-software.nix
-        ./modules/8-docker.nix
-        ./modules/9-borgmatic.nix
-        ./modules/10-services.nix
+    imports = [                         
+      ./modules
+      ./services
     ];
 }
