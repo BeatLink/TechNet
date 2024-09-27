7be509137cff1d715c9935d213e638b70327ae2f
BeatLink <beatlink+git@simplelogin.co>
2024-09-27 18:03:36 -0500

    Setup auditd for security boost

---
+++ 0-common/modules/7-auditd.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, modulesPath, ... }: 
+{
+    security.auditd.enable = true;
+    security.audit.enable = true;
+    security.audit.rules = [
+        "-a exit,always -F arch=b64 -S execve"
+    ];
+}
