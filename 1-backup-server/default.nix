7c5ad3acb773146c7a9d3a43705c7c9b87387bac
BeatLink <beatlink+git@simplelogin.co>
2024-09-28 17:35:39 -0500

    add default.nix

--- 1-backup-server/default.nix
+++ 1-backup-server/default.nix
@@ -7,13 +7,6 @@
 { config, lib, pkgs, modulesPath, ... }: 
 {
     imports = [
-        (modulesPath + "/installer/scan/not-detected.nix")
-        ./modules/0-sd-image.nix
-        ./modules/1-boot.nix
-        ./modules/3-software.nix
-        ./modules/4-networking.nix
-        ./modules/5-ssh.nix
-        ./modules/6-backup-drive.nix
-        ./modules/7-borg.nix
+        ./modules
     ];
 }
