1ed130eee3d2922629ddd1df1a9331bf0248647a
BeatLink <beatlink+git@simplelogin.co>
2024-09-29 15:23:54 -0500

    remove extra line

--- 2-server/modules/7-software.nix
+++ 2-server/modules/7-software.nix
@@ -3,11 +3,11 @@
 #######################################################################################################################################                                                   
 { config, lib, pkgs, modulesPath, ... }: 
 {
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     system.stateVersion = "23.11";                                      # Sets the base version. Don't change unless reinstalling everything
     system.autoUpgrade.allowReboot = lib.mkForce  false;                # Prevents rebooting since LUKS manual decryption is needed
     environment.systemPackages = with pkgs; [                           # Set packages installed on system
         arion
         docker-client
     ];    
-
 }
