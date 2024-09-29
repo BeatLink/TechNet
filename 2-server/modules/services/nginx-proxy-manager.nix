15e4f840e70dc877a3243128fabd15005fd984fc
BeatLink <beatlink+git@simplelogin.co>
2024-09-29 17:25:08 -0500

    Add nginx proxy manager

---
+++ 2-server/modules/services/nginx-proxy-manager.nix
@@ -0,0 +1,45 @@
+# Nginx Proxy Manager #####################################################################################################################
+#
+# This is the reverse proxy for all my docker services.
+#
+###########################################################################################################################################
+{ config, lib, pkgs, modulesPath, ... }: 
+{
+    virtualisation.arion.projects.nginx-proxy-manager = {
+        serviceName = "nginx-proxy-manager";
+        settings = {
+            services = {
+                app.service = {
+                    image = "jc21/nginx-proxy-manager:latest";
+                    container_name = "nginx-proxy-manager";
+                    restart = "always";
+                    environment = {
+                        "DISABLE_IPV6" = "true";
+                        "X_FRAME_OPTIONS" = "sameorigin";
+                        "ENABLE_DNSMASQ" = "true";
+                    };
+                    volumes = [ 
+                        "/Storage/Services/Nginx-Proxy-Manager/data:/data"
+                        "/Storage/Services/Nginx-Proxy-Manager/letsencrypt:/etc/letsencrypt"
+                    ];
+                    ports = [
+                        "80:80"
+                        "443:443"
+                        "81:81"
+                    ];
+                    networks = [
+                        "public"
+                    ];
+                    extra_hosts = [
+                        "host.docker.internal:host-gateway"
+                    ]
+                };
+            };
+            networks = {
+                public = {
+                    driver = "bridge";
+                };
+            };
+        };
+    };
+}
