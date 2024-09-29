727e96afb61b089f44bba95b98e609c5bbbf06b1
BeatLink <beatlink+git@simplelogin.co>
2024-09-29 18:53:28 -0500

    Add PiHole

---
+++ 2-server/modules/services/pihole.nix
@@ -0,0 +1,61 @@
+# Pi-Hole #################################################################################################################################
+#
+# Pi-Hole acts as an ad-blocking system for the TechNet. More importantly, it also acts as the DNS server for all docker services and hosts
+# in the TechNet, allowing them to be accessed via the .technet domain
+#
+# More info at https://github.com/pi-hole/docker-pi-hole/ and https://docs.pi-hole.net/
+#
+# Links:
+#     - https://github.com/pi-hole/pi-hole/
+# 
+# Device List  
+#     - Heimdall - heimdall.technet  
+#     - Odin - odin.technet
+#     - Hela - hela.technet  
+#     - Thor - thor.technet
+#     - Ragnarok - ragnarok.technet
+#
+###########################################################################################################################################
+
+{ config, lib, pkgs, modulesPath, ... }: 
+{
+    virtualisation.arion.projects.pihole = {
+        serviceName = "pihole";
+        settings = {
+            services = {
+                pihole.service = {
+                    image = "pihole/pihole:latest";
+                    container_name = "pihole";
+                    restart = "always";
+                    volumes = [ 
+                        "/Storage/Services/PiHole/etc-pihole:/etc/pihole"
+                        "/Storage/Services/PiHole/etc-dnsmasq.d:/etc/dnsmasq.d"
+                    ];
+                    env_file = [
+                        "/Storage/Services/PiHole/.env"
+                    ];
+                    environment = {
+                        "TZ" = "America/Jamaica";
+                        "WEB_PORT" = "80";
+                        "FTLCONF_LOCAL_IPV4" = "10.100.100.1";
+                    };
+                    expose = [
+                        "80" 
+                    ];
+                    ports = [
+                      "53:53/tcp"
+                      "53:53/udp"
+                    ];
+                    networks = [
+                        "nginx-proxy-manager_public"
+                    ];
+                };
+            };
+            networks = {
+                nginx-proxy-manager_public = {
+                    external = true;
+                };
+            };
+        };
+    };
+}
