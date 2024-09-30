b49e5b05eff173279e17e181bf7490eb1d0d033d
BeatLink <beatlink+git@simplelogin.co>
2024-09-29 19:18:55 -0500

    Add Traccar

---
+++ 2-server/modules/services/traccar.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, modulesPath, ... }: 
+{
+    virtualisation.arion.projects.traccar = {
+        serviceName = "traccar";
+        settings = {
+            services = {
+                traccar.service = {
+                    image = "traccar/traccar:latest";
+                    container_name = "traccar";
+                    restart = "always";
+                    volumes = [ 
+                        "/Storage/Services/Traccar/logs:/opt/traccar/logs:rw"
+                        "/Storage/Services/Traccar/traccar.xml:/opt/traccar/conf/traccar.xml:ro"
+                        "/Storage/Services/Traccar/data:/opt/traccar/data:rw"
+                    ];
+                    env_file = [
+                        "/Storage/Services/PiHole/.env"
+                    ];
+                    environment = {
+                        "PUID" = "1000";
+                        "PGID" = "1000";
+                        "APP_BASE_URL" = "syncthing.heimdall.technet";
+                    };
+                    expose = [
+                        "8082" 
+                    ];
+                    ports = [
+                      "5013:5013"
+                      "5013:5013/udp"
+                    ];
+                    networks = [
+                        "nginx-proxy-manager_public"
+                    ];
+                };
+            };
+            networks = {
+                nginx-proxy-manager_public = {
+                    external = true;
+                };
+            };
+        };
+    };
+}
