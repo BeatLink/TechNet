a02aaf14a0427ad233b9891735a838fd241db1eb
BeatLink <beatlink+git@simplelogin.co>
2024-09-30 22:28:52 -0500

    Update update script

--- 2-server/scripts/update.sh
+++ 2-server/scripts/update.sh
@@ -2,5 +2,9 @@
 #
 # This scripts automatically updates heimdall from this flake
 #
-nixos-rebuild switch --use-remote-sudo --flake ../#Heimdall --target-host "beatlink@192.168.0.2"
-
+nixos-rebuild \
+    --flake ../../#Heimdall \
+    --target-host "beatlink@192.168.0.2" \
+    --build-host "beatlink@192.168.0.2" \
+    --use-remote-sudo \
+    switch 
