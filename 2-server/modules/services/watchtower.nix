2df4b3196a75807322b3af1c12ee9fd7c5c6ddff
BeatLink <beatlink+git@simplelogin.co>
2024-10-01 07:21:38 -0500

    add options for watchtower

--- 2-server/modules/services/watchtower.nix
+++ 2-server/modules/services/watchtower.nix
@@ -9,8 +9,25 @@
                     container_name = "watchtower";
                     restart = "always";
                     volumes = [ 
-                        "/var/run/docker.sock:/var/run/docker.sock"
+                      "/var/run/docker.sock:/var/run/docker.sock"
+                    ];    
+                    env_file = [
+                      "/Storage/Services/Watchtower/.env"
                     ];
+                    command = [
+                      "--interval" "1800"  "--http-api-update" "--http-api-periodic-polls" "--http-api-metrics"
+                    ];
+                    expose = [
+                      "8080"
+                    ];
+                    networks = [
+                      "nginx-proxy-manager_public"
+                    ];
+                };
+            };
+            networks = {
+                nginx-proxy-manager_public = {
+                    external = true;
                 };
             };
         };
