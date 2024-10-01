a980e8f5d7c64602e7756cad43163e7bbc047e7f
BeatLink <beatlink+git@simplelogin.co>
2024-09-30 22:00:00 -0500

    add HDDtemp for glances

--- 0-common/modules/7-utilities.nix
+++ 0-common/modules/7-utilities.nix
@@ -2,6 +2,14 @@
 {
     hardware.sensor.hddtemp = {
         enable = true;
-        drives = ["/dev/disk/by-path/*"];
+        drives = [
+            "/dev/sda"
+            "/dev/sdb"
+            "/dev/sdc"
+        ];
+        dbEntries = [
+            "\"TOSHIBA MQ04ABF100\" 194 C \"TOSHIBA MQ04ABF100\""
+            "\"WDC WDS120G2G0B-00EPW0\" 194 C \"WDC WDS120G2G0B-00EPW0\""
+        ];
     };
 }
