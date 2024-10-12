d2d548b8944bf992597e84c595e513aca1507bc9
BeatLink <beatlink+git@simplelogin.co>
2024-10-12 11:50:40 -0500

    Add Patch

--- 1-backup-server/modules/0-sd-image.nix
+++ 1-backup-server/modules/0-sd-image.nix
@@ -1,4 +1,10 @@
 { config, lib, pkgs, ... }: 
+let 
+    uboot = pkgs.ubootRock64v2.override {
+        version = "2024.10";
+        extraPatches = [./1-boot-fix-uboot.patch];
+    };
+in
 {
     sdImage.populateRootCommands = ''
         ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c \${config.system.build.toplevel} -d ./files/boot
@@ -8,8 +14,8 @@
     sdImage.firmwarePartitionOffset = 30;
     sdImage.firmwareSize = 1;
     sdImage.postBuildCommands = ''
-        dd if=\${pkgs.ubootRock64v2.override {extraPatches = [./1-boot-fix-uboot-initrd.patch];}}/idbloader.img of=$img  conv=fsync,notrunc bs=512 seek=64;
-        dd if=\${pkgs.ubootRock64v2.override {extraPatches = [./1-boot-fix-uboot-initrd.patch];}}/u-boot.itb of=$img  conv=fsync,notrunc bs=512 seek=16384;
+        dd if=\${uboot}/idbloader.img of=$img  conv=fsync,notrunc bs=512 seek=64;
+        dd if=\${uboot}}/u-boot.itb of=$img  conv=fsync,notrunc bs=512 seek=16384;
         sync;
     '';
     fileSystems."/boot/firmware" = {};
