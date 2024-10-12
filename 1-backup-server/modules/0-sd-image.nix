c3b9d6114ef1c430e48492520aac8309e0067dcf
BeatLink <beatlink+git@simplelogin.co>
2024-10-12 13:55:09 -0500

    Fix SD Image

--- 1-backup-server/modules/0-sd-image.nix
+++ 1-backup-server/modules/0-sd-image.nix
@@ -2,6 +2,10 @@
 let 
     uboot = pkgs.ubootRock64v2.override {
         version = "2024.10";
+        src = pkgs.fetchurl {
+            url = "https://ftp.denx.de/pub/u-boot/u-boot-2024.10.tar.bz2";
+            hash = "sha256-so2vSsF+QxVjYweL9RApdYQTf231D87ZsS3zT2GpL7A=";
+        };
         extraPatches = [./1-boot-fix-uboot.patch];
     };
 in
@@ -15,7 +19,7 @@ in
     sdImage.firmwareSize = 1;
     sdImage.postBuildCommands = ''
         dd if=\${uboot}/idbloader.img of=$img  conv=fsync,notrunc bs=512 seek=64;
-        dd if=\${uboot}}/u-boot.itb of=$img  conv=fsync,notrunc bs=512 seek=16384;
+        dd if=\${uboot}/u-boot.itb of=$img  conv=fsync,notrunc bs=512 seek=16384;
         sync;
     '';
     fileSystems."/boot/firmware" = {};
