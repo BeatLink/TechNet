7964fe056f49eb3e598646b8acfc27547b749e96
BeatLink <beatlink+git@simplelogin.co>
2024-11-01 19:51:25 -0500

    Move server settings to folder

--- 2-server/modules/system/7-software.nix
+++ 2-server/modules/system/7-software.nix
@@ -3,7 +3,6 @@
 #######################################################################################################################################                                                   
 { config, lib, pkgs, modulesPath, ... }: 
 {
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     system.stateVersion = "23.11";                                      # Sets the base version. Don't change unless reinstalling everything
     system.autoUpgrade.allowReboot = lib.mkForce  false;                # Prevents rebooting since LUKS manual decryption is needed
     environment.systemPackages = with pkgs; [                           # Set packages installed on system
