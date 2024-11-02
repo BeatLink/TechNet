cda2f57d32923068867fe7ea94eb7b82c8257838
BeatLink <beatlink+git@simplelogin.co>
2024-11-01 19:51:25 -0500

    Move server settings to folder

--- 2-server/modules/system/0-hardware-configuration.nix
+++ 2-server/modules/system/0-hardware-configuration.nix
@@ -1,7 +1,7 @@
-
 { config, lib, pkgs, modulesPath, ... }:
-
 {
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.kernelModules = [ "kvm-intel" ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- 2-server/modules/system/default.nix
+++ 2-server/modules/system/default.nix
@@ -5,7 +5,16 @@
 { config, lib, pkgs, ... }:
 {
     imports = [                                   
-        ./system
-        ./services
+        ./0-hardware-configuration.nix
+        ./1-disk-config.nix
+        ./2-boot.nix
+        ./3-networking.nix
+        ./4-ssh.nix
+        ./5-impermanence.nix
+        ./6-data-drive.nix
+        ./7-software.nix
+        ./8-docker.nix
+        ./9-borgmatic.nix
+        ./10-utilities.nix
     ];
 }
