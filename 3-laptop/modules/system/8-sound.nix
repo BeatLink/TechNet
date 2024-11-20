8e645bb3cd8089059f09d31cd982517213dc47a4
BeatLink <beatlink+git@simplelogin.co>
2024-11-20 18:55:19 -0500

    Enable Laptop Sound

---
+++ 3-laptop/modules/system/8-sound.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{  
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+        media-session.enable = true;
+    };
+}
