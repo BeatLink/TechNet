13072442a9a6cee168b60529de8b4624305ca52f
BeatLink <beatlink+git@simplelogin.co>
2024-12-07 13:10:48 -0500

    Add nvidia

---
+++ 3-laptop/modules/4-apps/system/nvidia.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }: 
+{
+    home.file = {
+        ".config/autostart/nvidia-prime.desktop".text = ''
+            [Desktop Entry]
+            Name=Support for NVIDIA Prime
+            Comment=Shows a tray icon when a compatible NVIDIA Optimus graphics card is detected
+            Exec=/usr/lib/nvidia-prime-applet/nvidia-prime
+            Icon=prime-tray-nvidia
+            Terminal=false
+            Type=Application
+            Categories=GTK;GNOME;Settings;
+            StartupNotify=false
+            X-GNOME-Autostart-enabled=false
+            Hidden=true
+        '';
+    };
+}
