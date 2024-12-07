3109a27adacc9e41c6e97466c48ea17f0db07dcb
BeatLink <beatlink+git@simplelogin.co>
2024-12-07 01:10:05 -0500

    Add sops config pointing to persistence

--- 3-laptop/modules/1-system/8-sops.nix
+++ 3-laptop/modules/1-system/8-sops.nix
@@ -6,5 +6,5 @@
 
 { config, lib, pkgs, modulesPath, ... }: 
 {
-    sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];     # Sets the sops ssh key to persistent
+    sops.age.sshKeyPaths = [ "/persistent/etc/ssh/ssh_host_ed25519_key" ];     # Sets the sops ssh key to persistent
 }
