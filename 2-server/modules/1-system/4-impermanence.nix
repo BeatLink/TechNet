e873258a35e8d37815c296e837f42fe2ffedcefc
BeatLink <beatlink+git@simplelogin.co>
2024-12-07 04:32:43 -0500

    Fix impermanence

--- 2-server/modules/1-system/4-impermanence.nix
+++ 2-server/modules/1-system/4-impermanence.nix
@@ -49,7 +49,6 @@
         ];
         files = [
             { file = "/etc/machine-id"; parentDirectory = { mode = "0755"; }; }
-            { file = "/etc/ssh/ssh_host_ed25519_key"; parentDirectory = { mode = "0755"; }; }
         ];
     };
 }
