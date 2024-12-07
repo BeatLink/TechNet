91500343e0e61d8ed02894e407fa3629f63f6b38
BeatLink <beatlink+git@simplelogin.co>
2024-12-07 04:07:08 -0500

    Update boot

--- 3-laptop/modules/1-system/2-boot.nix
+++ 3-laptop/modules/1-system/2-boot.nix
@@ -12,10 +12,34 @@
             systemd-boot.enable = true;                                 # Use Systemd-Boot to manage booting
             grub.enable = false;                                        # Disable Grub since we're using Systemd-Boot
             efi.canTouchEfiVariables = true;                            # Allows setting boot order, UEFI settings, etc
+            timeout = 0;
         };
         initrd = {
             supportedFilesystems = [ "zfs" ];                           # Needed for impermanence
             systemd.enable = true;
+            verbose = false;
+    
         };
+        plymouth = {
+            enable = true;
+            theme = "double";
+            themePackages = with pkgs; [
+                # By default we would install all themes
+                (adi1090x-plymouth-themes.override {
+                selected_themes = [ "double" ];
+            })
+            ];
+        };
+        # Enable "Silent Boot"
+        consoleLogLevel = 0;
+        kernelParams = [
+            "quiet"
+            "splash"
+            "boot.shell_on_fail"
+            "loglevel=3"
+            "rd.systemd.show_status=false"
+            "rd.udev.log_level=3"
+            "udev.log_priority=3"
+        ];
     };
 }
