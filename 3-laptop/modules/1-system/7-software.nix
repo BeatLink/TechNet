bb057bfeb4deb8011e8a6456b8e614c6820cbfb0
BeatLink <beatlink+git@simplelogin.co>
2024-12-08 15:59:49 -0500

    Move dconf-editor to separate module

--- 3-laptop/modules/1-system/7-software.nix
+++ 3-laptop/modules/1-system/7-software.nix
@@ -1,8 +1,5 @@
 { config, pkgs, ... }:
 {
-    environment.systemPackages = [
-        pkgs.dconf-editor
-    ];
     system.stateVersion = "24.05"; # Did you read the comment?
 
     services.flatpak = {
