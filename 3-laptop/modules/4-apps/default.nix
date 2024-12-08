a5d8c98876608621deba721a114bf776dd7e4b73
BeatLink <beatlink+git@simplelogin.co>
2024-12-08 16:00:56 -0500

    Enable decoder

--- 3-laptop/modules/4-apps/default.nix
+++ 3-laptop/modules/4-apps/default.nix
@@ -4,6 +4,6 @@
         #./comms
         #./fun
         ./system
-        #./tools
+        ./tools
     ];
 }
---
+++ 3-laptop/modules/4-apps/tools/default.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+{
+    imports = [
+        ./decoder.nix
+        #./deluge.nix
+        #./fastfetch.nix
+        #./redshift.nix
+        #./valent.nix
+    ];
+}
