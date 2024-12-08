3831211729c4ec7890e3452eb77df5bd966c3a9d
BeatLink <beatlink+git@simplelogin.co>
2024-12-08 10:49:49 -0500

    Remove ssh host keys from impermanence

--- 3-laptop/modules/1-system/4-impermanence.nix
+++ 3-laptop/modules/1-system/4-impermanence.nix
@@ -40,7 +40,6 @@
         ];
         files = [
             { file = "/etc/machine-id"; parentDirectory = { mode = "0755"; }; }
-            { file = "/etc/ssh/ssh_host_ed25519_key"; parentDirectory = { mode = "0755"; }; }
         ];
     };
 }
