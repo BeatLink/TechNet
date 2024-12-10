6ca3109638cc632a174eddaa8424cc359c9d07c8
BeatLink <beatlink+git@simplelogin.co>
2024-12-10 11:08:59 -0500

    Update Disko Data Drive

--- 3-laptop/modules/1-system/5-disko-data-drive.nix
+++ 3-laptop/modules/1-system/5-disko-data-drive.nix
@@ -9,52 +9,48 @@
 
 {
     disko.devices = {
-        disk = {
-            data-drive = {
-                type = "disk";
-                device = "/dev/disk/by-id/nvme-Corsair_MP600_MICRO_A828B42710C0GL";
-                content = {
-                    type = "gpt";                                       # GPT Partition tables used on modern systems
-                    partitions = {
-                        zfs-data-partition = {                          # This creates a ZFS partition to be added to the ZFS pool
-                            size = "100%";
-                            content = {
-                                type = "zfs";
-                                pool = "zfs-data-pool";
-                            };
+        disk.data-drive = {
+            type = "disk";
+            device = "/dev/disk/by-id/nvme-Corsair_MP600_MICRO_A828B42710C0GL";
+            content = {
+                type = "gpt";                                       # GPT Partition tables used on modern systems
+                partitions = {
+                    zfs-data-partition = {                          # This creates a ZFS partition to be added to the ZFS pool
+                        size = "100%";
+                        content = {
+                            type = "zfs";
+                            pool = "data-pool";
                         };
                     };
                 };
             };
         };
-        zpool = {                                                       # Creates a ZFS pool for managing storage volumes
-            zfs-data-pool = {
-                type = "zpool";
-                options = {
-                    autotrim = "on";
-                };
-                rootFsOptions = {
-                    mountpoint = "none";                                # We will be mounting the children datasets, not this root one
-                    compression = "zstd";                               # Compresses files to save space
-                    xattr = "sa";                                       # Allows extended attributes stored in the filesystem inodes
-                    acltype = "posix";                                  # Uses posix compliant ACL for extended attributes
-                    "com.sun:auto-snapshot" = "false";                  # Prevents autosnapshots in general (will be enabled for specific datasets later)
-                    encryption = "aes-256-gcm";
-                    keyformat = "passphrase";
-                    keylocation = "file:///tmp/disk-1.key";
+        zpool.data-pool = {
+            type = "zpool";
+            options = {
+                autotrim = "on";
+            };
+            rootFsOptions = {
+                mountpoint = "none";                                # We will be mounting the children datasets, not this root one
+                compression = "zstd";                               # Compresses files to save space
+                xattr = "sa";                                       # Allows extended attributes stored in the filesystem inodes
+                acltype = "posix";                                  # Uses posix compliant ACL for extended attributes
+                "com.sun:auto-snapshot" = "false";                  # Prevents autosnapshots in general (will be enabled for specific datasets later)
+                encryption = "aes-256-gcm";
+                keyformat = "passphrase";
+                keylocation = "file:///tmp/disk-1.key";
 
-                };  
-                postCreateHook = ''
-                    zfs set keylocation="prompt" "zfs-data-pool";             # use this to read the key during boot
-                '';                                                      
-                datasets = {
-                    storage = {                                           # The dataset for the root filesystem mounted at /
-                        type = "zfs_fs";
-                        mountpoint = "/Storage";
-                        options = {
-                            mountpoint = "legacy";
-                            "com.sun:auto-snapshot" = "true";           # Generates snapshots to persist data
-                        };
+            };  
+            postCreateHook = ''
+                zfs set keylocation="prompt" "data-pool";             # use this to read the key during boot
+            '';                                                      
+            datasets = {
+                storage = {                                           # The dataset for the root filesystem mounted at /
+                    type = "zfs_fs";
+                    mountpoint = "/Storage";
+                    options = {
+                        mountpoint = "legacy";
+                        "com.sun:auto-snapshot" = "true";           # Generates snapshots to persist data
                     };
                 };
             };
