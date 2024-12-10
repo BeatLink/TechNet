9fbf388f806e59490d02e88974cb115f6278d0ec
BeatLink <beatlink+git@simplelogin.co>
2024-12-10 09:37:24 -0500

    Disables the data drive disko according to the installation environment variable

--- 3-laptop/modules/1-system/default.nix
+++ 3-laptop/modules/1-system/default.nix
@@ -4,12 +4,14 @@
 
 { config, lib, pkgs, ... }:
 {
+    disabledModules = lib.mkIf config.installationEnvironmentEnable [ "./5-disko-data-drive.nix ];
+
     imports = [                                   
         ./1-hardware-configuration.nix
         ./2-boot.nix
-        ./3-disk-config.nix
+        ./3-disko-root-drive.nix
         ./4-impermanence.nix
-        #./5-data-drive.nix
+        ./5-disko-data-drive.nix
         ./6-sops.nix
         ./7-software.nix
         ./8-networking.nix
