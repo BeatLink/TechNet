c931c5cb082d7c0113e0d2befacb646ca3b6dbcb
BeatLink <beatlink+git@simplelogin.co>
2024-12-10 11:07:32 -0500

    Update impermanence

--- 3-laptop/modules/1-system/4-impermanence.nix
+++ 3-laptop/modules/1-system/4-impermanence.nix
@@ -19,14 +19,14 @@
     boot.initrd.systemd.services.rollback = {
         description = "Rollback ZFS root subvolume to a pristine state";
         wantedBy = [ "initrd.target" ];
-        after = ["zfs-import-zfspool.service"];
+        after = ["zfs-import-root-pool.service"];
         before = [ "sysroot.mount" ];
         path = with pkgs; [ zfs ];
         unitConfig.DefaultDependencies = "no";
         serviceConfig.Type = "oneshot";
         script = ''
-            zfs rollback -r  zfspool/root@blank && 
-            zfs rollback -r  zfspool/home@blank && 
+            zfs rollback -r  root-pool/root@blank && 
+            zfs rollback -r  root-pool/home@blank && 
             echo "Rollback Complete"
         '';
     };
