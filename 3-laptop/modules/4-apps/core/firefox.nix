fc7c6f41c5a156c72812084434ff866874efe738
BeatLink <beatlink+git@simplelogin.co>
2024-12-10 19:36:37 -0500

    Add firefox

--- 3-laptop/modules/4-apps/core/firefox.nix
+++ 3-laptop/modules/4-apps/core/firefox.nix
@@ -2,6 +2,15 @@
 {
     services.flatpak.packages = ["flathub:app/org.mozilla.firefox//stable"];
 
+    programs.fuse.userAllowOther = true;
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home.persistence."/Storage/Apps/Core/Firefox" = {
+            directories = [
+                ".var/app/org.mozilla.firefox"
+            ];
+            allowOther = true;
+        };
+    };
 
     # Migrate Profiles
 
