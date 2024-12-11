c7c431f2acf2d8d41d5a6ef9112558bd31fea3a1
BeatLink <github@beatlink.simplelogin.com>
2024-12-10 20:34:15 -0500

    Update Plank

--- 3-laptop/modules/1-system/desktop-environment/plank/default.nix
+++ 3-laptop/modules/1-system/desktop-environment/plank/default.nix
@@ -14,8 +14,6 @@
     environment.systemPackages = [ pkgs.plank ];                # Installs Plank from nixpkgs
     programs.fuse.userAllowOther = true;
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        dconf.enable = true;                                    # Enables dconf which stores plank settings
-        imports = [./2-dconf-settings.nix];                     # Imports the dconf settings
         home = {
             persistence."/Storage/Apps/System/Plank" = {
                 directories = [
@@ -25,7 +23,38 @@
                 ];
                 allowOther = true;
             };
-            file.".config/autostart/plank.desktop".source = "${pkgs.plank}/share/applications/plank.desktop";       # Configures plank to autostart on login
+            file = {
+                ".config/autostart/plank.desktop".source = "${pkgs.plank}/share/applications/plank.desktop";       # Configures plank to autostart on login
+                
+                ".config/plank/dock1/launchers/firefox.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/org.mozilla.firefox.desktop
+                '';
+
+                ".config/plank/dock1/launchers/org.keepassxc.KeePassXC.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/org.keepassxc.KeePassXC.desktop
+                '';
+
+                ".config/plank/dock1/launchers/org.mozilla.Thunderbird.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/org.mozilla.Thunderbird.desktop
+                '';
+
+                ".config/plank/dock1/launchers/im.riot.Riot.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/im.riot.Riot.desktop
+                '';
+
+                ".config/plank/dock1/launchers/com.vscodium.codium.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/com.vscodium.codium.desktop
+                '';
+            
+            };
         };
+        dconf.enable = true;                                    # Enables dconf which stores plank settings
+        imports = [./2-dconf-settings.nix];                     # Imports the dconf settings
+
     };
 }
