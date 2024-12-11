b382670174b87c6143a0b168073547f5d89fb6b2
BeatLink <beatlink+git@simplelogin.co>
2024-12-10 19:44:16 -0500

    Update Vscodium

--- 3-laptop/modules/4-apps/tools/vscodium.nix
+++ 3-laptop/modules/4-apps/tools/vscodium.nix
@@ -1,4 +1,14 @@
 { config, lib, pkgs, ... }:
 {
     services.flatpak.packages = ["flathub:app/com.vscodium.codium//stable"];
+    programs.fuse.userAllowOther = true;
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home.persistence."/Storage/Apps/Programming/VsCodium" = {
+            directories = [
+                ".var/app/im.riot.Riot"
+                ".vscode-oss"
+            ];
+            allowOther = true;
+        };
+    };
 }
