5d2e1a59dff653a38ce4edc17d7941bbcee7e40b
BeatLink <github@beatlink.simplelogin.com>
2024-12-10 23:18:10 -0500

    Remove redundant files

--- users/beatlink/apps/system/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-{
-    imports = [
-        ./cinnamon.nix
-        ./flatpak.nix
-        ./nvidia.nix
-        ./vorta.nix
-    ];
-}
--- users/beatlink/apps/tools/fastfetch.nix
+++
@@ -1,4 +0,0 @@
-{ config, lib, pkgs, ... }:
-{
-    programs.fastfetch.enable = true;
-}
