b9ccbe1c410208eafc6bf010b80e705569dbccad
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 18:55:50 -0500

    Update Whatsapp

--- 3-laptop/modules/4-apps/comms/whatsapp.nix
+++ 3-laptop/modules/4-apps/comms/whatsapp.nix
@@ -1,25 +1,28 @@
 { config, pkgs, ... }: 
 {
-    home-manager.users.beatlink.xdg.desktopEntries = {
-        "whatsapp-private" = {
-            name = "WhatsApp (Private)";
-            comment = "";
-            exec = "firefox \"ext+container:name=Private&url=https://web.whatsapp.com\"";
-            terminal = false;
-            type = "Application";
-            prefersNonDefaultGPU = false;
-            icon = "whatsapp";
-            categories = ["Network" "InstantMessaging"];
-        };
-        "whatsapp" = {
-            name = "WhatsApp";
-            comment = "";
-            exec = "firefox https://web.whatsapp.com";
-            terminal = false;
-            type = "Application";
-            prefersNonDefaultGPU = false;
-            icon = "whatsapp";
-            categories = ["Network" "InstantMessaging"];
-        };
+    home-manager.users.beatlink.home.file = {
+        ".local/share/applications/whatsapp-private.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=WhatsApp (Private)
+            Comment=""
+            Icon=whatsapp
+            Exec=flatpak run org.mozilla.firefox "ext+container:name=Private&url=https://web.whatsapp.com"
+            Categories=Network;InstantMessaging
+            PrefersNonDefaultGPU = false;
+            Terminal=false
+        '';
+        
+        ".local/share/applications/whatsapp.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=WhatsApp
+            Comment=""
+            Icon=whatsapp
+            Exec=flatpak run org.mozilla.firefox https://web.whatsapp.com
+            Categories=Network;InstantMessaging
+            PrefersNonDefaultGPU = false;
+            Terminal=false
+        '';        
     };
 }
