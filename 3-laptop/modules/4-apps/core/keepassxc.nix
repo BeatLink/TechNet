e66bfb5a9ade137495f3bb73459d4ed77ee345a5
BeatLink <github@beatlink.simplelogin.com>
2024-12-11 22:05:34 -0500

    Update KeePassXC

--- 3-laptop/modules/4-apps/core/keepassxc.nix
+++ 3-laptop/modules/4-apps/core/keepassxc.nix
@@ -1,4 +1,15 @@
 { config, lib, pkgs, ... }:
 {
     services.flatpak.packages = ["flathub:app/org.keepassxc.KeePassXC//stable"];
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home = {
+            persistence."/Storage/Apps/Core/KeePassXC" = {
+                directories = [
+                    ".var/app/org.keepassxc.KeePassXC"
+                ];
+                allowOther = true;
+            };
+            file.".config/autostart/org.keepassxc.KeePassXC".source = config.lib.file.mkOutOfStoreSymlink "/var/lib/flatpak/app/org.keepassxc.KeePassXC/current/active/files/share/applications/org.keepassxc.KeePassXC.desktop";
+        };
+    };
 }
