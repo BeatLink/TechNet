765ed2d6ef86815895207857201e2c4023ee383c
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 18:07:09 -0500

    Update Stremio

--- 3-laptop/modules/4-apps/fun/stremio.nix
+++ 3-laptop/modules/4-apps/fun/stremio.nix
@@ -3,13 +3,11 @@
 
     services.flatpak.packages = ["flathub:app/com.stremio.Stremio//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        home = {
-            persistence."/Storage/Apps/Fun/Stremio" = {
-                directories = [
-                    ".var/app/com.stremio.Stremio"
-                ];
-                allowOther = true;
-            };
+        home.persistence."/Storage/Apps/Fun/Stremio" = {
+            directories = [
+                ".var/app/com.stremio.Stremio"
+            ];
+            allowOther = true;
         };
     };
 }
