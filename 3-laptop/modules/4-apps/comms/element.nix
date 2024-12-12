0df1e8b24167038dad3cfaf4854c1581e138b2e5
BeatLink <github@beatlink.simplelogin.com>
2024-12-11 22:20:24 -0500

    Update Element

--- 3-laptop/modules/4-apps/comms/element.nix
+++ 3-laptop/modules/4-apps/comms/element.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }: 
 {
     services.flatpak.packages = ["flathub:app/im.riot.Riot//stable"];
-    programs.fuse.userAllowOther = true;
-
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home.persistence."/Storage/Apps/Comms/Element" = {
             directories = [
