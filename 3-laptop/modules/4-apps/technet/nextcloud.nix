72c3cf6ff06512fbdfb8c1c931ffcf415b0c3a56
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 19:15:38 -0500

    Update Nextcloud

--- 3-laptop/modules/4-apps/technet/nextcloud.nix
+++ 3-laptop/modules/4-apps/technet/nextcloud.nix
@@ -1,15 +1,22 @@
 { config, lib, pkgs, ... }:
 {
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        home.file.".local/share/applications/nextcloud.desktop".text = ''
-            [Desktop Entry]
-            Name=Nextcloud
-            Exec=flatpak run org.mozilla.firefox https://nextcloud.heimdall.technet
-            Comment=
-            Terminal=false
-            PrefersNonDefaultGPU=false
-            Icon=goa-account-owncloud
-            Type=Application
-        '';
+        home.file = {
+            ".local/share/applications/nextcloud.desktop".text = ''
+                [Desktop Entry]
+                Name=Nextcloud
+                Exec=flatpak run org.mozilla.firefox https://nextcloud.heimdall.technet
+                Comment=
+                Terminal=false
+                PrefersNonDefaultGPU=false
+                Icon=goa-account-owncloud
+                Type=Application
+            '';
+
+            ".config/plank/dock1/launchers/nextcloud.dockitem".text = ''
+                [PlankDockItemPreferences]
+                Launcher=file:///home/beatlink/.local/share/applications/nextcloud.desktop
+            '';
+        };
     };
 }
