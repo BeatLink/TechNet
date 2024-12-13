9b7a35d315ee2489f8c53c8c908d63bb96a8e2bf
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 19:15:48 -0500

    Update Homeassistant

--- 3-laptop/modules/4-apps/technet/home-assistant/default.nix
+++ 3-laptop/modules/4-apps/technet/home-assistant/default.nix
@@ -1,16 +1,25 @@
 { config, lib, pkgs, ... }:
 {
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        home.file.".local/share/applications/home-assistant.desktop".text = ''
-            [Desktop Entry]
-            Name=Home Assistant
-            Exec=flatpak run org.mozilla.firefox https://homeassistant.heimdall.technet
-            Comment=
-            Terminal=false
-            PrefersNonDefaultGPU=false
-            Icon=home-assistant.png
-            Type=Application
-        '';
-        home.file.".local/share/icons/home-assistant.png".source = ./home-assistant.png;
+        home.file = {
+            ".local/share/icons/home-assistant.png".source = ./home-assistant.png;
+
+            ".local/share/applications/home-assistant.desktop".text = ''
+                [Desktop Entry]
+                Name=Home Assistant
+                Exec=flatpak run org.mozilla.firefox https://homeassistant.heimdall.technet
+                Comment=
+                Terminal=false
+                PrefersNonDefaultGPU=false
+                Icon=home-assistant.png
+                Type=Application
+            '';
+
+            ".config/plank/dock1/launchers/home-assistant.dockitem".text = ''
+                [PlankDockItemPreferences]
+                Launcher=file:///home/beatlink/.local/share/applications/home-assistant.desktop
+            '';
+
+        };
     };
 }
