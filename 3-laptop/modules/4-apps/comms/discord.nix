1d838d150f15aeca9be19f3a2a3f27cbca7f8bc5
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 19:38:28 -0500

    Update Discord

--- 3-laptop/modules/4-apps/comms/discord.nix
+++ 3-laptop/modules/4-apps/comms/discord.nix
@@ -2,11 +2,19 @@
 {
     services.flatpak.packages = ["flathub:app/com.discordapp.Discord//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        home.persistence."/Storage/Apps/Comms/Discord" = {
-            directories = [
-                ".var/app/com.discordapp.Discord"
-            ];
-            allowOther = true;
+        home = {
+            persistence."/Storage/Apps/Comms/Discord" = {
+                directories = [
+                    ".var/app/com.discordapp.Discord"
+                ];
+                allowOther = true;
+            };
+            file = {
+                ".config/plank/dock1/launchers/com.discordapp.Discord.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/com.discordapp.Discord.desktop
+                '';
+            };
         };
     };
 }
