b9d80c04199c6321c7650ab4114ec15e74f37b93
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 19:15:27 -0500

    Update Stremio

--- 3-laptop/modules/4-apps/fun/stremio.nix
+++ 3-laptop/modules/4-apps/fun/stremio.nix
@@ -3,11 +3,19 @@
 
     services.flatpak.packages = ["flathub:app/com.stremio.Stremio//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        home.persistence."/Storage/Apps/Fun/Stremio" = {
-            directories = [
-                ".var/app/com.stremio.Stremio"
-            ];
-            allowOther = true;
+        home = {
+            persistence."/Storage/Apps/Fun/Stremio" = {
+                directories = [
+                    ".var/app/com.stremio.Stremio"
+                ];
+                allowOther = true;
+            };
+            file = {
+                ".config/plank/dock1/launchers/com.stremio.Stremio.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/com.stremio.Stremio.desktop
+                '';
+            };
         };
     };
 }
