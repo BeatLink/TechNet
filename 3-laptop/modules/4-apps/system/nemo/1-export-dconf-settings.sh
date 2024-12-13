573aee566cc369fad420649f6ed90054acb98b9f
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 23:02:21 -0500

    Fix Nemo Dconf

--- 3-laptop/modules/4-apps/system/nemo/1-export-dconf-settings.sh
+++ 3-laptop/modules/4-apps/system/nemo/1-export-dconf-settings.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-dconf dump /org/nemo/ | nix-shell -p dconf2nix --run "dconf2nix --root "/org/nemo/plank" > 2-dconf-settings.nix"
+dconf dump /org/nemo/ | nix-shell -p dconf2nix --run "dconf2nix --root "/org/nemo/" > 2-dconf-settings.nix"
--- 3-laptop/modules/4-apps/system/nemo/2-dconf-settings.nix
+++ 3-laptop/modules/4-apps/system/nemo/2-dconf-settings.nix
@@ -5,48 +5,15 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "org/nemo/plank/plank/preferences" = {
+    "org/nemo/preferences" = {
+      last-server-connect-method = 0;
       show-hidden-files = true;
     };
 
-    "org/nemo/plank/plank/search" = {
-      search-reverse-sort = false;
-      search-sort-column = "name";
-    };
-
-    "org/nemo/plank/plank/window-state" = {
+    "org/nemo/window-state" = {
       maximized = true;
-      sidebar-bookmark-breakpoint = 0;
-      start-with-sidebar = true;
-    };
-
-    "org/nemo/plank/preferences" = {
-      close-device-view-on-device-eject = true;
-      quick-renames-with-pause-in-between = true;
-      show-computer-icon-toolbar = true;
-      show-hidden-files = true;
-      show-home-icon-toolbar = true;
-      show-new-folder-icon-toolbar = true;
-      show-open-in-terminal-toolbar = true;
-      show-reload-icon-toolbar = true;
-      show-show-thumbnails-toolbar = true;
-    };
-
-    "org/nemo/plank/preferences/menu-config" = {
-      selection-menu-copy-to = true;
-      selection-menu-duplicate = true;
-      selection-menu-make-link = true;
-      selection-menu-move-to = true;
-    };
-
-    "org/nemo/plank/search" = {
-      search-reverse-sort = false;
-      search-sort-column = "name";
-    };
-
-    "org/nemo/plank/window-state" = {
-      maximized = true;
-      sidebar-bookmark-breakpoint = 0;
+      sidebar-bookmark-breakpoint = 12;
+      sidebar-width = 373;
       start-with-sidebar = true;
     };
 
