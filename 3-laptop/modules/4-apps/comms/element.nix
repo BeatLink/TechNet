9ee8e07c06931bf5d80e7d62821e95f79538f945
BeatLink <github@beatlink.simplelogin.com>
2024-12-12 19:11:30 -0500

    Update element

--- 3-laptop/modules/4-apps/comms/element.nix
+++ 3-laptop/modules/4-apps/comms/element.nix
@@ -2,11 +2,19 @@
 {
     services.flatpak.packages = ["flathub:app/im.riot.Riot//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        home.persistence."/Storage/Apps/Comms/Element" = {
-            directories = [
-                ".var/app/im.riot.Riot"
-            ];
-            allowOther = true;
+        home = {
+            persistence."/Storage/Apps/Comms/Element" = {
+                directories = [
+                    ".var/app/im.riot.Riot"
+                ];
+                allowOther = true;
+            };
+            file = {
+                ".config/plank/dock1/launchers/im.riot.Riot.dockitem".text = ''
+                    [PlankDockItemPreferences]
+                    Launcher=file:///var/lib/flatpak/exports/share/applications/im.riot.Riot.desktop
+                '';
+            };
         };
     };
 }
