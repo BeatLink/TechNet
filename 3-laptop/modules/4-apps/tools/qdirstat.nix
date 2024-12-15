227aeb4f6b727e0f0384d493555749a4746de59d
BeatLink <github@beatlink.simplelogin.com>
2024-12-15 14:51:07 -0500

    Update QDirStat

--- 3-laptop/modules/4-apps/tools/qdirstat.nix
+++ 3-laptop/modules/4-apps/tools/qdirstat.nix
@@ -1,4 +1,14 @@
 { config, lib, pkgs, ... }:
 {
     environment.systemPackages = with pkgs; [ qdirstat ];
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home = {
+            persistence."/Storage/Apps/Tools/QDirStat" = {
+                directories = [
+                    ".config/QDirStat"
+                ];
+                allowOther = true;
+            };
+        };
+    };
 }
