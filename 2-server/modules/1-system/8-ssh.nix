92caffed5e99c580a75c1540aacaf735cea24f8b
BeatLink <github@beatlink.simplelogin.com>
2024-12-15 03:21:20 -0500

    Update SSH

--- 2-server/modules/1-system/8-ssh.nix
+++ 2-server/modules/1-system/8-ssh.nix
@@ -2,27 +2,15 @@
 # Enables SSH in Boot for Remote LUKS Unlock
 #######################################################################################################################################
 { config, lib, pkgs, ... }:{
-    boot.initrd = {
-        network.ssh = {                                             
-            port = 22;
-            enable = true;
-            authorizedKeys = [ 
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM4GfJHxZu55mhQPpL1MqLCrS4ws/1ZUodC/QicApyGF beatlink@technet" 
-            ];
-            hostKeys = [
-                "/persistent/etc/ssh/ssh_initrd_host_ed25519_key"
-            ];
-        };
-        systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
-    };
-    services.openssh = {                                                # This is needed to solve problems with SSH permissions 
-        hostKeys = [
-            { 
-                type = "ed25519"; 
-                path = "/persistent/etc/ssh/ssh_host_ed25519_key"; 
-            }
-        ];
-    };
+    boot.initrd.network.ssh.hostKeys = [
+        "/persistent/etc/ssh/ssh_initrd_host_ed25519_key"
+    ];
+    services.openssh.hostKeys = [
+        { 
+            type = "ed25519"; 
+            path = "/persistent/etc/ssh/ssh_host_ed25519_key"; 
+        }
+    ];
 }
 
 
