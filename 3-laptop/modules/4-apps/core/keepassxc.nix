053c6646c2b1d299d481830959bac555bfd6e25c
BeatLink <github@beatlink.simplelogin.com>
2024-12-18 01:33:19 -0500

    Autostart keepassxc

--- 3-laptop/modules/4-apps/core/keepassxc.nix
+++ 3-laptop/modules/4-apps/core/keepassxc.nix
@@ -10,8 +10,8 @@
                 allowOther = true;
             };
             file = {
-                ".config/autostart/org.keepassxc.KeePassXC".source = config.lib.file.mkOutOfStoreSymlink "/var/lib/flatpak/app/org.keepassxc.KeePassXC/current/active/files/share/applications/org.keepassxc.KeePassXC.desktop";
-                
+                ".config/autostart/org.keepassxc.KeePassXC.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/lib/flatpak/exports/share/applications/org.keepassxc.KeePassXC.desktop";
+
                 ".config/plank/dock1/launchers/org.keepassxc.KeePassXC.dockitem".text = ''
                     [PlankDockItemPreferences]
                     Launcher=file:///var/lib/flatpak/exports/share/applications/org.keepassxc.KeePassXC.desktop
