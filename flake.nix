6b0543469e9d7a4f535e956bf6e61a45375744b6
BeatLink <github@beatlink.simplelogin.com>
2024-12-18 07:22:02 -0500

    Rollback Flake (Fix instability)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         nixpkgs = {
-            url = "github:NixOS/nixpkgs/nixos-unstable";
+            url = "github:NixOS/nixpkgs/nixos-24.11";
         };
         nixos-hardware = {
             url = "github:NixOS/nixos-hardware/master";
