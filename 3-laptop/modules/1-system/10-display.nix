3a1ee113a8e9ec9c8a3d8f84db4b419a67d4c0da
BeatLink <github@beatlink.simplelogin.com>
2024-12-18 21:47:20 -0500

    Update display

--- 3-laptop/modules/1-system/10-display.nix
+++ 3-laptop/modules/1-system/10-display.nix
@@ -1,3 +1,21 @@
 { config, lib, pkgs, ... }:
 {
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = ["nvidia" "amdgpu"];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        open = false;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        prime = {
+            offload = {
+			    enable = true;
+			    enableOffloadCmd = true;
+		    };
+            amdgpuBusId = "PCI:6:0:0";
+            nvidiaBusId = "PCI:1:0:0";
+        };
+    };
 }
