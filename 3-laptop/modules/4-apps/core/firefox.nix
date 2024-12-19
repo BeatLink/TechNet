accb928a86eeb707be10c6c07fe10fb037c138f5
BeatLink <github@beatlink.simplelogin.com>
2024-12-18 23:04:21 -0500

    Update Firefox

--- 3-laptop/modules/4-apps/core/firefox.nix
+++ 3-laptop/modules/4-apps/core/firefox.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
 {
-    services.flatpak.packages = ["flathub:app/org.mozilla.firefox//stable"];
-
+    services.flatpak.packages = [
+        "flathub:app/org.mozilla.firefox//stable"
+        "flathub:runtime/org.freedesktop.Platform.ffmpeg-full//23.08"
+    ];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
             persistence."/Storage/Apps/Core/Firefox" = {
