02fe877590fd45cd1a93a5008633c59e2af87b91
BeatLink <github@beatlink.simplelogin.com>
2024-12-18 21:47:29 -0500

    Update hardware config

--- 3-laptop/modules/1-system/1-hardware-configuration.nix
+++ 3-laptop/modules/1-system/1-hardware-configuration.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "uas" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" "mt7921e" ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
