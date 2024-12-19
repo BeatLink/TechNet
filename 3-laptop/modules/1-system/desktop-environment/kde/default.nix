c306c1e3e43eca9dca4455e5eb1748234cc001c2
BeatLink <github@beatlink.simplelogin.com>
2024-12-19 18:43:09 -0500

    Enable Breeze Theme

--- 3-laptop/modules/1-system/desktop-environment/kde/default.nix
+++ 3-laptop/modules/1-system/desktop-environment/kde/default.nix
@@ -4,7 +4,12 @@
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
 
+
     programs.kdeconnect.enable = true;
+
+    services.flatpak.packages = [ "flathub:app/org.gtk.Gtk3theme.Breeze//stable" ];
+
+    #org.gtk.Gtk3theme.Breeze
 
     environment.plasma6.excludePackages = with pkgs.kdePackages; [
         elisa
@@ -13,7 +18,6 @@
     ];
 
     home-manager.users.beatlink = { config, pkgs, ... }: {
-
         programs.plasma.enable = true;
 
         /*input.touchpads = [
