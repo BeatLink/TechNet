cbab52a67d321559aeab1ff635d4e7f44c4fa0e7
BeatLink <github@beatlink.simplelogin.com>
2024-12-19 13:41:31 -0500

    Add Screenlock

---
+++ 3-laptop/modules/1-system/desktop-environment/kde/screenlock.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+{
+    programs.plasma.kscreenlocker = {
+        autoLock = true;
+        timeout = 2;
+        passwordRequired = true;
+        passwordRequiredDelay = 1;
+        lockOnResume = true;
+        lockOnStartup = false;
+        appearance = {
+            alwaysShowClock = false;
+            showMediaControls = false;
+        };
+    };
+};
