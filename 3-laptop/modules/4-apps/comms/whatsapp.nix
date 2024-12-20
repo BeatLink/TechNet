333d97610205da1cd23e628f07d30b66fa03c843
BeatLink <github@beatlink.simplelogin.com>
2024-12-19 21:38:24 -0500

    Update Whatsapp

--- 3-laptop/modules/4-apps/comms/whatsapp.nix
+++ 3-laptop/modules/4-apps/comms/whatsapp.nix
@@ -1,38 +1,16 @@
 { config, pkgs, ... }: 
 {
-    home-manager.users.beatlink.home.file = {
-        ".local/share/applications/whatsapp-private.desktop".text = ''
-            [Desktop Entry]
-            Type=Application
-            Name=WhatsApp (Private)
-            Comment=""
-            Icon=whatsapp
-            Exec=flatpak run org.mozilla.firefox "ext+container:name=Private&url=https://web.whatsapp.com"
-            Categories=Network;InstantMessaging
-            PrefersNonDefaultGPU = false;
-            Terminal=false
-        '';
-        
-        ".local/share/applications/whatsapp.desktop".text = ''
-            [Desktop Entry]
-            Type=Application
-            Name=WhatsApp
-            Comment=""
-            Icon=whatsapp
-            Exec=flatpak run org.mozilla.firefox https://web.whatsapp.com
-            Categories=Network;InstantMessaging
-            PrefersNonDefaultGPU = false;
-            Terminal=false
-        '';
+    home-manager.users.beatlink.home = {
+        file = {
 
-        ".config/plank/dock1/launchers/WhatsApp.dockitem".text = ''
-            [PlankDockItemPreferences]
-            Launcher=file:///home/beatlink/.local/share/applications/whatsapp.desktop
-        '';
-        
-        ".config/plank/dock1/launchers/WhatsApp (Private).dockitem".text = ''
-            [PlankDockItemPreferences]
-            Launcher=file:///home/beatlink/.local/share/applications/whatsapp-private.desktop
-        '';
+        };
+        persistence."/Storage/Apps/Comms/WhatsApp/" = {
+            files = [
+                ".local/share/applications/whatsapp-private.desktop"
+                ".local/share/applications/whatsapp.desktop"
+                ".local/share/icons/whatsapp.png"
+            ];
+            allowOther = true;
+        };
     };
 }
