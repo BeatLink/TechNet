bb98e97c5eca7f6cb2fa5d33cc5273d715f449c0
BeatLink <github@beatlink.simplelogin.com>
2024-12-20 23:24:44 -0500

    Add clock

---
+++ 3-laptop/modules/4-apps/tools/clock.nix
@@ -0,0 +1,11 @@
+
+{ config, pkgs, ... }: 
+{
+    environment.systemPackages = with pkgs; [ kdePackages.kclock ];
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home.persistence."/Storage/Apps/Tools/KClock" = {
+            files = [".config/kclockdrc"];
+            allowOther = true;
+        };
+    };
+}
