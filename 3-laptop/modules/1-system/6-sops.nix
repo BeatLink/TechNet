cf9bf928d439882c37b0911e6474116df42f8ac9
BeatLink <github@beatlink.simplelogin.com>
2024-12-21 08:48:17 -0500

    Update Sops, MoneyManager

--- 3-laptop/modules/1-system/6-sops.nix
+++ 3-laptop/modules/1-system/6-sops.nix
@@ -7,5 +7,14 @@
 { config, lib, pkgs, modulesPath, ... }: 
 {
     sops.age.sshKeyPaths = [ "/persistent/etc/ssh/ssh_host_ed25519_key" ];     # Sets the sops ssh key to persistent
+    environment.systemPackages = with pkgs; [ sops ];
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home.persistence."/Storage/Apps/System/SOPS" = {
+            files = [
+                ".config/sops/age/keys.txt"
+            ];
+            allowOther = true;
+        };
+    };
 }
 
