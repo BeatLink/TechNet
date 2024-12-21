271aaf154d9ecf1c541a0fcab694ab5d71c1e9c8
BeatLink <github@beatlink.simplelogin.com>
2024-12-21 09:03:30 -0500

    Update MoneyManager

--- 3-laptop/modules/4-apps/core/moneymanager.nix
+++ 3-laptop/modules/4-apps/core/moneymanager.nix
@@ -2,7 +2,7 @@
 {
     services.flatpak = {
         packages = [ "flathub:app/org.moneymanagerex.MMEX//stable" ];
-        overrides."io.lmms.LMMS" = {
+        overrides."org.moneymanagerex.MMEX" = {
             filesystems = [
                 "/Storage/Files/Documents"
             ];
@@ -13,6 +13,7 @@
             persistence."/Storage/Apps/Core/MoneyManager" = {
                 directories = [
                     ".var/app/org.moneymanagerex.MMEX"
+                    ".mmex"
                 ];
                 allowOther = true;
             };
