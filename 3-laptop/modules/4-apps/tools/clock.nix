99d1e29ac6f5188cf9f0f618827d3b810348dc3a
BeatLink <github@beatlink.simplelogin.com>
2024-12-20 23:41:53 -0500

    Update Clock

--- 3-laptop/modules/4-apps/tools/clock.nix
+++ 3-laptop/modules/4-apps/tools/clock.nix
@@ -1,6 +1,11 @@
 
 { config, pkgs, ... }: 
 {
-    environment.systemPackages = with pkgs; [ kclock ];
-
+    environment.systemPackages = with pkgs; [ kdePackages.kclock ];
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home.persistence."/Storage/Apps/Tools/KClock" = {
+            files = [".config/kclockdrc"];
+            allowOther = true;
+        };
+    };
 }
