688c0641489035a2f896031e9b95d5f8085e3245
BeatLink <github@beatlink.simplelogin.com>
2024-12-20 22:59:53 -0500

    Update System

--- 3-laptop/modules/4-apps/system/default.nix
+++ 3-laptop/modules/4-apps/system/default.nix
@@ -1,11 +1,8 @@
 { config, lib, pkgs, ... }:
 {
     imports = [
-        #./cinnamon.nix
-        #./flatpak.nix
         ./dconf-editor.nix
         ./fonts.nix
-        ./nvidia.nix
         ./nemo
         ./vorta.nix
     ];
