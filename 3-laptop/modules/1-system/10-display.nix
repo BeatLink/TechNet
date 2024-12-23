9447bf945118a6241c6893e731c2d648e60e694c
BeatLink <github@beatlink.simplelogin.com>
2024-12-23 15:01:48 -0500

    Switch to only nvidia display drivers

--- 3-laptop/modules/1-system/10-display.nix
+++ 3-laptop/modules/1-system/10-display.nix
@@ -11,12 +11,10 @@
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
         prime = {
-            offload = {
-			    enable = true;
-			    enableOffloadCmd = true;
-		    };
+            sync.enable = true;
             amdgpuBusId = "PCI:6:0:0";
             nvidiaBusId = "PCI:1:0:0";
         };
     };
+    services.xserver.videoDrivers = ["nvidia"];
 }
