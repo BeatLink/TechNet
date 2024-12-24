cd6abfb9fcc5031a0c2d0616481488939be5436d
BeatLink <github@beatlink.simplelogin.com>
2024-12-24 07:57:07 -0500

    Fix KDE Connect

--- 3-laptop/modules/4-apps/technet/kde-connect.nix
+++ 3-laptop/modules/4-apps/technet/kde-connect.nix
@@ -2,6 +2,7 @@
 {
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
+            file.".config/autostart/plank.desktop".source = "${pkgs.kdeconnect}/share/applications/org.kde.kdeconnect.daemon.desktop";       # Configures plank to autostart on login
             persistence."/Storage/Apps/TechNet/KDEConnect/" = {
                 directories = [
                     ".config/kdeconnect/"
