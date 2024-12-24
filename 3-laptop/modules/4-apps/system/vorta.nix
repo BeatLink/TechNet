9c6b442ddeb667a17a57a8270542de4a070c02dc
BeatLink <github@beatlink.simplelogin.com>
2024-12-24 07:30:10 -0500

    Update Vorta

--- 3-laptop/modules/4-apps/system/vorta.nix
+++ 3-laptop/modules/4-apps/system/vorta.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }: 
-{
+
+
+let 
+    prebackupCommand = pkgs.writeShellScript "vorta-prebackup.sh" ''
+        until ssh-add -l &> /dev/null 
+        do 
+            flatpak-spawn --host notify-send -a "Vorta" "Database Unlock Required" "Please unlock the KeePassXC database for server backups."; 
+            flatpak run org.keepassxc.KeePassXC &> /dev/null; 
+            sleep 10; 
+        done 
+        wget --spider "http://uptime-kuma.heimdall.technet/api/push/8ME1iuK3yx?status=up&msg=Backups Started&ping=";
+    '';
+in {
+    environment.systemPackages = with pkgs; [ libnotify ];
     services.flatpak.packages = [ "flathub:app/com.borgbase.Vorta//stable" ];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
@@ -10,6 +23,7 @@
                 allowOther = true;
             };
             file = {
+                ".var/app/com.borgbase.Vorta/vorta-prebackup.sh".source = prebackupCommand;
                 ".config/autostart/com.borgbase.Vorta.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/lib/flatpak/exports/share/applications/com.borgbase.Vorta.desktop";
             };
         };
