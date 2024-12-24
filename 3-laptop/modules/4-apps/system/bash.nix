79f37b142cb4b9e31563d7601b020be223489761
BeatLink <github@beatlink.simplelogin.com>
2024-12-24 09:08:37 -0500

    Add bash

---
+++ 3-laptop/modules/4-apps/system/bash.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }: 
+{
+    programs.bash = {
+        shellAliases = {
+            update = "cd /Storage/TechNet && sudo nixos-rebuild --flake .# switch"
+        }
+    };
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home = {
+            persistence."/Storage/Apps/System/Bash" = {
+                directories = [
+                    ".config/QDirStat"
+                ];
+                allowOther = true;
+            };
+        };
+    };
+}
