5bef79b258c5aa58b40c43b5b3d37ffbd9cd2c21
BeatLink <github@beatlink.simplelogin.com>
2024-12-24 08:21:16 -0500

    Update Dolphin

--- 3-laptop/modules/4-apps/system/dolphin.nix
+++ 3-laptop/modules/4-apps/system/dolphin.nix
@@ -4,6 +4,23 @@
 {
     environment.systemPackages = with pkgs; [ dolphin ];
     home-manager.users.beatlink = { config, pkgs, ... }: {
+        programs.plasma.configFile = {
+            "dolphinrc" = {
+                "General" = {
+                    "FilterBar" = true;
+                    "OpenExternallyCalledFolderInNewTab" = true;
+                    "ShowFullPath" = true;
+                    "ShowFullPathInTitlebar" = true;
+                    "ViewPropsTimestamp" = "2024,12,24,7,45,57.603";
+                };
+                "InformationPanel"."previewsAutoPlay" = true;
+                "KFileDialog Settings" = {
+                    "Places Icons Auto-resize" = false;
+                    "Places Icons Static Size" = 22;
+                };
+                "PreviewSettings"."Plugins" = "appimagethumbnail,audiothumbnail,blenderthumbnail,comicbookthumbnail,cursorthumbnail,djvuthumbnail,ebookthumbnail,exrthumbnail,directorythumbnail,fontthumbnail,imagethumbnail,jpegthumbnail,kraorathumbnail,windowsexethumbnail,windowsimagethumbnail,mobithumbnail,opendocumentthumbnail,gsthumbnail,rawthumbnail,svgthumbnail,textthumbnail,ffmpegthumbs";
+            };
+        };
         home = {
             persistence."/Storage/Apps/System/Dolphin" = {
                 directories = [
