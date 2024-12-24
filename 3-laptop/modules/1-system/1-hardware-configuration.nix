8790c7ab3137d0969bf1f7b15b119a40a4b90951
BeatLink <github@beatlink.simplelogin.com>
2024-12-24 09:01:31 -0500

    Use nvidia drivers

--- 3-laptop/modules/1-system/1-hardware-configuration.nix
+++ 3-laptop/modules/1-system/1-hardware-configuration.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... }:
 {
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "uas" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ "nvidia" ];
-  boot.kernelModules = [ "kvm-amd" "mt7921e" "ideapad_laptop" "nvidia"];
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.initrd.kernelModules = ["nvidia"];
+  boot.kernelParams = ["amd_pstate=active"];
+  boot.kernelModules = [ "kvm-amd" "mt7921e" "ideapad_laptop"];
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
 }
