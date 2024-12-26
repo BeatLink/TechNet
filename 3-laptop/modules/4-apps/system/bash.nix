ca67372516200490771cf7c77c8f8b6af2c5c5e4
BeatLink <github@beatlink.simplelogin.com>
2024-12-26 00:04:07 -0500

    Update Bash Alias

--- 3-laptop/modules/4-apps/system/bash.nix
+++ 3-laptop/modules/4-apps/system/bash.nix
@@ -2,7 +2,7 @@
 {
     programs.bash = {
         shellAliases = {
-            update = "cd /Storage/TechNet && sudo nixos-rebuild --flake .# switch";
+            upgrade = "cd /Storage/TechNet && sudo nixos-rebuild --flake .# switch";
         };
     };
     home-manager.users.beatlink = { config, pkgs, ... }: {
