cf5ef56471d4dd0f3374ecf58b1dbe234c258b0f
BeatLink <github@beatlink.simplelogin.com>
2024-12-27 11:09:25 -0500

    Update Bash

--- 3-laptop/modules/4-apps/system/bash.nix
+++ 3-laptop/modules/4-apps/system/bash.nix
@@ -3,6 +3,13 @@
     programs.bash = {
         shellAliases = {
             upgrade = "cd /Storage/TechNet && sudo nixos-rebuild --flake .# switch";
+            heimdall = "ssh heimdall.technet";
+            odin = "ssh odin.technet";
+            ragnarok = "ssh ragnarok.technet";
+
+            l = "ls";
+            la = "ls -la";
+            lt = "tree -a";
         };
     };
     home-manager.users.beatlink = { config, pkgs, ... }: {
