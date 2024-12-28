d30e0b93f2c335c316a4930fdcb045caa968b3ba
BeatLink <github@beatlink.simplelogin.com>
2024-12-28 01:24:36 -0500

    Update Hardware Configuration

--- 3-laptop/modules/1-system/1-hardware-configuration.nix
+++ 3-laptop/modules/1-system/1-hardware-configuration.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, ... }:
 {
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "uas" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = ["nvidia"];
-  boot.kernelParams = ["amd_pstate=active"];
-  boot.kernelModules = [ "kvm-amd" "mt7921e" "ideapad_laptop"];
+  boot = {
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "uas" "usbhid" "sd_mod" ];
+    initrd.kernelModules = ["nvidia"];
+    kernelParams = ["amd_pstate=active"];
+    kernelModules = [ "kvm-amd" "mt7921e" "ideapad_laptop"];
+    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  };
   nixpkgs.hostPlatform = "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
