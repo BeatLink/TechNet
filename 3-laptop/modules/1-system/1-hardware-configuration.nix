0b32c9b6bdce196554d5875a2c5f5c6cfdb6f8b6
BeatLink <github@beatlink.simplelogin.com>
2024-12-30 13:16:14 -0500

    Move boot options to hardware configuration

--- 3-laptop/modules/1-system/1-hardware-configuration.nix
+++ 3-laptop/modules/1-system/1-hardware-configuration.nix
@@ -17,10 +17,20 @@
                 "kvm-amd"
             ];
             kernelModules = ["nvidia"];
+            supportedFilesystems = [ "zfs" ];                           # Needed for impermanence
+            systemd.enable = true;
         };
+        supportedFilesystems = [ "zfs" ];                               # Needed for impermanence
         kernelParams = ["amd_pstate=active"];
         kernelModules = [];
         extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+        loader = {
+            systemd-boot.enable = true;                                 # Use Systemd-Boot to manage booting
+            grub.enable = false;                                        # Disable Grub since we're using Systemd-Boot
+            efi.canTouchEfiVariables = true;                            # Allows setting boot order, UEFI settings, etc
+            timeout = 0;
+        };
+
     };
     nixpkgs.hostPlatform = "x86_64-linux";
 }
