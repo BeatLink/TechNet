6a133ca6dabe34f232e9394e73246b1555222251
BeatLink <github@beatlink.simplelogin.com>
2025-01-01 18:09:11 -0500

    FIx display

--- 3-laptop/modules/1-system/10-display.nix
+++ 3-laptop/modules/1-system/10-display.nix
@@ -2,9 +2,10 @@
 {
     boot = {
         initrd = {
-            kernelModules = ["amdgpu"];
+            kernelModules = ["amdgpu" "nvidia"];                 # Load AMD GPU drivers to show external monitor during initrd
             systemd.strip = false;
         };
+        extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
     };
     hardware = {
         graphics = {
@@ -12,23 +13,19 @@
         };
         nvidia = {
             modesetting.enable = true;
-            powerManagement = {
-                enable = true;
-                finegrained = true;
-            };
             open = true;
             nvidiaSettings = true;
             package = config.boot.kernelPackages.nvidiaPackages.stable;
             prime = {
-                offload = {
-                    enable = true;
-                    enableOffloadCmd = true;
-                };
-                #sync.enable = true;
+                #offload = {
+                #    enable = true;
+                #    enableOffloadCmd = true;
+                #};
+                sync.enable = true;
                 amdgpuBusId = "PCI:6:0:0";
                 nvidiaBusId = "PCI:1:0:0";
             };
         };
     };
-    services.xserver.videoDrivers = [ "nvidia" ];
+    services.xserver.videoDrivers = ["nvidia" "amdgpu"];
 }
