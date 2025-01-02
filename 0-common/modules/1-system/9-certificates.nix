746288970d6d9cfd9278f37637e3dc686d09b7e8
BeatLink <github@beatlink.simplelogin.com>
2025-01-02 01:27:01 -0500

    Add TechNet Website Certificates

---
+++ 0-common/modules/1-system/9-certificates.nix
@@ -0,0 +1,11 @@
+
+{ config, lib, pkgs, modulesPath, ... }: 
+{
+    sops.secrets.technet_certificate = {
+        sopsFile = ../../../secrets/secrets.yaml;
+    };
+    security.pki.certificateFiles = [ 
+        config.sops.secrets.technet_certificate.path
+    ]
+
+}
