7d485bf90079336bb000944e04ec1167bf1a7008
BeatLink <github@beatlink.simplelogin.com>
2025-01-05 19:55:21 -0500

    Update Hardware Config

--- 3-laptop/modules/1-system/1-hardware-configuration.nix
+++ 3-laptop/modules/1-system/1-hardware-configuration.nix
@@ -1,9 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-    hardware = {
-        cpu.amd.updateMicrocode = true;                                 # Updates the CPU Microcode
-        enableRedistributableFirmware = true;                           # Enable firmware with a license allowing redistribution.
-    };
     boot = {
         initrd = {
             availableKernelModules = [ 
@@ -12,12 +8,16 @@
                 "usbhid"                                                # For USB Devices
                 "mt7921e"                                               # Wi-Fi Drivers
                 "ideapad_laptop"                                        # Lenovo Drivers (Function Keys, Battery Management, etc)
-                "kvm-amd"                                               # Virtualization for VMs
             ];
         };
+        kernelModules = [ "kvm-amd" ];                                  # Virtualization for VMs
         kernelParams = [
             "amd_pstate=active"                                         # Enables Power Management for AMD CPUs
         ];
+    };
+    hardware = {
+        cpu.amd.updateMicrocode = true;                                 # Updates the CPU Microcode
+        enableRedistributableFirmware = true;                           # Enable firmware with a license allowing redistribution.
     };
     nixpkgs.hostPlatform = "x86_64-linux";                              # This laptop has a 64 bit architecture
 }
