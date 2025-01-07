48b2325794199f559867f1708b5ed821f6db553c
BeatLink <github@beatlink.simplelogin.com>
2025-01-06 19:32:25 -0500

    Separate boot and plymouth

--- 3-laptop/modules/1-system/2-boot.nix
+++ 3-laptop/modules/1-system/2-boot.nix
@@ -12,37 +12,12 @@
         kernel.sysctl."kernel.sysrq" = 1;
         initrd = {
             systemd.enable = true;
-
-            #verbose = false;
         };
         loader = {
             systemd-boot.enable = true;                                 # Use Systemd-Boot to manage booting
             grub.enable = false;                                        # Disable Grub since we're using Systemd-Boot
             efi.canTouchEfiVariables = true;                            # Allows setting boot order, UEFI settings, etc
-            timeout = 0;
+            timeout = lib.mkDefault 5;
         };
-
-        /*plymouth = {
-            enable = true;
-            theme = "double";
-            themePackages = with pkgs; [
-                # By default we would install all themes
-                (adi1090x-plymouth-themes.override {
-                selected_themes = [ "double" ];
-            })
-            ];
-        };
-        # Enable "Silent Boot"
-        consoleLogLevel = 0;
-        kernelParams = [
-            "quiet"
-            "splash"
-            "boot.shell_on_fail"
-            "loglevel=3"
-            "rd.systemd.show_status=false"
-            "rd.udev.log_level=3"
-            "udev.log_priority=3"
-        ];*/
     };
-    services.fstrim.enable = true;
 }
