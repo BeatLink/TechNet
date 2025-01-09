549d81ec74fd6be3f4e5995908cfdc6a524b8e72
BeatLink <github@beatlink.simplelogin.com>
2025-01-09 03:02:01 -0500

    Update Bluetooth

--- 3-laptop/modules/1-system/bluetooth.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, modulesPath, ... }: 
-{  
-    hardware.bluetooth = {
-        enable = true; # enables support for Bluetooth
-        powerOnBoot = true; # powers up the default Bluetooth controller on boot
-    };
-}
