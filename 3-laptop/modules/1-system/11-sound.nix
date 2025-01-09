e851771e01f88eeb234dca57068088c8a856ca67
BeatLink <github@beatlink.simplelogin.com>
2025-01-09 02:58:48 -0500

    Update Sound

--- 3-laptop/modules/1-system/11-sound.nix
+++ 3-laptop/modules/1-system/11-sound.nix
@@ -1,13 +1,15 @@
 { config, pkgs, ... }:
 {  
-    hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-        wireplumber.enable = true;
+    services = {
+        pulseaudio.enable = false;
+        pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+            jack.enable = true;
+            wireplumber.enable = true;
+        };
     };
 }
