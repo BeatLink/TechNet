c27a613e9ce16a55196d5f7cd2da4104d19ad8a0
BeatLink <github@beatlink.simplelogin.com>
2025-01-09 21:19:31 -0500

    Fix plank

--- 3-laptop/modules/1-system/desktop-environment/plank/default.nix
+++ 3-laptop/modules/1-system/desktop-environment/plank/default.nix
@@ -10,96 +10,85 @@
 ###########################################################################################################################################
 
 { config, lib, pkgs, ... }:
-{
+let 
+
+    plankItems = {
+        "01-firefox" =  "file:///var/lib/flatpak/exports/share/applications/org.mozilla.firefox.desktop";
+        "02-keepassxc" = "file:///var/lib/flatpak/exports/share/applications/org.keepassxc.KeePassXC.desktop";
+        "03-trilium" = "file:///run/booted-system/sw/share/applications/Trilium.desktop";
+        "04-nemo" = "file:///run/booted-system/sw/share/applications/nemo.desktop";
+        "05-separator1" = "file:///home/beatlink/.local/share/applications/separator1.desktop";
+        "06-thunderbird" = "file:///var/lib/flatpak/exports/share/applications/org.mozilla.Thunderbird.desktop";
+        "07-whatsapp" = "file:///home/beatlink/.local/share/applications/whatsapp.desktop";
+        "08-whatsapp-private" = "file:///home/beatlink/.local/share/applications/whatsapp-private.desktop";
+        "09-riot" = "file:///var/lib/flatpak/exports/share/applications/im.riot.Riot.desktop";
+        "10-discord" = "file:///var/lib/flatpak/exports/share/applications/com.discordapp.Discord.desktop";
+        "11-separator2" = "file:///home/beatlink/.local/share/applications/separator2.desktop";
+        "12-newsflash" = "file:///var/lib/flatpak/exports/share/applications/io.gitlab.news_flash.NewsFlash.desktop";
+        "13-gmusicbrowser" = "file:///var/lib/flatpak/exports/share/applications/org.gmusicbrowser.gmusicbrowser.desktop";
+        "14-freetube" = "file:///var/lib/flatpak/exports/share/applications/io.freetubeapp.FreeTube.desktop";
+        "15-stremio" = "file:///var/lib/flatpak/exports/share/applications/com.stremio.Stremio.desktop";
+        "16-steam" = "file:///run/booted-system/sw/share/applications/steam.desktop";
+        "17-calibre" = "file:///var/lib/flatpak/exports/share/applications/com.calibre_ebook.calibre.desktop";
+        "18-io.lmms.LMMS" = "file:///var/lib/flatpak/exports/share/applications/io.lmms.LMMS.desktop";
+        "19-pix" = "file:///run/booted-system/sw/share/applications/pix.desktop";
+        "20-separator3" = "file:///home/beatlink/.local/share/applications/separator3.desktop";
+        "21-home-assistant" = "file:///home/beatlink/.local/share/applications/home-assistant.desktop";
+        "22-nextcloud" = "file:///home/beatlink/.local/share/applications/nextcloud.desktop";
+        "23-separator4" = "file:///home/beatlink/.local/share/applications/separator4.desktop";
+        "24-terminal" = "file:///run/booted-system/sw/share/applications/org.gnome.Terminal.desktop";
+        "25-vscodium" = "file:///var/lib/flatpak/exports/share/applications/com.vscodium.codium.desktop";
+        "26-virt-manager" = "file:///run/booted-system/sw/share/applications/virt-manager.desktop";                   
+        "27-separator5" = "file:///home/beatlink/.local/share/applications/separator5.desktop";
+    };
+
+    dockitemFiles = lib.attrsets.concatMapAttrs(name: value: {                  # Generates the dockitem files from the attrset above
+        ".config/plank/dock1/launchers/${name}.dockitem".text = ''
+            [PlankDockItemPreferences]
+            Launcher=${value}
+        '';
+    }) plankItems;
+
+in {
+    services.bamf.enable = true;                                                # Allows plank to know running applications
     home-manager.users.beatlink = { config, pkgs, ... }: {
-        packages = [ pkgs.plank ];                                        # Installs Plank from nixpkgs
         home = {
-            persistence."/Storage/Apps/System/Plank" = {
-                directories = [
-                    ".config/plank"
-                    ".local/share/plank"
-                ];
-                files = [
-                    ".local/share/icons/separator-black-horizontal.png"
-                    ".local/share/icons/separator-black-vertical.png"
-                    ".local/share/icons/separator-blank.png"
-                    ".local/share/icons/separator-white-horizontal.png"
-                    ".local/share/icons/separator-white-vertical.png"
-                    ".local/share/applications/separator1.desktop"
-                    ".local/share/applications/separator2.desktop"
-                    ".local/share/applications/separator3.desktop"
-                    ".local/share/applications/separator4.desktop"
-                    ".local/share/applications/separator5.desktop"
-                ];
-                allowOther = true;
+            packages = [ pkgs.plank ];                                          # Installs Plank from nixpkgs
+            persistence = {
+                "/Storage/Apps/System/Plank" = {                                # Loads persistent data for plank
+                    directories = [
+                        ".local/share/plank"
+                    ];
+                    allowOther = true;
+                };
+                "/Storage/Apps/System/Separators/" = {                          # Loads custom separators used by panels
+                    files = [
+                        ".local/share/icons/separator-black-horizontal.png"
+                        ".local/share/icons/separator-black-vertical.png"
+                        ".local/share/icons/separator-blank.png"
+                        ".local/share/icons/separator-white-horizontal.png"
+                        ".local/share/icons/separator-white-vertical.png"
+                        ".local/share/applications/separator1.desktop"
+                        ".local/share/applications/separator2.desktop"
+                        ".local/share/applications/separator3.desktop"
+                        ".local/share/applications/separator4.desktop"
+                        ".local/share/applications/separator5.desktop"
+                    ];
+                    allowOther = true;
+                };
             };
             file = {
                 ".config/autostart/plank.desktop".source = "${pkgs.plank}/share/applications/plank.desktop";       # Configures plank to autostart on login
-
-                ".config/plank/dock1/launchers/firefox.dockitem".text = ''
-                    [PlankDockItemPreferences]
-                    Launcher=file:///var/lib/flatpak/exports/share/applications/org.mozilla.firefox.desktop
-                '';
-
-                ".config/plank/dock1/launchers/keepassxc.dockitem".text = ''
-                    [PlankDockItemPreferences]
-                    Launcher=file:///var/lib/flatpak/exports/share/applications/org.keepassxc.KeePassXC.desktop
-               '';
-
-                ".config/plank/dock1/launchers/trilium.dockitem".text = ''
-                    [PlankDockItemPreferences]
-                    Launcher=file:///run/booted-system/sw/share/applications/Trilium.desktop
-               '';
-
-
-                ".config/plank/dock1/launchers/WhatsApp.dockitem".text = ''
-                    [PlankDockItemPreferences]
-                    Launcher=file:///home/beatlink/.local/share/applications/whatsapp.desktop
-                '';
-            
-                ".config/plank/dock1/launchers/WhatsApp (Private).dockitem".text = ''
-                    [PlankDockItemPreferences]
-                    Launcher=file:///home/beatlink/.local/share/applications/whatsapp-private.desktop
-                '';
-
-                ".config/plank/dock1/launchers/com.calibre_ebook.calibre.dockitem".text = ''
-                    [PlankDockItemPreferences]
-                    Launcher=file:///var/lib/flatpak/exports/share/applications/com.calibre_ebook.calibre.desktop
-                '';
-            };
+            } // dockitemFiles;
         };
         dconf = {
-            enable = true;                                      # Enables dconf which stores plank settings
+            enable = true;                                                      # Enables dconf which stores plank settings
             settings = {
                 "net/launchpad/plank/docks/dock1" = {
                     alignment = "center";
                     auto-pinning = true;
                     current-workspace-only = false;
-                    dock-items = [ 
-                        "firefox.dockitem"
-                        "keepassxc.dockitem"
-                        "trilium.dockitem"
-                        "nemo.dockitem"
-                        "separator-380c7a99-902c-4742-8cff-e3342b3bf48f.dockitem"
-                        "org.mozilla.Thunderbird.dockitem" "WhatsApp.dockitem"
-                        "WhatsApp.dockitem"
-                        "WhatsApp (Private).dockitem"
-                        "im.riot.Riot.dockitem"
-                        "com.discordapp.Discord.dockitem"
-                        "separator-6c01701d-2e87-4009-86d6-62a88c7bd088.dockitem"
-                        "io.gitlab.news_flash.NewsFlash.dockitem"
-                        "org.gmusicbrowser.gmusicbrowser.dockitem"
-                        "io.freetubeapp.FreeTube.dockitem" "com.stremio.Stremio.dockitem"
-                        "steam.dockitem"
-                        "com.calibre_ebook.calibre.dockitem"
-                        "org.kde.gwenview.dockitem" "io.lmms.LMMS.dockitem"
-                        "separator-3ff87b4f-0ae5-48c7-8276-5b52b7361b3a.dockitem"
-                        "home-assistant.dockitem"
-                        "nextcloud.dockitem"
-                        "org.gnome.Terminal.dockitem"
-                        "com.vscodium.codium.dockitem"
-                        "virt-manager.dockitem"
-                    ];
+                    dock-items = lib.attrsets.mapAttrsToList (name: value: name + ".dockitem") plankItems;
                     hide-delay = 200;
                     hide-mode = "dodge-active";
                     icon-size = 48;
