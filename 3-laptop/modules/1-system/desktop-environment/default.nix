c58e5d826bd8b4eef1ccf642ef7a152dbcb6c07a
BeatLink <github@beatlink.simplelogin.com>
2025-01-11 20:52:56 -0500

    Add notes for desktop environment

--- 3-laptop/modules/1-system/desktop-environment/default.nix
+++ 3-laptop/modules/1-system/desktop-environment/default.nix
@@ -1,6 +1,21 @@
-# Laptop Configuration ###############################################################################################################
-# TODO: Add Notes
+# Desktop Environment #####################################################################################################################
+#
+# This file stores configuration settings on various desktop environments that can be used. 
+#
+# Cinnamon w/ Plank 
+#   This is the current Desktop Environment. I've been using it for approximately 12 years.
+# 
+# KDE
+#   Pretty, featureful and customizable. However, Nvidia Drivers on Wayland are broken which causes issues with system suspending and 
+#   resuming. Also Present Screen does not sort windows by recently used which breaks my hot corner / window switching workflow
+# 
+# Budgie
+#   Pretty and the Raven sidebar is an interesting concept. Not as feature rich as cinnamon (no hot corners)
+# 
 ###########################################################################################################################################
+
+
+
 
 { config, lib, pkgs, ... }:
 {
