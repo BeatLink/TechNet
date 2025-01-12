a9d8db0b059b9b97f6be692b41610acf9af38d26
BeatLink <github@beatlink.simplelogin.com>
2025-01-11 20:41:42 -0500

    Switch KeePassXC to nix

--- 3-laptop/modules/4-apps/core/keepassxc.nix
+++ 3-laptop/modules/4-apps/core/keepassxc.nix
@@ -7,12 +7,13 @@
             ];
             persistence."/Storage/Apps/Core/KeePassXC" = {
                 directories = [
-                    ".var/app/org.keepassxc.KeePassXC"
+                    ".config/keepassxc"
+                    ".cache/keepassxc"                   
                 ];
                 allowOther = true;
             };
             file = {
-                ".config/autostart/org.keepassxc.KeePassXC.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/lib/flatpak/exports/share/applications/org.keepassxc.KeePassXC.desktop";
+                ".config/autostart/org.keepassxc.KeePassXC.desktop".source = "${pkgs.keepassxc}/share/applications/org.keepassxc.KeePassXC.desktop";
             };
         };
     };
