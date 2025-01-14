208452df7614cd2a03a8df6e68afd21369851293
BeatLink <github@beatlink.simplelogin.com>
2025-01-13 19:57:32 -0500

    Update newsflash

--- 3-laptop/modules/4-apps/fun/newsflash.nix
+++ 3-laptop/modules/4-apps/fun/newsflash.nix
@@ -5,7 +5,6 @@
             packages = with pkgs; [ newsflash ];
             persistence."/Storage/Apps/Fun/NewsFlash" = {
                 directories = [
-                    ".var/app/io.gitlab.news_flash.NewsFlash"
                     ".cache/news_flash"
                     ".config/news-flash"
                     ".local/share/news_flash"
