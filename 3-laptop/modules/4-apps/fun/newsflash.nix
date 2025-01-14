7b0c5b96a8b331e5f7feef5419a1aec34ce8225c
BeatLink <github@beatlink.simplelogin.com>
2025-01-13 19:57:05 -0500

    Update Newsflash

--- 3-laptop/modules/4-apps/fun/newsflash.nix
+++ 3-laptop/modules/4-apps/fun/newsflash.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }: 
 {
-
-    services.flatpak.packages = ["flathub:app/io.gitlab.news_flash.NewsFlash//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
             packages = with pkgs; [ newsflash ];
