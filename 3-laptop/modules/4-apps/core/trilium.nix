59df28d7fa7a2bcf219e7856bcc35efb63569da7
BeatLink <github@beatlink.simplelogin.com>
2025-01-19 12:13:04 -0500

    Fix Trilium

--- 3-laptop/modules/4-apps/core/trilium.nix
+++ 3-laptop/modules/4-apps/core/trilium.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
 {
-   
-    environment.systemPackages = [(pkgs.callPackage ./trilium { })];
+    nixpkgs.config.permittedInsecurePackages = [
+        "electron-31.7.7"
+    ];
+    environment.systemPackages = [ pkgs.trilium-next-desktop ];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
             persistence."/Storage/Apps/Core/Trilium" = {
