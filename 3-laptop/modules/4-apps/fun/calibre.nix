e4d37886d54a479b4c61bfdbc397c1f2304dc3eb
BeatLink <github@beatlink.simplelogin.com>
2025-01-26 13:34:35 -0500

    Update Calibre

--- 3-laptop/modules/4-apps/fun/calibre.nix
+++ 3-laptop/modules/4-apps/fun/calibre.nix
@@ -1,8 +1,7 @@
-{ config, pkgs, ... }: 
 {
-    environment.systemPackages = with pkgs; [ calibre ];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
+            packages = with pkgs; [ calibre ];
             persistence."/Storage/Apps/Fun/Calibre" = {
                 directories = [
                     ".cache/calibre"
