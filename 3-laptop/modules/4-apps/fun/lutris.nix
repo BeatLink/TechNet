cfc853f237e3f18fa20b135fba5beb9ac35d22f2
BeatLink <github@beatlink.simplelogin.com>
2025-01-26 14:54:09 -0500

    Update Lutris

--- 3-laptop/modules/4-apps/fun/lutris.nix
+++ 3-laptop/modules/4-apps/fun/lutris.nix
@@ -1,5 +1,6 @@
 {
     home-manager.users.beatlink = { config, pkgs, ... }: {
+        nixpkgs.config.allowUnfree = true;
         home = {
             packages = with pkgs; [ lutris ]; 
             persistence."/Storage/Apps/Fun/Lutris" = {
