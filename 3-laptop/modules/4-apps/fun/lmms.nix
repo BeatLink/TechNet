869c2bef486b79ada2c4476f7c8682daca6cd9f5
BeatLink <github@beatlink.simplelogin.com>
2025-01-26 14:09:39 -0500

    Fix LMMS

--- 3-laptop/modules/4-apps/fun/lmms.nix
+++ 3-laptop/modules/4-apps/fun/lmms.nix
@@ -1,18 +1,21 @@
-{ config, pkgs, ... }: 
 {
-    environment.persistence."/Storage/Apps/Fun/LMMS" = {
-        enable = true;
-        hideMounts = true;
-        users.beatlink = {
-            files = [
-                ".lmmsrc.xml"
-            ];
+    home-manager.users.beatlink = { config, pkgs, ... }: {
+        home = {
+            packages =  with pkgs; [ lmms ];
         };
     };
 
-    home-manager.users.beatlink = { config, pkgs, ... }: {
-        home = {
-            packages = with pkgs; [ lmms ];
+    # Nixos Impermanence is used instead of Home Manager impermanence as the latter does not allow bind mounts, only symlinks
+    # LMMS does NOT like symlinks for its configuration file apparently.
+    environment = {
+        persistence."/Storage/Apps/Fun/LMMS" = {
+            enable = true;
+            hideMounts = true;
+            users.beatlink = {
+                files = [
+                    ".lmmsrc.xml"
+                ];
+            };
         };
     };
 }
