89964212753f313f97b4ab1fc0adf02481852110
BeatLink <github@beatlink.simplelogin.com>
2025-01-29 21:21:07 -0500

    switch discord to native

--- 3-laptop/modules/4-apps/comms/discord.nix
+++ 3-laptop/modules/4-apps/comms/discord.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }: 
 {
-    services.flatpak.packages = ["flathub:app/com.discordapp.Discord//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
+            packages = with pkgs; [ discord ];
             persistence."/Storage/Apps/Comms/Discord" = {
                 directories = [
-                    ".var/app/com.discordapp.Discord"
+                    ".config/discord"
                 ];
                 allowOther = true;
             };
