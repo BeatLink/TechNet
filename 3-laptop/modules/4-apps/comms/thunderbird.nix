c8371e825f7b67dfd431ca3862dd58fcf847d715
BeatLink <github@beatlink.simplelogin.com>
2025-01-29 20:51:29 -0500

    Switch thunderbird to native

--- 3-laptop/modules/4-apps/comms/thunderbird.nix
+++ 3-laptop/modules/4-apps/comms/thunderbird.nix
@@ -1,16 +1,12 @@
 { config, pkgs, ... }: 
 {
-    services.flatpak.packages = [
-        "flathub:app/org.mozilla.Thunderbird//stable"
-        "flathub:app/com.ulduzsoft.Birdtray//stable"
-    ];
-    programs.fuse.userAllowOther = true;
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
+            packages = with pkgs; [ thunderbird ];
             persistence."/Storage/Apps/Comms/Thunderbird" = {
                 directories = [
-                    ".var/app/org.mozilla.Thunderbird"
-                    ".var/app/com.ulduzsoft.Birdtray"
+                    ".cache/thunderbird"
+                    ".thunderbird"
                 ];
                 allowOther = true;
             };
