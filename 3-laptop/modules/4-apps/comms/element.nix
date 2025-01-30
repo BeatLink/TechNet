7a3dcfae146983af5263d885f7e0cce1dd1bd737
BeatLink <github@beatlink.simplelogin.com>
2025-01-29 21:21:13 -0500

    Switch element to native

--- 3-laptop/modules/4-apps/comms/element.nix
+++ 3-laptop/modules/4-apps/comms/element.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }: 
 {
-    services.flatpak.packages = ["flathub:app/im.riot.Riot//stable"];
     home-manager.users.beatlink = { config, pkgs, ... }: {
         home = {
+            packages = with pkgs; [ element-desktop ];
             persistence."/Storage/Apps/Comms/Element" = {
                 directories = [
-                    ".var/app/im.riot.Riot"
+                    ".config/Element"
                 ];
                 allowOther = true;
             };
