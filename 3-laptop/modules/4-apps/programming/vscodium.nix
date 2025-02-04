393c4ed986f4247f381d4ac9996d62c893af2614
BeatLink <github@beatlink.simplelogin.com>
2025-02-04 06:52:01 -0500

    Switch VScodium to native

--- 3-laptop/modules/4-apps/programming/vscodium.nix
+++ 3-laptop/modules/4-apps/programming/vscodium.nix
@@ -1,23 +1,15 @@
-{ config, lib, pkgs, ... }:
 {
-    environment.systemPackages = with pkgs; [ nixd nixfmt-rfc-style ];
-    services.flatpak = {
-        packages = ["flathub:app/com.vscodium.codium//stable"];
-        overrides."com.vscodium.codium" = {
-            filesystems = [
-                "/home/beatlink/.config/git:ro"
-                "host"
-            ];
-        };
-    };
     programs.fuse.userAllowOther = true;
-    home-manager.users.beatlink = { config, pkgs, ... }: {
+    home-manager.users.beatlink = { pkgs, ... }: {
         home = {
+            packages = with pkgs; [ nixd nixfmt-rfc-style vscodium ];
             persistence."/Storage/Apps/Programming/VsCodium" = {
                 directories = [
-                    ".var/app/com.vscodium.codium"
+                    ".config/VSCodium"
+                    ".local/share/codium"                    
                     ".vscode-oss"
                 ];
+                files = [".config/npmrc"];
                 allowOther = true;
             };
         };
