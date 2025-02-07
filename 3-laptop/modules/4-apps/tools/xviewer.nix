146658b38e40f2b993e9e737ea8c1d1adfba7871
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 13:00:07 -0500

    Add Xviewer

---
+++ 3-laptop/modules/4-apps/tools/xviewer.nix
@@ -0,0 +1,14 @@
+{
+    home-manager.users.beatlink = { pkgs, ... }: {
+        home = {
+            packages = with pkgs; [ xviewer ];
+            persistence."/Storage/Apps/Tools/XViewer" = {
+                directories = [
+                    ".config/xviewer"
+                ];
+                allowOther = true;
+            };
+        };
+    };
+}
+
