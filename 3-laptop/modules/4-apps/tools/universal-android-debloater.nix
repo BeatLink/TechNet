adfe8907a480b3cd105c76558e2b328f9fd8c967
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 12:19:06 -0500

    Add universal android debloater

---
+++ 3-laptop/modules/4-apps/tools/universal-android-debloater.nix
@@ -0,0 +1,11 @@
+{
+    home-manager.users.beatlink = { pkgs, ... }: {
+        home = {
+            packages = with pkgs; [ universal-android-debloater ];
+            persistence."/Storage/Apps/Tools/Universal-Android-Debloater" = {
+                allowOther = true;
+            };
+        };
+    };
+}
+
