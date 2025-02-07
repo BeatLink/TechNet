1931bd476b4357b7d86481abe36ebf201f1b1fab
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 09:26:39 -0500

    Switch Decoder to Native

--- 3-laptop/modules/4-apps/tools/decoder.nix
+++ 3-laptop/modules/4-apps/tools/decoder.nix
@@ -1,5 +1,12 @@
-
-{ config, pkgs, ... }: 
 {
-    services.flatpak.packages = ["flathub:app/com.belmoussaoui.Decoder//stable"];
+    home-manager.users.beatlink = { pkgs, ... }: {
+        home = {
+            packages = with pkgs; [ gnome-decoder ];
+            persistence."/Storage/Apps/Tools/Decoder" = {
+                directories = [];
+                allowOther = true;
+            };
+        };
+    };
 }
+
