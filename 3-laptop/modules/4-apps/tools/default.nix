1abe3c20b74fc622c2a35d5540f2255ab80d57ba
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 09:54:27 -0500

    Add simple scan

--- 3-laptop/modules/4-apps/tools/default.nix
+++ 3-laptop/modules/4-apps/tools/default.nix
@@ -10,6 +10,7 @@
         ./dupeguru.nix
         ./inkscape.nix
         ./libreoffice.nix
+        ./simple-scan.nix
         ./virt-manager.nix
         ./pix
     ];
---
+++ 3-laptop/modules/4-apps/tools/simple-scan.nix
@@ -0,0 +1,12 @@
+{
+    home-manager.users.beatlink = { pkgs, ... }: {
+        home = {
+            packages = with pkgs; [ simple-scan ];
+            persistence."/Storage/Apps/Tools/SimpleScan" = {
+                directories = [];
+                allowOther = true;
+            };
+        };
+    };
+}
+
