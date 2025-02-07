454106997852c1027d7c5bc77b6336a4fac17537
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 13:00:28 -0500

    Fix Video Downloader

--- 3-laptop/modules/4-apps/tools/video-downloader.nix
+++ 3-laptop/modules/4-apps/tools/video-downloader.nix
@@ -4,6 +4,7 @@
             packages = with pkgs; [ video-downloader ];
             persistence."/Storage/Apps/Tools/Video-Downloader" = {
                 directories = [
+                    ".cache/youtube-dlp"
                 ];
                 allowOther = true;
             };
