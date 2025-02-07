bb542c67a0fa5656b5cdcd3387665153880341fc
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 09:38:54 -0500

    Add Inkscape

---
+++ 3-laptop/modules/4-apps/tools/inkscape.nix
@@ -0,0 +1,15 @@
+{
+    home-manager.users.beatlink = { pkgs, ... }: {
+        home = {
+            packages = with pkgs; [ inkscape ];
+            persistence."/Storage/Apps/Tools/Inkscape" = {
+                directories = [
+                    ".cache/inkscape"
+                    ".config/inkscape"
+                ];
+                allowOther = true;
+            };
+        };
+    };
+}
+
