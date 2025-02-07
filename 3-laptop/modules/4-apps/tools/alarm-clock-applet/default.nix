1d6ab2527e9db744cb2ee6f05fb1efcf3797451b
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 13:00:18 -0500

    Autostart alarm clock

--- 3-laptop/modules/4-apps/tools/alarm-clock-applet/default.nix
+++ 3-laptop/modules/4-apps/tools/alarm-clock-applet/default.nix
@@ -5,6 +5,9 @@
             persistence."/Storage/Apps/Tools/Alarm-Clock-Applet" = {
                 allowOther = true;
             };
+            file = {
+                ".config/autostart/alarm-clock-applet.desktop".source = "${pkgs.alarm-clock-applet}/share/applications/alarm-clock-applet.desktop";
+            };
         };
         imports = [ ./2-dconf-settings.nix ];
     };
