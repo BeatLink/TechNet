5e352981e981dda33df275ba00ddbacbbc825525
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 09:41:46 -0500

    Switch libreoffice to native

--- 3-laptop/modules/4-apps/tools/libreoffice.nix
+++ 3-laptop/modules/4-apps/tools/libreoffice.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, ... }: 
 {
-    services.flatpak.packages = ["flathub:app/org.libreoffice.LibreOffice//stable"];
-    home-manager.users.beatlink = { config, pkgs, ... }: {
+    home-manager.users.beatlink = { pkgs, ... }: {
         home = {
+            packages = with pkgs; [ libreoffice ];
             persistence."/Storage/Apps/Tools/LibreOffice" = {
                 directories = [
-                    ".var/app/org.libreoffice.LibreOffice"
+                    ".config/libreoffice"
                 ];
                 allowOther = true;
             };
