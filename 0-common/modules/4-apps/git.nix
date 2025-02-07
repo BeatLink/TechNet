112cfedf2e7dbfc3ee73b8b313a4c9d90698bf8c
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 04:48:44 -0500

    Update Git

--- 0-common/modules/4-apps/git.nix
+++ 0-common/modules/4-apps/git.nix
@@ -1,16 +1,14 @@
+{ pkgs, ... }:
 {
-    programs.git = {
-        enable = true;                                                          # For downloading git repos;
-    };
     home-manager.users.beatlink.programs.git = {
         enable = true;
-        userEmail = "github@beatlink.simplelogin.com";
+        userEmail = "git@beatlink.simplelogin.com";
         userName = "BeatLink";
-        #package = pkgs.gitFull;
-        #extraConfig = {
-        #    credential.helper = "${
-        #        pkgs.git.override { withLibsecret = true; }
-        #    }/bin/git-credential-libsecret";
-        #};
+        package = pkgs.gitFull;
+        extraConfig = {
+            core = {
+                autocrlf = "input";
+            };
+        };
     };
 }
