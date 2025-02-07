8ff76641cef5fda818093863ef16917f9c53a2de
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 17:02:35 -0500

    Update KeePassXC

--- 3-laptop/modules/4-apps/core/keepassxc.nix
+++ 3-laptop/modules/4-apps/core/keepassxc.nix
@@ -1,5 +1,9 @@
+{lib, ...}:
 {
+    services.gnome.gnome-keyring.enable  = lib.mkForce false;
+    security.pam.services.login.kwallet.enable = false;
     home-manager.users.beatlink = { pkgs, ... }: {
+        services.gnome-keyring.enable = lib.mkForce false;
         home = {
             packages = with pkgs; [ keepassxc ];
             persistence."/Storage/Apps/Core/KeePassXC" = {
