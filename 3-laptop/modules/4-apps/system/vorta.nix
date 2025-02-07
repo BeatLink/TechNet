2a276892180155c6798c3f400168112c4eb6f434
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 17:02:53 -0500

    Update Vorta

--- 3-laptop/modules/4-apps/system/vorta.nix
+++ 3-laptop/modules/4-apps/system/vorta.nix
@@ -1,32 +1,47 @@
-{ config, pkgs, ... }: 
-
-
-let 
-    prebackupCommand = pkgs.writeShellScript "vorta-prebackup.sh" ''
-        flatpak-spawn --host notify-send -a "Vorta" "Database Unlock Required" "Please unlock the KeePassXC database for server backups."; 
-        flatpak run org.keepassxc.KeePassXC &> /dev/null; 
-        until ssh-add -l &> /dev/null 
-        do 
-            sleep 10; 
-        done 
-        wget --spider "http://uptime-kuma.heimdall.technet/api/push/8ME1iuK3yx?status=up&msg=Backups Started&ping=";
-    '';
-in {
-    environment.systemPackages = with pkgs; [ libnotify ];
-    services.flatpak.packages = [ "flathub:app/com.borgbase.Vorta//stable" ];
-    home-manager.users.beatlink = { config, pkgs, ... }: {
+{
+    home-manager.users.beatlink = { pkgs, ... }: {
         home = {
+            packages = with pkgs; [ 
+                libnotify 
+                vorta
+                (
+                    writeShellScriptBin 
+                    "vorta-server-prebackup.sh" 
+                    ''
+                        #!/usr/bin/env bash
+                        until ssh-add -l &> /dev/null 
+                        do 
+                            notify-send -a "Vorta" "Database Unlock Required" "Please unlock the KeePassXC database for server backups."; 
+                            sleep 10; 
+                        done 
+                        wget --spider "http://uptime-kuma.heimdall.technet/api/push/8ME1iuK3yx?status=up&msg=Backups Started&ping=";
+                    ''
+                )
+                (
+                    writeShellScriptBin
+                    "vorta-server-postbackup.sh"
+                    ''
+                        if [ $returncode == 0 ]; then 
+                            wget --spider "http://uptime-kuma.heimdall.technet/api/push/8ME1iuK3yx?status=up&msg=Backups Completed&ping="; 
+                        else
+                            wget --spider "http://uptime-kuma.heimdall.technet/api/push/8ME1iuK3yx?status=down&msg=Backups Failed&ping=";
+                        fi
+                    ''
+                )
+            ];
             persistence."/Storage/Apps/System/Vorta" = {
                 directories = [
-                    ".var/app/com.borgbase.Vorta"
+                    ".cache/borg"
+                    ".cache/Vorta"
+                    ".config/borg"
+                    ".local/share/Vorta"
+                    ".local/state/Vorta"
                 ];
                 allowOther = true;
             };
             file = {
-                ".var/app/com.borgbase.Vorta/vorta-prebackup.sh".source = prebackupCommand;
-                ".config/autostart/com.borgbase.Vorta.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/lib/flatpak/exports/share/applications/com.borgbase.Vorta.desktop";
+                ".config/autostart/vorta.desktop".source = "${pkgs.vorta}/share/applications/com.borgbase.Vorta.desktop";
             };
         };
     };
 }
-
