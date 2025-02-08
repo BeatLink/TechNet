f8ae5031a1cf0a5bf33940639f7047fd779d1203
BeatLink <github@beatlink.simplelogin.com>
2025-02-07 21:18:26 -0500

    Fix nextcloud and homeassistant

--- 3-laptop/modules/4-apps/technet/home-assistant/default.nix
+++ 3-laptop/modules/4-apps/technet/home-assistant/default.nix
@@ -1,13 +1,12 @@
-{ config, lib, pkgs, ... }:
 {
-    home-manager.users.beatlink = { config, pkgs, ... }: {
+    home-manager.users.beatlink = {
         home.file = {
             ".local/share/icons/home-assistant.png".source = ./home-assistant.png;
 
             ".local/share/applications/home-assistant.desktop".text = ''
                 [Desktop Entry]
                 Name=Home Assistant
-                Exec=flatpak run org.mozilla.firefox https://homeassistant.heimdall.technet
+                Exec=firefox https://homeassistant.heimdall.technet
                 Comment=
                 Terminal=false
                 PrefersNonDefaultGPU=false
