105a2addf877899400afe178193be18c6cc6a80c
BeatLink <github@beatlink.simplelogin.com>
2025-02-16 01:46:40 -0500

    update imports

--- 2-server/modules/1-system/default.nix
+++ 2-server/modules/1-system/default.nix
@@ -2,19 +2,16 @@
 # TODO: Add Notes
 ###########################################################################################################################################
 
-{ config, lib, pkgs, ... }:
 {
     imports = [                                   
         ./1-hardware-configuration.nix
         ./2-boot.nix
-        ./3-disk-config.nix
-        ./4-impermanence.nix
-        ./5-data-drive.nix
-        ./6-software.nix
-        ./7-networking.nix
-        ./8-ssh.nix
-        ./9-docker.nix
-        ./10-borgmatic.nix
-        ./11-utilities.nix
+        ./3-root-drive-disko.nix
+        ./4-data-drive.nix
+        ./5-software.nix
+        ./6-networking.nix
+        ./7-docker.nix
+        ./8-borgmatic.nix
+        ./9-utilities.nix
     ];
 }
