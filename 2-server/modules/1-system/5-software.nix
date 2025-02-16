78036115f65888c0738472a4079cefcb6cc4347c
BeatLink <github@beatlink.simplelogin.com>
2025-02-16 01:41:25 -0500

    Update Software

---
+++ 2-server/modules/1-system/5-software.nix
@@ -0,0 +1,9 @@
+# Software Management##################################################################################################################
+#
+# This section handles the state version
+#
+#######################################################################################################################################                                                   
+
+{
+    system.stateVersion = "23.11";                                      # Sets the base version. Don't change unless reinstalling everything
+}
--- 2-server/modules/1-system/6-software.nix
+++
@@ -1,8 +0,0 @@
-# Software Management################################################################################################################## 
-# This section handles enabling flakes, configuring upgrades and installing additional system packages
-#######################################################################################################################################                                                   
-{ config, lib, pkgs, modulesPath, ... }: 
-{
-    system.stateVersion = "23.11";                                      # Sets the base version. Don't change unless reinstalling everything
-    system.autoUpgrade.allowReboot = lib.mkForce  false;                # Prevents rebooting since LUKS manual decryption is needed  
-}
