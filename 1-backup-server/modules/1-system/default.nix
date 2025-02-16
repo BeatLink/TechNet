9782f4affc192fd131fd6157ee28f54576d83e8a
BeatLink <github@beatlink.simplelogin.com>
2025-02-15 22:37:07 -0500

    Fix imports

--- 1-backup-server/modules/1-system/default.nix
+++ 1-backup-server/modules/1-system/default.nix
@@ -3,7 +3,7 @@
         ./1-hardware-configuration.nix
         ./2-boot.nix
         ./3-root-drive-disko.nix
-        ./4-backup-drive.nix
+        ./4-data-drive.nix
         ./5-software.nix
         ./6-networking.nix
         ./7-borg.nix
