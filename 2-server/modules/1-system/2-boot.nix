e5092f9f83a0848e6aaa12549adf60b4ab52c173
BeatLink <github@beatlink.simplelogin.com>
2025-02-16 01:34:57 -0500

    Update boot

--- 2-server/modules/1-system/2-boot.nix
+++ 2-server/modules/1-system/2-boot.nix
@@ -1,15 +1,9 @@
 # Boot Loader ##########################################################################################################################
+#
 # This section manages misc boot settings
+#
 #######################################################################################################################################
-{ config, lib, pkgs, ... }:
+
 {
-    boot = {
-        supportedFilesystems = [ "btrfs" ];
-        loader = {
-            systemd-boot.enable = true;                                 # Use Systemd-Boot to manage booting
-            grub.enable = false;                                        # Disable Grub since we're using Systemd-Boot
-            efi.canTouchEfiVariables = false;                           # This laptop doesn't play nice with EFI variables (Curse you Acer!)
-        };
-        initrd.systemd.enable = true;
-    };
+    boot.loader.efi.canTouchEfiVariables = false;                         # This laptop doesn't play nice with EFI variables (Curse you Acer!)
 }
