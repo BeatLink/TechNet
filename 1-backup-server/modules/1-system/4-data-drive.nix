53913333e5f90d8826b49b968e7241178e38d0fc
BeatLink <github@beatlink.simplelogin.com>
2025-02-16 14:52:26 -0500

    update data drive

--- 1-backup-server/modules/1-system/4-data-drive.nix
+++ 1-backup-server/modules/1-system/4-data-drive.nix
@@ -15,8 +15,8 @@
         };
     };
     fileSystems."/Storage" = {                                                      # Mounts the drive
-        device = "/dev/disk/by-uuid/4c6831d6-0d99-4d5a-805f-88d7878d6c4b";
-        fsType = "ext4";
-        options = ["defaults" "nofail" "discard"];
+        device = "ragnarok-data-pool/storage";
+        fsType = "zfs";
+        options = ["zfsutil" "nofail" ];
     };
 }
