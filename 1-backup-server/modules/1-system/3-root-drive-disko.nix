b3cfe577d5a92b470721718f83b6fbd0c2359846
BeatLink <github@beatlink.simplelogin.com>
2025-02-15 22:36:28 -0500

    Update Root drive

--- 1-backup-server/modules/1-system/3-root-drive-disko.nix
+++ 1-backup-server/modules/1-system/3-root-drive-disko.nix
@@ -1,3 +1,3 @@
 {
-    disko.devices.disk.root-drive.device = "/dev/disk/by-id/sb-SABRENT_SABRENT_000000000000A-0:0";
+    disko.devices.disk.root-drive.device = "/dev/disk/by-id/ata-SATA_SSD_22020812000605";
 }
