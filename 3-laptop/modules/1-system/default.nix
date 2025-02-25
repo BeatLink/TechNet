541638abef0781d9b70c8388a392123ced48565c
BeatLink <github@beatlink.simplelogin.com>
2025-02-24 23:36:56 -0500

    update default

--- 3-laptop/modules/1-system/default.nix
+++ 3-laptop/modules/1-system/default.nix
@@ -2,25 +2,22 @@
 # TODO: Add Notes
 ###########################################################################################################################################
 
-{ config, lib, pkgs, ... }:
 {
     imports = [                                   
         ./1-hardware-configuration.nix
         ./2-boot.nix
-        ./3-plymouth.nix
-        ./3-disko-root-drive.nix
+        ./3-root-drive-disko.nix
         ./4-disko-data-drive.nix
-        ./5-impermanence.nix
         ./6-sops.nix
         ./7-software.nix
         ./8-networking.nix
-        ./9-ssh.nix
         ./10-display.nix
         ./11-sound.nix
         ./12-bluetooth.nix
         ./13-printing.nix
         ./14-fuse.nix
         ./15-home-folders.nix
+        ./plymouth.nix
         ./desktop-environment
 
     ];
