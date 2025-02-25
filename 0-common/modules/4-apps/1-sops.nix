4ad02dc688726b778ba325b97679735c0695ee30
BeatLink <github@beatlink.simplelogin.com>
2025-02-24 23:25:12 -0500

    Update sops

--- 0-common/modules/4-apps/1-sops.nix
+++ 0-common/modules/4-apps/1-sops.nix
@@ -6,5 +6,6 @@
 
 { pkgs, ... }:
 {
+    sops.age.sshKeyPaths = [ "/persistent/etc/ssh/ssh_host_ed25519_key" ];     # Sets the sops ssh key to persistent
     environment.systemPackages = with pkgs; [ sops ];
 }
