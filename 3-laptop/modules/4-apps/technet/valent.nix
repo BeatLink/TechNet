fef8d0954682ff9cf10611b9401f8fbefcfb395b
BeatLink <github@beatlink.simplelogin.com>
2025-02-25 12:26:12 -0500

    Update Valent

--- 3-laptop/modules/4-apps/technet/valent.nix
+++ 3-laptop/modules/4-apps/technet/valent.nix
@@ -1,1 +1,15 @@
 # Only enable if not using kde
+
+{ config, pkgs, ... }: 
+{
+# services.flatpak.packages = ["flathub:app/ca.andyholmes.Valent//stable"];
+    home.file = {
+        ".config/autostart/ca.andyholmes.Valent.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=ca.andyholmes.Valent
+            Exec=flatpak run --command=valent ca.andyholmes.Valent --gapplication-service
+            X-Flatpak=ca.andyholmes.Valent
+        '';
+    };
+}
