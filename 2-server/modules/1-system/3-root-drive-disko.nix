9535db0df45de836f8cbec0e139b7b481b7ee960
BeatLink <github@beatlink.simplelogin.com>
2025-03-09 17:57:11 -0500

    Update Root drive

--- 2-server/modules/1-system/3-root-drive-disko.nix
+++ 2-server/modules/1-system/3-root-drive-disko.nix
@@ -1,3 +1,3 @@
 {
-    disko.devices.disk.root-drive.device = "/dev/disk/by-id/ata-WDC_WDS120G2G0B-00EPW0_185253802728";
+    disko.devices.disk.root-drive.device = "/dev/disk/by-id/ata-Dogfish_SSD_64GB_5E56255506071556041";
 }
