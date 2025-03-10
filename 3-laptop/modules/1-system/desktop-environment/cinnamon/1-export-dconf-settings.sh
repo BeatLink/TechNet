17c030936b835b5058dc300c6c7ff251920ae33d
BeatLink <github@beatlink.simplelogin.com>
2025-03-09 22:50:06 -0500

    move dconf settings

--- 3-laptop/modules/1-system/desktop-environment/cinnamon/1-export-dconf-settings.sh
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/1-export-dconf-settings.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-dconf dump /org/cinnamon/ | nix-shell -p dconf2nix --run "dconf2nix --root "/org/cinnamon/" > 2-dconf-settings.nix"
+dconf dump /org/cinnamon/ | nix-shell -p dconf2nix --run "dconf2nix --root "/org/cinnamon/" > dconf-settings.nix"
