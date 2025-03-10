0387c5aa8209f99ccf522c500fa11794b98e634f
BeatLink <github@beatlink.simplelogin.com>
2025-03-10 01:00:35 -0500

    Update Default apps

--- 3-laptop/modules/1-system/desktop-environment/cinnamon/default-applications.nix
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/default-applications.nix
@@ -1,12 +1,11 @@
+{ config, ... }:
 {
-    home = {
-        persistence."/Storage/Apps/System/DefaultApps" = {
-            files = [
-                ".config/mimeapps.list"
-                ".config/X-Cinnamon-xdg-terminals.list"
-                ".config/xdg-terminals.list"
-            ];
-            allowOther = true;
-        };
+    xdg.configFile."mimeapps.list".source = config.lib.file.mkOutOfStoreSymlink "/Storage/Apps/System/DefaultApps/.config/mimeapps.list";
+    home.persistence."/Storage/Apps/System/DefaultApps" = {
+        files = [
+            ".config/X-Cinnamon-xdg-terminals.list"
+            ".config/xdg-terminals.list"
+        ];
+        allowOther = true;
     };
 }
