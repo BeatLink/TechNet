09983ad59bcd25f3b943d6666ae4f02abc777b96
BeatLink <github@beatlink.simplelogin.com>
2025-03-14 00:42:11 -0500

    Update Night Light

--- 3-laptop/modules/1-system/desktop-environment/cinnamon/night-light.nix
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/night-light.nix
@@ -6,10 +6,10 @@ with lib.hm.gvariant;
     dconf.settings = {
         "org/cinnamon/settings-daemon/plugins/color" = {
             "night-light-enabled" = true;
-            "night-light-schedule-from" = 21;
+            "night-light-schedule-from" = 18;
             "night-light-schedule-mode" =  "manual";
             "night-light-schedule-to" =  6;
-            "night-light-temperature" =  2700;
+            "night-light-temperature" =  3000;
         };
     };
 }
