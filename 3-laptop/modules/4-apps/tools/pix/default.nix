725ecf85350a2caa7547143e49e541ec13a85473
BeatLink <github@beatlink.simplelogin.com>
2025-03-14 00:38:57 -0500

    Fix pix

--- 3-laptop/modules/4-apps/tools/pix/default.nix
+++ 3-laptop/modules/4-apps/tools/pix/default.nix
@@ -1,6 +1,15 @@
 {
     home-manager.users.beatlink  = { pkgs, ... }: {
-        home.packages = with pkgs; [ pix ];
+        home = {
+            packages = with pkgs; [ pix ];
+            persistence."/Storage/Apps/Tools/Pix" = {
+                directories = [
+                    ".config/pix"
+                ];
+                allowOther = true;
+            };
+        };
+
         dconf.enable = true;                                                        # Enables dconf for Pix Setting Management
         imports = [                                                                 # Imports Pix Dconf Settings
             ./2-dconf-settings.nix
