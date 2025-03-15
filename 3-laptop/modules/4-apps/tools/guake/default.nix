029bdf3699998bc151e5121b0a81319dfdefa6de
BeatLink <github@beatlink.simplelogin.com>
2025-03-15 00:25:43 -0500

    remove xterm

--- 3-laptop/modules/4-apps/tools/guake/default.nix
+++ 3-laptop/modules/4-apps/tools/guake/default.nix
@@ -11,7 +11,7 @@
                 ".config/autostart/guake.desktop".source = "${pkgs.guake}/share/applications/guake.desktop";
             };
         };
-        
+        services.xserver.excludePackages = [ pkgs.xterm ];
         dconf.settings."org/cinnamon/desktop/applications/terminal" = {
             "exec" = "guake";
             "exec-args" = "";
