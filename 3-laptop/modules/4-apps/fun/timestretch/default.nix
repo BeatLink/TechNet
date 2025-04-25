ad1a074b67d8296ce7c3184e36821055f397e345
BeatLink <github@beatlink.simplelogin.com>
2025-04-24 20:20:00 -0500

    Fix timestretch icon

--- 3-laptop/modules/4-apps/fun/timestretch/default.nix
+++ 3-laptop/modules/4-apps/fun/timestretch/default.nix
@@ -1,6 +1,7 @@
 {
         home-manager.users.beatlink = {
         home.file = {
+            ".local/share/icons/timestretch.png".source = ./timestretch.png;
             ".local/share/applications/TimeStretch.desktop".text = ''
                 [Desktop Entry]
                 Name=TimeStretch Player
