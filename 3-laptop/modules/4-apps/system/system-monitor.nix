8edcf5f8ee004e10190d5924d8eca88679d37ba5
BeatLink <github@beatlink.simplelogin.com>
2025-04-24 20:43:00 -0500

    add gnome system monitor

--- 3-laptop/modules/4-apps/system/system-monitor.nix
+++ 3-laptop/modules/4-apps/system/system-monitor.nix
@@ -1,2 +1,14 @@
 #stacer
 #gnome system monitor
+
+
+{
+    home-manager.users.beatlink = { pkgs, ... }: {
+        home = {
+            packages = with pkgs; [ gnome-system-monitor ];
+            persistence."/Storage/Apps/System/Gnome-System-Monitor" = {
+                allowOther = true;
+            };
+        };
+    };
+}
