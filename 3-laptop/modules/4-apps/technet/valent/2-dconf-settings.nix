21e2952340e4b1cb1ff87ac7da5de34ffa4237c9
BeatLink <github@beatlink.simplelogin.com>
2025-05-02 21:01:14 -0500

    Fix valent

--- 3-laptop/modules/4-apps/technet/valent/2-dconf-settings.nix
+++ 3-laptop/modules/4-apps/technet/valent/2-dconf-settings.nix
@@ -35,9 +35,6 @@ with lib.hm.gvariant;
       local-allow = true;
     };
 
-    "ca/andyholmes/valent/device/eec8ec68_56b6_492f_904b_838330bcaa45/plugin/runcommand" = {
-      commands = "{'24da1a0e-920b-4ea2-b166-577bc0a3c307': {'name': 'Shutdown', 'command': 'sudo systemctl poweroff'}, 'e5e5b2a9-9264-42e1-8aee-9ad9773f0802': {'name': 'Sleep', 'command': 'systemctl suspend'}, '07f552db-09e6-4271-bf7c-a6a424e2e231': {'name': 'Lock Screen', 'command': 'cinnamon-screensaver-command -l'}, '31f2fd45-4939-452b-8b87-61a06d0e1fcf': {'name': 'Unlock Screen', 'command': 'cinnamon-screensaver-command -d'}}";
-    };
 
     "ca/andyholmes/valent/device/eec8ec68_56b6_492f_904b_838330bcaa45/plugin/share" = {
       download-folder = "/Storage/Files/Downloads";
