2660d74bbe44961f1fb370e95f9578b6782ca88e
BeatLink <github@beatlink.simplelogin.com>
2025-05-23 00:55:33 -0500

    Fix tilix

--- 3-laptop/modules/4-apps/tools/tilix/default.nix
+++ 3-laptop/modules/4-apps/tools/tilix/default.nix
@@ -12,6 +12,37 @@
         imports = [                                                                 # Imports Pix Dconf Settings
             ./2-dconf-settings.nix
         ];
+        xdg.desktopEntries."com.gexperts.Tilix" = {
+            name = "Tilix";
+            genericName = "Terminal emulator";
+            comment = "A tiling terminal for GNOME";
+            exec = "tilix --focus-window --maximize"; # this is the main fix and the rest is to conform with original
+            terminal = false;
+            type = "Application";
+            startupNotify = true;
+            icon = "com.gexperts.Tilix";
+            categories = [ "System" "TerminalEmulator" "X-GNOME-Utilities" ];
+            settings = {
+                Keywords = "shell;prompt;command;commandline;cmd;terminal;";
+                DBusActivatable = "true";
+            };
+            actions = {
+                "new-window" = {
+                    name = "New Window";
+                    exec = "tilix --action=app-new-window";
+                };
+                "new-session" = {
+                    name = "New Session";
+                    exec = "tilix --action=app-new-session";
+                };
+
+                "preferences" = {
+                    name = "Preferences";
+                    exec = "tilix --preferences";
+                };
+
+            };
+        };
     };
 }
 
