8af37931596ab05687d87cd6bcc9c9387632d2fe
BeatLink <github@beatlink.simplelogin.com>
2025-05-22 22:48:15 -0500

    Add Borg Backup for Odin to Heimdall

---
+++ 2-server/modules/3-services/borg.nix
@@ -0,0 +1,14 @@
+# BorgBackup ##############################################################################################################################
+#
+# Configures the Borg Server for other devices to connect
+#
+###########################################################################################################################################
+
+{
+    services.borgbackup.repos = {
+        laptop = {
+            authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFERN9fyw16t2LvfrrZdO1CpY5ZWEolg2bY1ZF4WF2SU odin-borg-key"];
+            path = "/Storage/Files/Backups/Laptop/Vorta";
+        };
+    };
+}
