18996acfd88d074c79eefb99bfeb3b9403939301
BeatLink <github@beatlink.simplelogin.com>
2025-05-22 22:06:15 -0500

    Fix ventoy

--- 3-laptop/modules/4-apps/tools/ventoy.nix
+++ 3-laptop/modules/4-apps/tools/ventoy.nix
@@ -1,5 +1,12 @@
 {
+    nixpkgs.config.permittedInsecurePackages = [
+        "ventoy-gtk3-1.1.05"
+    ];
+
     home-manager.users.beatlink = { pkgs, ... }: {
+        nixpkgs.config.permittedInsecurePackages = [
+            "ventoy-gtk3-1.1.05"
+        ];
         home = {
             packages =  [ pkgs.ventoy-full-gtk ];
             persistence."/Storage/Apps/Tools/Ventoy" = {
