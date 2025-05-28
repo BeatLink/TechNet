e423a70650a3c5de2992d8b511a174533e6ebaab
BeatLink <github@beatlink.simplelogin.com>
2025-05-27 22:16:21 -0500

    Add home manager for root

--- 0-common/modules/2-users/1-root.nix
+++ 0-common/modules/2-users/1-root.nix
@@ -7,4 +7,16 @@
 {
     users.users.root.hashedPassword = "!";                                      # Disables Root Account Password Login
     nix.settings.trusted-users = [ "root" ];                                    # Sets the Root Account as trusted for nix updates
+    home-manager.users.root = {
+        imports = [ ];
+        home = {
+            username = "root";
+            homeDirectory = "/root";
+            stateVersion = "24.11";                                             # Dont change unless reinstalled.
+            sessionVariables = {
+                EDITOR = "nano";
+            };
+        };
+    };
+
 }
