01dd1efcbbe3d1508bda76788741e1204e649794
BeatLink <github@beatlink.simplelogin.com>
2025-06-02 21:33:18 -0500

    Add Accessibility

---
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/settings/5-accessibility.nix
@@ -0,0 +1,10 @@
+{
+    dconf.settings = {
+        "org/cinnamon/desktop/a11y/keyboard" = {
+            togglekeys-enable-beep = true;
+            togglekeys-enable-osd = true;
+            togglekeys-sound-on = "/Storage/Files/Sounds/Interface Sounds/Linux/Fresh and Clean/stereo/button-toggle-on.ogg";
+            togglekeys-sound-off = "/Storage/Files/Sounds/Interface Sounds/Linux/Fresh and Clean/stereo/button-toggle-off.ogg";
+        };
+    };
+}
