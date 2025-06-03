4c043dd936eb74540a0f3792ef200707dd9bb7bd
BeatLink <github@beatlink.simplelogin.com>
2025-06-02 21:15:36 -0500

    Update Themes

---
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/settings/4-themes.nix
@@ -0,0 +1,28 @@
+{
+    dconf.settings = {
+        "org/cinnamon/theme" = {
+            name = "Mint-Y-Dark-Aqua";
+        };
+        "org/cinnamon/desktop/interface" = {
+            cursor-theme = "Bibata-Modern-Classic";
+            gtk-theme = "Mint-Y-Aqua";
+            gtk-theme-backup = "Mint-Y-Aqua";
+            icon-theme = "Mint-Y-Aqua";
+            icon-theme-backup = "Mint-Y-Aqua";
+        };
+        "org/gnome/desktop/interface" = {
+            cursor-theme = "Bibata-Modern-Classic";
+            gtk-theme = "Mint-Y-Aqua";
+            icon-theme = "Mint-Y-Aqua";
+        };
+        "org/cinnamon/desktop/wm/preferences" = {
+            theme = "Mint-Y";
+            theme-backup = "Mint-Y";
+        };
+        "org/gnome/desktop/wm/preferences" = {
+            theme = "Mint-Y";
+        };
+    };
+}
+
+
