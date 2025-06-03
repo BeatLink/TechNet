f35797cfdbf9f88d9a3ce46bab04c3eddddca282
BeatLink <github@beatlink.simplelogin.com>
2025-06-02 20:42:49 -0500

    Add power

---
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/power.nix
@@ -0,0 +1,14 @@
+{
+    dconf.settings = {
+        "org/cinnamon/settings-daemon/plugins/power" = {
+            button-power = "interactive";
+            critical-battery-action = "suspend";
+            lid-close-ac-action = "blank";
+            lid-close-battery-action = "suspend";
+            sleep-display-ac = 900;
+            sleep-display-battery = 300;
+            sleep-inactive-ac-timeout = 3600;
+            sleep-inactive-battery-timeout = 600;
+        };
+    };
+}
