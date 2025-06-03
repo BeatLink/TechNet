8fbe275066531dec52e80c2e1578f1342b7dead6
BeatLink <github@beatlink.simplelogin.com>
2025-06-02 21:07:14 -0500

    Update Effects

---
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/settings/2-effects.nix
@@ -0,0 +1,11 @@
+{
+    dconf.settings = {
+        "org/cinnamon" = {
+            window-effect-speed = 0;
+            desktop-effects-close = "traditional";
+            desktop-effects-map = "traditional";
+            desktop-effects-minimize = "traditional";
+        };
+
+    };
+}
