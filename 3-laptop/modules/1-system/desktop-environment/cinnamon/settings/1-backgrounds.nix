c7f57e52c9e31a1d31d1f52176ad4e68cd0b9646
BeatLink <github@beatlink.simplelogin.com>
2025-06-02 21:04:33 -0500

    Move Backgrounds

---
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/settings/1-backgrounds.nix
@@ -0,0 +1,13 @@
+{
+    dconf.settings = {
+        "org/cinnamon/desktop/background" = {
+            picture-options = "zoom";
+        };
+
+        "org/cinnamon/desktop/background/slideshow" = {
+            delay = 15;
+            image-source = "directory:///Storage/Files/Pictures/Wallpapers";
+            slideshow-enabled = false;
+        };
+    };
+}
