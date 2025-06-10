a353c5295191e225c168fed8b10074edf48fa267
BeatLink <github@beatlink.simplelogin.com>
2025-06-09 22:19:28 -0500

    Remove unused earphones

--- 3-laptop/modules/4-apps/system/blueman/2-dconf-settings.nix
+++ 3-laptop/modules/4-apps/system/blueman/2-dconf-settings.nix
@@ -16,15 +16,6 @@ with lib.hm.gvariant;
 
     "org/blueman/plugins/recentconns" = {
       max-items = 10;
-      recent-connections = [ {
-        adapter = "30:03:C8:00:01:7A";
-        address = "41:42:DC:6D:EB:20";
-        alias = "KTE-006";
-        icon = "audio-headphones";
-        name = "Audio and input profiles";
-        uuid = "00000000-0000-0000-0000-000000000000";
-        time = "1736203641.3961515";
-      } ];
     };
 
     "org/blueman/plugins/standarditems" = {
