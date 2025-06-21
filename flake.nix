02c1cccca492a6997f0fb442a173c20cd489f405
BeatLink <github@beatlink.simplelogin.com>
2025-06-21 15:53:19 -0500

    Switch flake to unstable small

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         nixpkgs = {
-            url = "github:NixOS/nixpkgs/nixos-unstable";
+            url = "github:NixOS/nixpkgs/nixos-unstable-small";
         };
         disko = {
             url = "github:nix-community/disko";
