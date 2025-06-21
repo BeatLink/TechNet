c1aa32c941123d91329a009b948f83b331064381
BeatLink <github@beatlink.simplelogin.com>
2025-06-21 14:40:20 -0500

    freeze triliumnext to latest version

--- 2-server/modules/3-services/trilium.nix
+++ 2-server/modules/3-services/trilium.nix
@@ -4,7 +4,7 @@
         settings = {
             services = {
                 trilium.service = {
-                    image = "triliumnext/notes:latest";
+                    image = "triliumnext/notes:v0.95.0";
                     container_name = "trilium";
                     restart = "always";
                     volumes = [ 
