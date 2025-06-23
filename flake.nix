229ebbb4a9a81db3d528c7fe0c09495fbcc24bcd
BeatLink <github@beatlink.simplelogin.com>
2025-06-23 07:09:07 -0500

    Update Flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         nixpkgs = {
-            url = "github:NixOS/nixpkgs/nixos-unstable-small";
+            url = "github:NixOS/nixpkgs/nixos-unstable";
         };
         disko = {
             url = "github:nix-community/disko";
