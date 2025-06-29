dc79de64e9bbad42c52af839d425e905a0b542cc
BeatLink <github@beatlink.simplelogin.com>
2025-06-29 00:38:08 -0500

    Update Fwupd

--- 0-common/modules/1-system/1-hardware-configuration.nix
+++ 0-common/modules/1-system/1-hardware-configuration.nix
@@ -1,9 +1,27 @@
-{ modulesPath, ... }:
+{ pkgs, modulesPath, ... }:
 {
     imports = [ 
         (modulesPath + "/installer/scan/not-detected.nix")
     ];
     services.fstrim.enable = true;
     services.fwupd.enable = true;
+
+    systemd.timers.fwupd-auto-update = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+            OnCalendar = "weekly";
+            Persistent = true;
+        };
+    };
+
+    systemd.services.fwupd-auto-update = {
+        script = ''
+            ${pkgs.fwupd}/bin/fwupdmgr refresh --force
+            ${pkgs.fwupd}/bin/fwupdmgr update --no-reboot
+        '';
+        serviceConfig = {
+            Type = "oneshot";
+        };
+    };
 
 }
