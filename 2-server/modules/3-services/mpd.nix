9d1bea7d7ee711de631f2100915073436be5fdf6
BeatLink <github@beatlink.simplelogin.com>
2025-07-03 18:59:45 -0500

    Fix MPD

--- 2-server/modules/3-services/mpd.nix
+++ 2-server/modules/3-services/mpd.nix
@@ -15,6 +15,10 @@
         # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
         XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.beatlink.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
     };
+    firewall = {
+        allowedUDPPorts = [ 6600 ];
+        allowedTCPPorts = [ 6600 ];
+    };
     fileSystems =  {
         "/Storage/Services/MPD/MusicFolder/Music" = {
             depends = [ "/Storage" ];
