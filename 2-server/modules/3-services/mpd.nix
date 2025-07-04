13fd002d4635f6ef3f7a18d9cd959f0d6d46a411
BeatLink <github@beatlink.simplelogin.com>
2025-07-03 19:01:22 -0500

    Fix MPD

--- 2-server/modules/3-services/mpd.nix
+++ 2-server/modules/3-services/mpd.nix
@@ -15,7 +15,7 @@
         # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
         XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.beatlink.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
     };
-    firewall = {
+    networking.firewall = {
         allowedUDPPorts = [ 6600 ];
         allowedTCPPorts = [ 6600 ];
     };
