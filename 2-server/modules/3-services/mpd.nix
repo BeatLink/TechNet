f0f0c867209827ca5fd1f498cc2ab13d0baaddfc
BeatLink <github@beatlink.simplelogin.com>
2025-07-06 12:08:11 -0500

    Fix MPD

--- 2-server/modules/3-services/mpd.nix
+++ 2-server/modules/3-services/mpd.nix
@@ -1,24 +1,22 @@
 { config, ... }: {
-    home-manager.users.beatlink = {
-        services.mpd = {
-            enable = true;
-            musicDirectory = "/Storage/Services/MPD/Music";
-            dataDir = "/Storage/Services/MPD/Data";
-            extraConfig = ''
-                audio_output {
-                    type "pipewire"
-                    name "PipeWire Output"
-                }
-            '';
-            network = {
-                listenAddress = "any";          # if you want to allow non-localhost connections
-                port = 21280;
-            };
+    services.mpd = {
+        enable = true;
+        musicDirectory = "/Storage/Services/MPD/Music";
+        dataDir = "/Storage/Services/MPD/Data";
+        extraConfig = ''
+            audio_output {
+                type "pipewire"
+                name "PipeWire Output"
+            }
+        '';
+        network = {
+            listenAddress = "any";          # if you want to allow non-localhost connections
         };
-        systemd.user.services.mpd.environment = {
-            # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-            XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.beatlink.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
-        };
+        user = "beatlink";
+    };
+    systemd.services.mpd.environment = {
+        # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+        XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.beatlink.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
     };
     networking.firewall = {
         allowedTCPPorts = [ 21280 ];
