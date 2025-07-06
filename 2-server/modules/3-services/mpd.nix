932dd0bfb380e19616673d231ee39a70d31040c9
BeatLink <github@beatlink.simplelogin.com>
2025-07-06 12:37:30 -0500

    Fix MPD

--- 2-server/modules/3-services/mpd.nix
+++ 2-server/modules/3-services/mpd.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{
     services.mpd = {
         enable = true;
         musicDirectory = "/Storage/Services/MPD/Music";
@@ -12,14 +12,9 @@
         network = {
             listenAddress = "any";          # if you want to allow non-localhost connections
         };
-        user = "beatlink";
-    };
-    systemd.services.mpd.environment = {
-        # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-        XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.beatlink.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
     };
     networking.firewall = {
-        allowedTCPPorts = [ 21280 ];
+        allowedTCPPorts = [ 6600 ];
     };
     fileSystems =  {
         "/Storage/Services/MPD/Music/Music" = {
