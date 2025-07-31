9f972d1c9f604c8c6141cdab6abf5da9c8c21b70
BeatLink <github@beatlink.simplelogin.com>
2025-07-30 22:39:03 -0500

    fix blueman

--- 3-laptop/modules/4-apps/system/blueman/2-dconf-settings.nix
+++ 3-laptop/modules/4-apps/system/blueman/2-dconf-settings.nix
@@ -1,3 +1,4 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
 { lib, ... }:
 
 with lib.hm.gvariant;
@@ -10,26 +11,23 @@ with lib.hm.gvariant;
     };
 
     "org/blueman/plugins/autoconnect" = {
-      services = [
-        (mkTuple [ "83:BA:4A:3E:E1:42" "00000000-0000-0000-0000-000000000000" ])
-        (mkTuple [ "41:42:DC:6D:EB:20" "00000000-0000-0000-0000-000000000000" ])
-      ];
+      services = [ (mkTuple [ "83:BA:4A:3E:E1:42" "00000000-0000-0000-0000-000000000000" ]) (mkTuple [ "41:42:DC:6D:EB:20" "00000000-0000-0000-0000-000000000000" ]) ];
     };
 
     "org/blueman/plugins/recentconns" = {
       max-items = 10;
 
       recent-connections = [
-        [
-          (mkDictionaryEntry {
-            "adapter" = "30:03:C8:00:01:7A";
-            "address" = "83:BA:4A:3E:E1:42";
-            "alias" = "KTE-007";
-            "icon" = "audio-headset";
-            "name" = "Audio and input profiles";
-            "uuid" = "00000000-0000-0000-0000-000000000000";
-            "time" = "1753930258.915562";
-          })
+        [  # Outer array element: array of dictionaries
+          mkArray [
+            mkDictionaryEntry "adapter" "30:03:C8:00:01:7A"
+            mkDictionaryEntry "address" "83:BA:4A:3E:E1:42"
+            mkDictionaryEntry "alias" "KTE-007"
+            mkDictionaryEntry "icon" "audio-headset"
+            mkDictionaryEntry "name" "Audio and input profiles"
+            mkDictionaryEntry "uuid" "00000000-0000-0000-0000-000000000000"
+            mkDictionaryEntry "time" "1753930258.915562"
+          ]
         ]
       ];
     };
@@ -37,5 +35,6 @@ with lib.hm.gvariant;
     "org/blueman/plugins/standarditems" = {
       toggle-manager-onclick = true;
     };
+
   };
 }
