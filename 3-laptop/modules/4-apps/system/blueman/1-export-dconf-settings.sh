1e374f7195e5a85e82abdf9264625f5bd6151276
BeatLink <github@beatlink.simplelogin.com>
2025-07-31 01:15:19 -0500

    fix bluetooth

--- 3-laptop/modules/4-apps/system/blueman/1-export-dconf-settings.sh
+++ 3-laptop/modules/4-apps/system/blueman/1-export-dconf-settings.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 
-dconf dump /org/blueman/ | nix-shell -p dconf2nix --run "dconf2nix --root "/org/blueman/" > 2-dconf-settings.nix"
-
-
-dconf dump /org/blueman/ > 2-dconf-settings.ini
+dconf dump /org/blueman/ > settings.dconf
