3538450263278992248635ed00e85d6e3b57d9f9
BeatLink <github@beatlink.simplelogin.com>
2025-08-04 17:47:07 -0500

    Fix ragnarok

--- 1-backup-server/modules/1-system/6-networking.nix
+++ 1-backup-server/modules/1-system/6-networking.nix
@@ -10,16 +10,17 @@
         hostId = "bed2ee51";
         useNetworkd = true;                                                         # Use Systemd-Networkd
         nameservers = [ "10.100.100.1" "8.8.8.8" "1.1.1.1" ];                       # Sets up dns
-        firewall.trustedInterfaces = [ "wireguard0" ];
+        firewall.trustedInterfaces = [ "wireguard0" "wireguard-initrd"];
     };
     sops.secrets.wireguard_private_key.sopsFile = ../../secrets.yaml;
+    sops.secrets.wireguard_initrd_private_key.sopsFile = ../../secrets.yaml;
     boot = {
         initrd = {
             availableKernelModules = [
                 "wireguard"                                             # Needed for wireguard in initrd for remote LUKS unlocking
             ];
             secrets = {                                                 # Sops doesn't work in initrd so we use boot.initrd.secrets
-                "/wireguard_private_key" = config.sops.secrets.wireguard_private_key.path;
+                "/wireguard_private_key" = config.sops.secrets.wireguard_initrd_private_key.path;
             };
             systemd = {
                 services.fix_wireguard_key_perms = {            # The Wireguard privatekey must be owned by systemd-network to be used.
@@ -41,10 +42,11 @@
                 };
                 network = {
                     enable = true;
-                    netdevs."01-wireguard" = {
+                    flushBeforeStage2 = true;
+                    netdevs."01-wireguard-initrd" = {
                         netdevConfig = {
                             Kind = "wireguard";
-                            Name = "wireguard0";
+                            Name = "wireguard-initrd";
                         };
                         wireguardConfig = {
                             PrivateKeyFile = /wireguard_private_key;
@@ -66,12 +68,48 @@
                             networkConfig.DHCP = "ipv4";
                             linkConfig.RequiredForOnline = "routable";
                         };
-                        "01-wireguard" = {
-                            matchConfig.Name = "wireguard0";
-                            address = ["10.100.100.5/24"];
+                        "01-wireguard-initrd" = {
+                            matchConfig.Name = "wireguard-initrd";
+                            address = ["10.100.100.6/24"];
                             dns = [ "10.100.100.1"];
                         };
                     };
+                };
+            };
+        };
+    };
+    systemd = {
+        network = {
+            enable = true;
+            netdevs."01-wireguard" = {
+                netdevConfig = {
+                    Kind = "wireguard";
+                    Name = "wireguard0";
+                };
+                wireguardConfig = {
+                    PrivateKeyFile = config.sops.secrets.wireguard_private_key.path;
+                    ListenPort = 51820;
+                };
+                wireguardPeers = [
+                    {
+                        # Server
+                        PublicKey = "SLW2DFKk+Cf5K5KZl0OLYrEGyqTCqYHBKV2mTA3W2hQ=";
+                        AllowedIPs = ["10.100.100.0/24"];
+                        Endpoint = "72.252.37.234:51820";
+                        PersistentKeepalive = 15;
+                    }
+                ];
+            };
+            networks = {
+                "01-end0" = {
+                    matchConfig.Name = "end0";
+                    networkConfig.DHCP = "ipv4";
+                    linkConfig.RequiredForOnline = "routable";
+                };
+                "01-wireguard" = {
+                    matchConfig.Name = "wireguard0";
+                    address = ["10.100.100.5/24"];
+                    dns = [ "10.100.100.1"];
                 };
             };
         };
