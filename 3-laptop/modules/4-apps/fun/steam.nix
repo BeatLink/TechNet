f115e8a067328946d305e75be67c0a8d14ad4ca8
BeatLink <github@beatlink.simplelogin.com>
2025-08-13 07:48:35 -0500

    Fix steam

--- 3-laptop/modules/4-apps/fun/steam.nix
+++ 3-laptop/modules/4-apps/fun/steam.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }: 
 {
-    programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    programs = {
+        steam = {
+            enable = true;
+            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+            localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+            gamescopeSession.enable = true;
+        };
+        gamemode.enable = true;
     };
 
     environment.systemPackages = with pkgs; [
