5d1e5dbf9a99c3bdf5bce05d7e30d31f2977c2f8
BeatLink <github@beatlink.simplelogin.com>
2025-08-14 20:56:24 -0500

    Fix Cinnamon

--- 3-laptop/modules/1-system/desktop-environment/cinnamon/dconf/default.nix
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/dconf/default.nix
@@ -5,15 +5,15 @@
             path = "/org/cinnamon/";
         };
         org-gnome-desktop = {
-            source = "./org.gnome.desktop.dconf";
+            source = ./org.gnome.desktop.dconf;
             path = "/org/gnome/desktop/";
         };
         org-gtk-settings = {
-            source = "./org.gtk.settings.dconf";
+            source = ./org.gtk.settings.dconf;
             path = "/org/gtk/settings/";
         };
         org-x-apps = {
-            source = "./org.x.apps.dconf";
+            source = ./org.x.apps.dconf;
             path = "/org/x/apps";
         };
     };
