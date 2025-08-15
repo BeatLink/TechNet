44ff21a1a087b95cde4c5a5f07933ca04940b8da
BeatLink <github@beatlink.simplelogin.com>
2025-08-14 21:17:55 -0500

    Setup cinnamon

--- 3-laptop/modules/1-system/10-display.nix
+++ 3-laptop/modules/1-system/10-display.nix
@@ -32,4 +32,19 @@
             lidSwitchDocked = "ignore";
         };
     };
+    systemd.services.set-brightness = {
+        description = "Set default screen brightness";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "multi-user.target" ];
+        serviceConfig = {
+            Type = "oneshot";
+            ExecStart = ''
+            /run/current-system/sw/bin/bash -c 'echo 255 > /sys/class/backlight/amdgpu_bl1/brightness'
+            '';
+        };
+        # You may need this if writing to /sys requires permissions
+        serviceConfig.ExecStartPre = [
+            "/run/current-system/sw/bin/chmod u+w /sys/class/backlight/amdgpu_bl1/brightness"
+        ];
+    };
 }
