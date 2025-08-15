adc8b573cddba78f7aba8d470379cb1fe04354fa
BeatLink <github@beatlink.simplelogin.com>
2025-08-14 21:02:05 -0500

    Fix cinnamon

--- 3-laptop/modules/1-system/desktop-environment/cinnamon/brightness.nix
+++ 3-laptop/modules/1-system/desktop-environment/cinnamon/brightness.nix
@@ -1,20 +1,22 @@
 {pkgs, ...}: {
-    systemd.user.services.set-brightness = {
-        Unit = {
-            Description = "Set default screen brightness";
-            After = [ "multi-user.target" ];
-        };
-        Service = {
-            ExecStartPre = [
-                "${pkgs.coreutils-full}/bin/chmod u+w /sys/class/backlight/amdgpu_bl1/brightness"
-            ];
-            ExecStart = ''
-                ${pkgs.bash}/bin/bash -c 'echo 255 > /sys/class/backlight/amdgpu_bl1/brightness'
-            '';
-            Type = "oneshot";
-        };
-        Install = {
-            WantedBy = [ "multi-user.target" ];
+    systemd.user.services = {
+        set-brightness = {
+            Unit = {
+                Description = "Set default screen brightness";
+                After = [ "multi-user.target" ];
+            };
+            Service = {
+                ExecStartPre = [
+                    "${pkgs.coreutils-full}/bin/chmod u+w /sys/class/backlight/amdgpu_bl1/brightness"
+                ];
+                ExecStart = ''
+                    ${pkgs.bash}/bin/bash -c 'echo 255 > /sys/class/backlight/amdgpu_bl1/brightness'
+                '';
+                Type = "oneshot";
+            };
+            Install = {
+                WantedBy = [ "multi-user.target" ];
+            };
         };
     };
 }
