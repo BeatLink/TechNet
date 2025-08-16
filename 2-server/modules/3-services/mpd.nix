a525a649cdf6b5e7c739c947fcafc323255e1d2c
BeatLink <github@beatlink.simplelogin.com>
2025-08-15 19:00:16 -0500

    Fix mpd

--- 2-server/modules/3-services/mpd.nix
+++ 2-server/modules/3-services/mpd.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
     services.mpd = {
         enable = true;
         dataDir = "/Storage/Services/MPD/Data";
@@ -13,7 +13,35 @@
     networking.firewall = {
         allowedTCPPorts = [ 6600 ];
     };
-    systemd.services.mpd.serviceConfig.SupplementaryGroups = [ "pipewire" ];
+    systemd.services = {
+        mpd.serviceConfig.SupplementaryGroups = [ "pipewire" ];
+        
+        "mpd-set-volume" = {
+            description = "Set MPD volume to 50% at boot";
+            after = [ "mpd.service" "network-online.target" ];
+            wants = [ "mpd.service" "network-online.target" ];
+            serviceConfig = {
+                Type = "oneshot";
+                ExecStart = ''
+                    #!/bin/sh
+                    # Wait until MPD responds to mpc
+                    for i in $(seq 1 30); do
+                        if ${pkgs.mpc}/bin/mpc >/dev/null 2>&1; then
+                            break
+                        fi
+                            sleep 1
+                    done
+
+                    # Set volume to 50%
+                    ${pkgs.mpc}/bin/mpc volume 50
+
+                    # Persist volume so MPD remembers it
+                    ${pkgs.mpc}/bin/mpc save
+                '';
+            };
+            wantedBy = [ "multi-user.target" ];
+        };
+    };
 }
 
 # sudo setfacl -m u:mpd:rwx /Storage/
