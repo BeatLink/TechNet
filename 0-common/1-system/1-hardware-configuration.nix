91d9c324467f6d33606f29533df5aa8e85563e8e
BeatLink <github@beatlink.simplelogin.com>
2025-08-19 16:54:57 -0500

    Update filesystem management

--- 0-common/1-system/1-hardware-configuration.nix
+++ 0-common/1-system/1-hardware-configuration.nix
@@ -3,24 +3,32 @@
     imports = [ 
         (modulesPath + "/installer/scan/not-detected.nix")
     ];
-    services.fstrim.enable = true;
-    services.fwupd.enable = true;
-
-    systemd.timers.fwupd-auto-update = {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-            OnCalendar = "weekly";
-            Persistent = true;
+    services = {
+        fstrim.enable = true;
+        zfs = {
+            autoSnapshot.enable = true;
+            autoScrub.enable = true;
+            trim.enable = true;
         };
+        fwupd.enable = true;
     };
 
-    systemd.services.fwupd-auto-update = {
-        script = ''
-            ${pkgs.fwupd}/bin/fwupdmgr refresh --force
-            ${pkgs.fwupd}/bin/fwupdmgr update --no-reboot
-        '';
-        serviceConfig = {
-            Type = "oneshot";
+    systemd = {
+        timers.fwupd-auto-update = {
+            wantedBy = [ "timers.target" ];
+            timerConfig = {
+                OnCalendar = "weekly";
+                Persistent = true;
+            };
+        };
+        services.fwupd-auto-update = {
+            script = ''
+                ${pkgs.fwupd}/bin/fwupdmgr refresh --force
+                ${pkgs.fwupd}/bin/fwupdmgr update --no-reboot
+            '';
+            serviceConfig = {
+                Type = "oneshot";
+            };
         };
     };
 
