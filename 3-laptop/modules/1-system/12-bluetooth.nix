b324b71d0c275d50f3bd394894f3c0a82a4c726b
BeatLink <github@beatlink.simplelogin.com>
2025-08-19 19:26:27 -0500

    Fix bluetooth

--- 3-laptop/modules/1-system/12-bluetooth.nix
+++ 3-laptop/modules/1-system/12-bluetooth.nix
@@ -2,6 +2,14 @@
     hardware.bluetooth = {
         enable = true;                                  # enables support for Bluetooth
         powerOnBoot = true;                             # powers up the default Bluetooth controller on boot
-        settings.General.ControllerMode = "bredr";      # Disables bluetooth low energy. Fixes a current bug in mediatek wifi drivers
+        settings = {
+            General = {
+                ControllerMode = "bredr";      # Disables bluetooth low energy. Fixes a current bug in mediatek wifi drivers
+                Experimental = true;            # Show battery charge of Bluetooth devices
+                DiscoverableTimeout = "0";  # Always discoverable
+                PairableTimeout = "0";     # Always pairable
+                AutoEnable = "true";       # Enable adapters on boot
+            };
+        };
     };
 }
