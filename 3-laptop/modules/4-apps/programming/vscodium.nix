ac456256eac47238169b427e1cf820f7d8a6e9dc
BeatLink <github@beatlink.simplelogin.com>
2025-08-24 08:59:06 -0500

    Update formatting

--- 3-laptop/modules/4-apps/programming/vscodium.nix
+++ 3-laptop/modules/4-apps/programming/vscodium.nix
@@ -1,16 +1,23 @@
 {
-    home-manager.users.beatlink = { pkgs, ... }: {
-        home = {
-            packages = with pkgs; [ nixd nixfmt-rfc-style vscodium nil];
-            persistence."/Storage/Apps/Programming/VsCodium" = {
-                directories = [
-                    ".config/VSCodium"
-                    ".local/share/codium"                    
-                    ".vscode-oss"
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            home = {
+                packages = with pkgs; [
+                    nixd
+                    nixfmt-rfc-style
+                    vscodium
+                    nil
                 ];
-                files = [".config/npmrc"];
-                allowOther = true;
+                persistence."/Storage/Apps/Programming/VsCodium" = {
+                    directories = [
+                        ".config/VSCodium"
+                        ".local/share/codium"
+                        ".vscode-oss"
+                    ];
+                    files = [ ".config/npmrc" ];
+                    allowOther = true;
+                };
             };
         };
-    };
 }
