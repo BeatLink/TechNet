37f1809cdbc7fab6b0c64fb7d10a2cfd5ba89b1f
BeatLink <github@beatlink.simplelogin.com>
2025-08-24 21:41:06 -0500

    Add tang server

---
+++ 3-laptop/modules/1-system/17-tang.nix
@@ -0,0 +1,8 @@
+{
+    services.tang = {
+        enable = true;
+        ipAddressAllow = [ "10.100.100.0/24" ];
+        listenStream = [ "10.100.100.2:7654" ];
+        networking.firewall.allowedTCPPorts = [ 7654 ];
+    };
+}
--- 3-laptop/modules/1-system/default.nix
+++ 3-laptop/modules/1-system/default.nix
@@ -14,6 +14,7 @@
         ./14-fuse.nix
         ./15-home-folders.nix
         ./16-disk-checking.nix
+        ./17-tang.nix
         ./plymouth.nix
         ./dconf
         ./desktop-environment
