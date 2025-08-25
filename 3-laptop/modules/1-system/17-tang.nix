fe0db8113c07be8031b42901971956cb9382bcce
BeatLink <github@beatlink.simplelogin.com>
2025-08-24 21:42:56 -0500

    Fix laptop tang

--- 3-laptop/modules/1-system/17-tang.nix
+++ 3-laptop/modules/1-system/17-tang.nix
@@ -3,6 +3,6 @@
         enable = true;
         ipAddressAllow = [ "10.100.100.0/24" ];
         listenStream = [ "10.100.100.2:7654" ];
-        networking.firewall.allowedTCPPorts = [ 7654 ];
     };
+    networking.firewall.allowedTCPPorts = [ 7654 ];
 }
