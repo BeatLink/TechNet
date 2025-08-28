adc97956077688a422d166208524a76c7e75622e
BeatLink <github@beatlink.simplelogin.com>
2025-08-27 23:20:23 -0500

    Fix formatting

--- 3-laptop/modules/4-apps/core/trilium.nix
+++ 3-laptop/modules/4-apps/core/trilium.nix
@@ -2,22 +2,26 @@
     nixpkgs.config.permittedInsecurePackages = [
         "electron-31.7.7"
     ];
-    home-manager.users.beatlink = { pkgs, ... }: {
-        nixpkgs.config.permittedInsecurePackages = [
-            "electron-31.7.7"
-        ];
-        home = {
-            packages = with pkgs; [ trilium-desktop ];
-            persistence."/Storage/Apps/Core/Trilium" = {
-                directories = [
-                    ".local/share/trilium-data"
-                    ".config/Trilium Notes"
-                ];
-                allowOther = true;
-            };
-            file = {
-                ".config/autostart/trilium-next.desktop".source = "${pkgs.trilium-desktop}/share/applications/Trilium.desktop";
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            nixpkgs.config.permittedInsecurePackages = [
+                "electron-31.7.7"
+            ];
+
+            home = {
+                packages = with pkgs; [ trilium-desktop ];
+                persistence."/Storage/Apps/Core/Trilium" = {
+                    directories = [
+                        ".local/share/trilium-data"
+                        ".config/Trilium Notes"
+                    ];
+                    allowOther = true;
+                };
+                file = {
+                    ".config/autostart/trilium-next.desktop".source =
+                        "${pkgs.trilium-desktop}/share/applications/Trilium.desktop";
+                };
             };
         };
-    };
 }
