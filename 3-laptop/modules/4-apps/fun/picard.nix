14a1df22b4a65c59ffad3649679d0ca163a6a3e2
BeatLink <github@beatlink.simplelogin.com>
2025-08-29 23:31:23 -0500

    Fix picard

--- 3-laptop/modules/4-apps/fun/picard.nix
+++ 3-laptop/modules/4-apps/fun/picard.nix
@@ -2,7 +2,7 @@
     home-manager.users.beatlink = { config, lib, pkgs, ... }: {
         home = {
             packages = with pkgs; [ picard ];
-            persistence."/Storage/Apps/Core/Picard" = {
+            persistence."/Storage/Apps/Fun/Picard" = {
                 directories = [
                     ".cache/MusicBrainz"
                     ".config/MusicBrainz"
