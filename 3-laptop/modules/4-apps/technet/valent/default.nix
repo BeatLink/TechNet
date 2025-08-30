bf397008c8e68d88445d628083427e8de12ed7f4
BeatLink <github@beatlink.simplelogin.com>
2025-08-29 21:40:48 -0500

    Fix valent

--- 3-laptop/modules/4-apps/technet/valent/default.nix
+++ 3-laptop/modules/4-apps/technet/valent/default.nix
@@ -1,23 +1,32 @@
 {
     networking.firewall = rec {
-        allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+        allowedTCPPortRanges = [
+            {
+                from = 1714;
+                to = 1764;
+            }
+        ];
         allowedUDPPortRanges = allowedTCPPortRanges;
     };
-    home-manager.users.beatlink = { pkgs, ... }: {
-        home = {
-            persistence."/Storage/Apps/TechNet/Valent" = {
-                directories = [
-                    ".cache/valent"
-                    ".config/valent"
-                ];
-                allowOther = true;
-            };
-            file.".config/autostart/valent.desktop".source = "${pkgs.valent}/share/applications/ca.andyholmes.Valent.desktop";       # Configures plank to autostart on login
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            home = {
+                packages = [ pkgs.valent ];
+                persistence."/Storage/Apps/TechNet/Valent" = {
+                    directories = [
+                        ".cache/valent"
+                        ".config/valent"
+                    ];
+                    allowOther = true;
+                };
+                file.".config/autostart/valent.desktop".source =
+                    "${pkgs.valent}/share/applications/ca.andyholmes.Valent.desktop"; # Configures plank to autostart on login
 
+            };
+            dconfImports.valent = {
+                source = ./settings.dconf;
+                path = "/ca/andyholmes/valent/";
+            };
         };
-        dconfImports.valent = {
-            source = ./settings.dconf;
-            path = "/ca/andyholmes/valent/";
-        };        
-    };
 }
