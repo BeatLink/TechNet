8e80594bd81602005e109021d6e5c840f600032f
BeatLink <github@beatlink.simplelogin.com>
2025-08-30 20:11:40 -0500

    Fix stremio

--- 3-laptop/modules/4-apps/fun/stremio.nix
+++ 3-laptop/modules/4-apps/fun/stremio.nix
@@ -1,17 +1,32 @@
+{ pkgs, ... }:
 {
 
-    home-manager.users.beatlink = { pkgs, ... }: {
-        home = {
-            packages = with pkgs; [ stremio ];
-            persistence."/Storage/Apps/Fun/Stremio" = {
-                directories = [
-                    ".cache/Smart Code ltd"
-                    ".config/Smart Code ltd"
-                    ".local/share/Smart Code ltd"
-                    ".stremio-server"
+    nixpkgs.config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+            "qtwebengine-5.15.19"
+        ];
+    };
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            nixpkgs.config = {
+                allowUnfree = true;
+                permittedInsecurePackages = [
+                    "qtwebengine-5.15.19"
                 ];
-                allowOther = true;
+            };
+            home = {
+                packages = with pkgs; [ stremio ];
+                persistence."/Storage/Apps/Fun/Stremio" = {
+                    directories = [
+                        ".cache/Smart Code ltd"
+                        ".config/Smart Code ltd"
+                        ".local/share/Smart Code ltd"
+                        ".stremio-server"
+                    ];
+                    allowOther = true;
+                };
             };
         };
-    };
 }
