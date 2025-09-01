09e8edf55de2534c9684bad8b2df75a98858ff9d
BeatLink <github@beatlink.simplelogin.com>
2025-09-01 10:43:09 -0500

    Fix server sound

--- 2-server/modules/1-system/10-sound.nix
+++ 2-server/modules/1-system/10-sound.nix
@@ -3,10 +3,7 @@
         pulseaudio.enable = false;
         pipewire = {
             enable = true;
-            alsa.enable = true;
-            alsa.support32Bit = true;
             pulse.enable = true;
-            jack.enable = true;
             wireplumber.enable = true;
             systemWide = true;
         };
