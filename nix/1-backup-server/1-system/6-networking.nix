85c8ece4a360a2eb87df0073728e27f7ddc4187d
BeatLink <github@beatlink.simplelogin.com>
2025-09-03 13:27:36 -0500

    Fix backup server networking

--- nix/1-backup-server/1-system/6-networking.nix
+++ nix/1-backup-server/1-system/6-networking.nix
@@ -3,7 +3,12 @@
 # This file configures wireguard and networking settings
 #
 
-{ config, pkgs, inputs, ... }:
+{
+    config,
+    pkgs,
+    inputs,
+    ...
+}:
 {
     networking = {
         # Sets the hostName
@@ -101,7 +106,8 @@
             networks = {
                 "end0" = {
                     matchConfig.Name = "end0";
-                    networkConfig.DHCP = "ipv4";
+                    address = [ "192.168.2.2/24" ];
+                    gateway = [ "192.168.2.1" ];
                     linkConfig.RequiredForOnline = "routable";
                 };
                 "wg0" = {
