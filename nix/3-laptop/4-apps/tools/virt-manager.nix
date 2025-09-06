d5dac45becc0092337cc60020214bf9ecf0cea47
BeatLink <github@beatlink.simplelogin.com>
2025-09-06 18:22:03 -0500

    Fix virt manager persistence

--- nix/3-laptop/4-apps/tools/virt-manager.nix
+++ nix/3-laptop/4-apps/tools/virt-manager.nix
@@ -1,14 +1,32 @@
-{ config, lib, pkgs, ... }:
+{
+    config,
+    lib,
+    pkgs,
+    ...
+}:
 {
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+    environment.persistence."/Storage/Apps/System/Virt-Manager/system" = {
+        directories = [
+            "/etc/libvirt"
+            "/var/lib/libvirt/"
+        ];
+    };
     home-manager.users.beatlink = {
+        home.persistence."/Storage/Apps/System/Virt-Manager/user" = {
+            allowOther = true;
+            directories = [
+                ".config/virt-manager"
+                ".local/share/virt-manager"
+            ];
+        };
         dconf = {
             enable = true;
             settings = {
                 "org/virt-manager/virt-manager/connections" = {
-                    autoconnect = ["qemu:///system"];
-                    uris = ["qemu:///system"];
+                    autoconnect = [ "qemu:///system" ];
+                    uris = [ "qemu:///system" ];
                 };
             };
         };
