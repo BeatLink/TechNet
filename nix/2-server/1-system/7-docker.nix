3fd76a626d2459c7078dc8d1a921fd3de91e129d
BeatLink <github@beatlink.simplelogin.com>
2025-09-06 18:34:53 -0500

    Move docker persistence to data drive

--- nix/2-server/1-system/7-docker.nix
+++ nix/2-server/1-system/7-docker.nix
@@ -3,31 +3,32 @@
 # Almost all server services are provisioned with docker. These settings configure it.
 #
 
-{ pkgs, ... }: 
+{ pkgs, ... }:
 {
-    environment.systemPackages = with pkgs; [                           # Set packages installed on system
+    environment.systemPackages = with pkgs; [
+        # Set packages installed on system
         arion
         docker-client
-    ];  
+    ];
     virtualisation = {
         docker = {
             enable = true;
-            liveRestore = false;                            # Solves hangs on shutdown
+            liveRestore = false; # Solves hangs on shutdown
         };
         arion.backend = "docker";
     };
-    environment.persistence."/persistent".directories = ["/var/lib/docker"];
+    environment.persistence."/Storage/System/Docker".directories = [ "/var/lib/docker" ];
     networking.firewall = {
         allowedTCPPorts = [
-            80                                          # Nginx Services
-            81                                          # Nginx Web Admin
-            443                                         # Nginx Services (HTTPS)
-            53                                          # Pihole DNS
-            82                                          # Pihole Web Admin
+            80 # Nginx Services
+            81 # Nginx Web Admin
+            443 # Nginx Services (HTTPS)
+            53 # Pihole DNS
+            82 # Pihole Web Admin
         ];
         allowedUDPPorts = [
-            53                                          # Pihole DNS
-        ];                                
+            53 # Pihole DNS
+        ];
     };
     users.extraGroups.docker.members = [ "beatlink" ];
 }
