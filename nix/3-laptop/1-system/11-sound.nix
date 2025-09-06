ff40dc124e3245809f780e1eb93443cccd67d63f
BeatLink <github@beatlink.simplelogin.com>
2025-09-06 18:27:48 -0500

    Fix sound

--- nix/3-laptop/1-system/11-sound.nix
+++ nix/3-laptop/1-system/11-sound.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, ... }:
-{  
+{
     security.rtkit.enable = true;
     services = {
         pulseaudio.enable = false;
@@ -12,26 +12,34 @@
             wireplumber.enable = true;
         };
     };
-    environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
-        gst-plugins-good
-        gst-plugins-bad
-        gst-plugins-ugly
-        gst-libav
-    ]);    
-    home-manager.users.beatlink = { pkgs, ... }: {
-        home = {
-            packages = with pkgs; [ sox ];
-            persistence."/Storage/Apps/Tools/Sox" = {
-                directories = [
-                ];
-                allowOther = true;
-            };
-            persistence."/Storage/Apps/System/Pipewire" = {
-                directories = [
-                    ".local/state/wireplumber"
-                ];
-                allowOther = true;
+    environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+        lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+            (
+                with pkgs.gst_all_1;
+                [
+                    gst-plugins-good
+                    gst-plugins-bad
+                    gst-plugins-ugly
+                    gst-libav
+                ]
+            );
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            home = {
+                packages = with pkgs; [ sox ];
+                persistence = {
+                    "/Storage/Apps/Tools/Sox" = {
+                        directories = [ ];
+                        allowOther = true;
+                    };
+                    "/Storage/Apps/System/Pipewire" = {
+                        directories = [
+                            ".local/state/wireplumber"
+                        ];
+                        allowOther = true;
+                    };
+                };
             };
         };
-    };
 }
