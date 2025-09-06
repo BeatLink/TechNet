384d8a12ab9e177bee564b50ce619f5200bfd582
BeatLink <github@beatlink.simplelogin.com>
2025-09-06 18:08:40 -0500

    Fix bluetooth formatting

--- nix/3-laptop/1-system/12-bluetooth.nix
+++ nix/3-laptop/1-system/12-bluetooth.nix
@@ -1,16 +1,16 @@
-{  
+{
     hardware.bluetooth = {
-        enable = true;                                  # enables support for Bluetooth
-        powerOnBoot = true;                             # powers up the default Bluetooth controller on boot
+        enable = true; # enables support for Bluetooth
+        powerOnBoot = true; # powers up the default Bluetooth controller on boot
         settings = {
             General = {
-                ControllerMode = "bredr";      # Disables bluetooth low energy. Fixes a current bug in mediatek wifi drivers
-                Experimental = true;            # Show battery charge of Bluetooth devices
-                DiscoverableTimeout = "0";  # Always discoverable
-                PairableTimeout = "0";     # Always pairable
-                AutoEnable = "true";       # Enable adapters on boot
+                ControllerMode = "bredr"; # Disables bluetooth low energy. Fixes a current bug in mediatek wifi drivers
+                Experimental = true; # Show battery charge of Bluetooth devices
+                DiscoverableTimeout = "0"; # Always discoverable
+                PairableTimeout = "0"; # Always pairable
+                AutoEnable = "true"; # Enable adapters on boot
             };
         };
     };
-    environment.persistence."/Storage/Apps/System/Bluetooth/".directories = ["/var/lib/bluetooth"];
+    environment.persistence."/Storage/Apps/System/Bluetooth".directories = [ "/var/lib/bluetooth" ];
 }
