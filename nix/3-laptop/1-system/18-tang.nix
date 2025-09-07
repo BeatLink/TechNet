932df12c4877d6baba9c080193fd73735d67486e
BeatLink <github@beatlink.simplelogin.com>
2025-09-07 15:22:45 -0500

    Fix tang

--- nix/3-laptop/1-system/18-tang.nix
+++ nix/3-laptop/1-system/18-tang.nix
@@ -8,13 +8,14 @@
             "10.100.100.2:7654"
         ];
     };
-    systemd.sockets.tangd = {
-        wants = [
-            "network-online.target"
-        ];
-        after = [
-            "network-online.target"
-        ];
+    systemd.services."tangd@" = {
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        serviceConfig = {
+            Restart = "always";
+            RestartSec = "1s";
+            StartLimitIntervalSec = 0;
+        };
     };
     networking.firewall.allowedTCPPorts = [ 7654 ];
     environment.persistence."/Storage/Apps/System/Tang" = {
