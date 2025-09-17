2d136a73ffd4021e67646e85f27b5058ad28e838
BeatLink <github@beatlink.simplelogin.com>
2025-09-17 10:17:37 -0500

    Fix hardware configuration

--- nix/3-laptop/1-system/1-hardware-configuration.nix
+++ nix/3-laptop/1-system/1-hardware-configuration.nix
@@ -1,22 +1,25 @@
 {
     boot = {
         initrd = {
-            availableKernelModules = [ 
-                "nvme"                                                  # For NVMe Storage Drives
-                "xhci_pci"                                              # For USB 3 and PCI Devices
-                "usbhid"                                                # For USB Devices
-                "mt7921e"                                               # Wi-Fi Drivers
-                "ideapad_laptop"                                        # Lenovo Drivers (Function Keys, Battery Management, etc)
+            availableKernelModules = [
+                "nvme" # For NVMe Storage Drives
+                "xhci_pci" # For USB 3 and PCI Devices
+                "usbhid" # For USB Devices
+                "mt7921e" # Wi-Fi Drivers
+                "ideapad_laptop" # Lenovo Drivers (Function Keys, Battery Management, etc)
             ];
         };
-        kernelModules = [ "kvm-amd" ];                                  # Virtualization for VMs
+        kernelModules = [ "kvm-amd" ]; # Virtualization for VMs
         kernelParams = [
-            "amd_pstate=active"                                         # Enables Power Management for AMD CPUs
+            "amd_pstate=active" # Enables Power Management for AMD CPUs
         ];
     };
     hardware = {
-        cpu.amd.updateMicrocode = true;                                 # Updates the CPU Microcode
-        enableRedistributableFirmware = true;                           # Enable firmware with a license allowing redistribution.
+        cpu.amd.updateMicrocode = true; # Updates the CPU Microcode
+        enableRedistributableFirmware = true; # Enable firmware with a license allowing redistribution.
     };
-    nixpkgs.hostPlatform = "x86_64-linux";                              # This laptop has a 64 bit architecture
+    nixpkgs.hostPlatform = "x86_64-linux"; # This laptop has a 64 bit architecture
+    services = {
+        thermald.enable = true;
+    };
 }
