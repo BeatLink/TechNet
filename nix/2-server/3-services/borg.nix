8a280c271ef1a24bc5cdebfadf8bf837c8a30fed
BeatLink <github@beatlink.simplelogin.com>
2025-09-16 21:49:59 -0500

    Fix borg

--- nix/2-server/3-services/borg.nix
+++ nix/2-server/3-services/borg.nix
@@ -6,16 +6,12 @@
 {
     services.borgbackup.repos = {
         laptop = {
-            authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFERN9fyw16t2LvfrrZdO1CpY5ZWEolg2bY1ZF4WF2SU odin-borg-key"];
+            authorizedKeys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFERN9fyw16t2LvfrrZdO1CpY5ZWEolg2bY1ZF4WF2SU odin-borg-key"
+            ];
             path = "/Storage/Files/Backups/Laptop/Vorta";
+            group = "beatlink";
+            user = "beatlink";
         };
     };
 }
-
-# sudo setfacl -m u:borg:--x /Storage
-# sudo setfacl -m u:borg:--x /Storage/Files
-# sudo setfacl -m u:borg:--x /Storage/Files/Backups
-# sudo setfacl -m u:borg:--x /Storage/Files/Backups/Laptop
-# sudo setfacl -R -m u:borg:rwX /Storage/Files/Backups/Laptop/Vorta
-# sudo setfacl -R -d -m u:borg:rwX /Storage/Files/Backups/Laptop/Vorta
-#
