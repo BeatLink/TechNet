0f0c2dbcaf1848f1f30f1414268998208f132ec8
BeatLink <github@beatlink.simplelogin.com>
2025-10-01 15:25:07 -0500

    Disable Kernel Parameters

--- nix/5-phone/1-system/2-boot.nix
+++ nix/5-phone/1-system/2-boot.nix
@@ -7,8 +7,8 @@
     boot = {
         loader.efi.canTouchEfiVariables = false;                            # Leaves Tow-Boot alone
         kernelParams =  [
-            "console=tty1"
-            "console=ttyS2,115200n8"
+            #"console=tty1"
+            #"console=ttyS2,115200n8"
         ];
     };
 }
