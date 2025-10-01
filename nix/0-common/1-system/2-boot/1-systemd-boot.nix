750f10f7502647aa725416c7ed4238968c5602af
BeatLink <github@beatlink.simplelogin.com>
2025-10-01 14:52:23 -0500

    Update systemd boot

--- nix/0-common/1-system/2-boot/1-systemd-boot.nix
+++ nix/0-common/1-system/2-boot/1-systemd-boot.nix
@@ -2,12 +2,14 @@
 {
     boot = {
         # Enable Systemd in init ####################################################################################################################
-        initrd.systemd.enable = true;
-
+        initrd = {
+            enable = true;
+            systemd.enable = true;
+        };
         # Use Systemd-Boot instead of GRUB to manage booting and set the timeout to 5 ###############################################################
         loader = {
-            systemd-boot.enable = true;
-            grub.enable = false;
+            systemd-boot.enable = lib.mkDefault true;
+            grub.enable = lib.mkDefault false;
             timeout = lib.mkDefault 5;
         };
     };
