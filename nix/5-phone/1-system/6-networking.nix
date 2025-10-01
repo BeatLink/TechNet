63747728036d9e05216bea236d4693578de5ccaf
BeatLink <github@beatlink.simplelogin.com>
2025-10-01 08:35:30 -0500

    Add networking

--- nix/5-phone/1-system/6-networking.nix
+++ nix/5-phone/1-system/6-networking.nix
@@ -5,5 +5,14 @@
 
         # Sets the Host ID for ZFS
         hostId = "aef23b78";
+        wireless.enable = false;
+        networkmanager.enable = true;
+
+        # FIXME : configure usb rndis through networkmanager in the future.
+        # Currently this relies on stage-1 having configured it.
+        networkmanager.unmanaged = [
+            "rndis0"
+            "usb0"
+        ];
     };
 }
