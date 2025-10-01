fa19ef433b2e983fbbd459af28ebc18b63580d21
BeatLink <github@beatlink.simplelogin.com>
2025-09-30 21:53:38 -0500

    Switch pools to include hostnames

--- nix/3-laptop/1-system/17-disk-usage-monitoring.nix
+++ nix/3-laptop/1-system/17-disk-usage-monitoring.nix
@@ -1,11 +1,20 @@
+{ config, ... }:
 {
-  services.zpool-usage-monitor = {
-    enable = true;
-    baseUrl = "https://uptime-kuma.heimdall.technet/api/push";
-    interval = "10m"; # run every 10 minutes
-    pools = [
-      { pool = "data-pool"; threshold = "90"; endpoint = "lKhOXk3VQ9"; }
-      { pool = "root-pool"; threshold = "90"; endpoint = "BkWAwCBjmu"; }
-    ];
-  };
+    services.zpool-usage-monitor = {
+        enable = true;
+        baseUrl = "https://uptime-kuma.heimdall.technet/api/push";
+        interval = "10m"; # run every 10 minutes
+        pools = [
+            {
+                pool = "data-pool-${config.networking.hostName}";
+                threshold = "90";
+                endpoint = "lKhOXk3VQ9";
+            }
+            {
+                pool = "root-pool-${config.networking.hostName}";
+                threshold = "90";
+                endpoint = "BkWAwCBjmu";
+            }
+        ];
+    };
 }
--- nix/3-laptop/1-system/4-disko-data-drive.nix
+++ nix/3-laptop/1-system/4-disko-data-drive.nix
@@ -1,55 +1,55 @@
-
-
 # Disko Data Drive
 #
-# This section declaratively describes the filesystem structure for the data drive. It is optionally used by disko during installation to format and 
+# This section declaratively describes the filesystem structure for the data drive. It is optionally used by disko during installation to format and
 # partition the data drive. It is also used during boot to find and mount the needed partitions
 #
-
+{ config, ... }:
 {
     disko.devices = {
         disk.data-drive = {
             type = "disk";
             device = "/dev/disk/by-id/nvme-Corsair_MP600_MICRO_A828B42710C0GL";
             content = {
-                type = "gpt";                                       # GPT Partition tables used on modern systems
+                type = "gpt"; # GPT Partition tables used on modern systems
                 partitions = {
-                    zfs-data-partition = {                          # This creates a ZFS partition to be added to the ZFS pool
+                    zfs-data-partition = {
+                        # This creates a ZFS partition to be added to the ZFS pool
                         size = "100%";
                         content = {
                             type = "zfs";
-                            pool = "data-pool";
+                            pool = "data-pool-${config.networking.hostName}";
                         };
                     };
                 };
             };
         };
-        zpool.data-pool = {
+        zpool."data-pool-${config.networking.hostName}" = {
             type = "zpool";
             options = {
                 autotrim = "on";
             };
             rootFsOptions = {
-                mountpoint = "none";                                # We will be mounting the children datasets, not this root one
-                compression = "zstd";                               # Compresses files to save space
-                xattr = "sa";                                       # Allows extended attributes stored in the filesystem inodes
-                acltype = "posix";                                  # Uses posix compliant ACL for extended attributes
-                "com.sun:auto-snapshot" = "false";                  # Prevents autosnapshots in general (will be enabled for specific datasets later)
+                mountpoint = "none"; # We will be mounting the children datasets, not this root one
+                compression = "zstd"; # Compresses files to save space
+                xattr = "sa"; # Allows extended attributes stored in the filesystem inodes
+                acltype = "posix"; # Uses posix compliant ACL for extended attributes
+                "com.sun:auto-snapshot" = "false"; # Prevents autosnapshots in general (will be enabled for specific datasets later)
                 encryption = "aes-256-gcm";
                 keyformat = "passphrase";
                 keylocation = "file:///tmp/encryption.key";
 
-            };  
+            };
             postCreateHook = ''
-                zfs set keylocation="prompt" "data-pool";             # use this to read the key during boot
-            '';                                                      
+                zfs set keylocation="prompt" "data-pool-${config.networking.hostName}";             # use this to read the key during boot
+            '';
             datasets = {
-                storage = {                                           # The dataset for the root filesystem mounted at /
+                storage = {
+                    # The dataset for the root filesystem mounted at /
                     type = "zfs_fs";
                     mountpoint = "/Storage";
                     options = {
                         mountpoint = "legacy";
-                        "com.sun:auto-snapshot" = "true";           # Generates snapshots to persist data
+                        "com.sun:auto-snapshot" = "true"; # Generates snapshots to persist data
                     };
                 };
             };
@@ -58,12 +58,7 @@
     fileSystems = {
         "/Storage".neededForBoot = true;
     };
-    systemd.tmpfiles.rules = [ 
-        "d /Storage 1770 beatlink beatlink"                             # Creates the mount point and sets needed permissions
+    systemd.tmpfiles.rules = [
+        "d /Storage 1770 beatlink beatlink" # Creates the mount point and sets needed permissions
     ];
 }
-
-
-
-
-
