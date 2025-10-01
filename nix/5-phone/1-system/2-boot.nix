18451a131738a90e4a3e4647877342cba995feca
BeatLink <github@beatlink.simplelogin.com>
2025-10-01 15:12:18 -0500

    Update boot

---
+++ nix/5-phone/1-system/2-boot.nix
@@ -0,0 +1,14 @@
+# Boot Loader
+#
+# This section manages misc boot settings
+#
+
+{
+    boot = {
+        loader.efi.canTouchEfiVariables = false;                            # Leaves Tow-Boot alone
+        kernelParams =  [
+            "console=tty1"
+            "console=ttyS2,115200n8"
+        ];
+    };
+}
--- nix/5-phone/1-system/default.nix
+++ nix/5-phone/1-system/default.nix
@@ -1,6 +1,7 @@
 {
-    imports =[
+    imports = [
         ./1-hardware-configuration.nix
+        ./2-boot.nix
         ./3-root-drive-disko.nix
         ./5-software.nix
         ./6-networking.nix
