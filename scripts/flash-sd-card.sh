07f4bd35bff4d3936ecc0d70b9e1ae0696d90b18
BeatLink <github@beatlink.simplelogin.com>
2025-09-30 22:12:07 -0500

    Fix flash sd card script

--- scripts/flash-sd-card.sh
+++ scripts/flash-sd-card.sh
@@ -1,22 +1,64 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash -p gptfdisk zfs parted
+#! nix-shell -i bash -p gum gptfdisk zfs parted
 
 # Inspired by https://github.com/danboid/creating-ZFS-disks-under-Linux/blob/master/README.md
 
-echo "----------------------------------------- Data SD Card Format Script -----------------------------------------"
-echo
-echo "This script format and prepare the SD Card to store TowBoot as well as the backup files and other stateful information."
-echo "This data drive will consist of a GPT partition table containing the Tow Boot Image as well as the encrypted ZFS data storage pool"
-echo
-echo "DANGER: The selected drive will be erased and formatted in its entirety. Double check to ensure that there is no important information on the drive"
-echo 
-read -p "TowBoot Version [2023.07-007]: " TOWBOOT_VERSION
+title() {
+    gum style --foreground "#00ACFF" --border-foreground "#00ACFF" \
+        --border thick --align center --width 150 --margin "1 0" \
+        --padding "1 2" "$@"
+}
+
+pr() {
+    local timestamp
+    timestamp=$(date +"[%H:%M:%S]")
+    gum style --foreground "#00ACFF" "$timestamp $*"
+}
+
+success() {
+    local timestamp
+    timestamp=$(date +"[%H:%M:%S]")
+    gum style --foreground "#00FF7F" "$timestamp $*"
+}
+
+fail() {
+    local timestamp
+    timestamp=$(date +"[%H:%M:%S]")
+    gum style --foreground "#FF4C4C" "$timestamp ❌ $*"
+}
+
+run_checked() {
+    local cmd="$*"
+    eval "$cmd"
+    if [[ $? -ne 0 ]]; then
+        fail "Command failed: $cmd"
+        exit 1
+    fi
+}
+
+choose() {
+    gum choose --item.foreground "#00ACFF" --header.foreground "#00ACFF" \
+        --cursor.foreground "#00ACFF" --header "$1" "${@:2}"
+}
+
+title "Data SD Card Format Script" \
+    "" \
+    "This script format and prepare the SD Card to store TowBoot as well as the backup files and other stateful information." \
+    "" \
+    "This data drive will consist of a GPT partition table containing the Tow Boot Image as well as the encrypted ZFS data storage pool" \
+    "" \
+    "DANGER: The selected drive will be erased and formatted in its entirety. Double check to ensure that there is no important information on the drive"
+
+
+# Get the SD Card
+pr "Select the SD Card that will be formatted"
+DATA_DRIVE=$(lsblk -dn -o NAME | sed 's|^|/dev/|' | gum choose)
+pr "SD Card: $DATA_DRIVE" ""
+
+# Get the Towboot Version
+TOWBOOT_VERSION=$(gum input --placeholder "2023.07-007" --prompt "TowBoot Version: ")
 TOWBOOT_VERSION=${TOWBOOT_VERSION:-"2023.07-007"}
-echo 
-lsblk
-echo
-read -p "Data Drive to Format: " DATA_DRIVE
-echo
+pr "TowBoot Version: $TOWBOOT_VERSION"
 
 # Prints executed commands
 set -x;
@@ -45,8 +87,8 @@ sudo sgdisk --new=2:0:0 --typecode=1:BF00 $DATA_DRIVE && sudo partprobe
 # Get Second Partition
 DATA_PARTITION=${DATA_DRIVE}2
 
-# Remove any old pools
-sudo zpool destroy data-pool-thor
+# Unmount pools
+sudo zpool export data-pool-thor
 
 # Creating ZFS Pool
 sudo zpool create -f -d -m none -o feature@zstd_compress=enabled -o ashift=12 -o autotrim=on data-pool-thor $DATA_PARTITION
