22d1bd2d0d95272e8302e67d11398524319c0995
BeatLink <github@beatlink.simplelogin.com>
2025-10-02 07:21:56 -0500

    Update firmware

--- nix/5-phone/1-system/1-hardware-configuration.nix
+++ nix/5-phone/1-system/1-hardware-configuration.nix
@@ -16,7 +16,7 @@
 
     hardware = {
         enableRedistributableFirmware = true;
-        #firmware = [ config.mobile.device.firmware ];
+        firmware = [ (pkgs.callPackage "${inputs.mobile-nixos}/devices/pine64-pinephone/firmware" { }) ];
     };
 
     environment.systemPackages = [
@@ -28,5 +28,11 @@
     boot.kernelModules = [
         "rtw88_8723cs" # Wifi Card
         "ax88179_178a" # USB Hub
+        "st_lsm6dsx" # Accelerometer
+        "typec_ucsi" # USB Port
+        "axp20x_battery"
+        "axp20x_usb_power"
+        "sun4i_usb_phy"
+        "typec"
     ];
 }
