2ea213bd273c283f46dbb13d44f100528cee4c8f
BeatLink <github@beatlink.simplelogin.com>
2025-10-02 07:22:15 -0500

    formatting

--- nix/5-phone/1-system/2-boot.nix
+++ nix/5-phone/1-system/2-boot.nix
@@ -5,8 +5,8 @@
 
 {
     boot = {
-        loader.efi.canTouchEfiVariables = false;                            # Leaves Tow-Boot alone
-        kernelParams =  [
+        loader.efi.canTouchEfiVariables = false; # Leaves Tow-Boot alone
+        kernelParams = [
             #"console=tty1"
             #"console=ttyS2,115200n8"
         ];
