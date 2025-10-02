389e684966d3dfd80b9910e758958ceeb32efca4
BeatLink <github@beatlink.simplelogin.com>
2025-10-01 22:01:07 -0500

    Update hardware config

--- nix/5-phone/1-system/1-hardware-configuration.nix
+++ nix/5-phone/1-system/1-hardware-configuration.nix
@@ -1,10 +1,30 @@
-{ pkgs, ... }:
 {
+    inputs,
+    config,
+    pkgs,
+    ...
+}:
+{
+
+    #nix.nixPath = [ "mobile-nixos=${inputs.mobile-nixos}" ];
+
+    #imports = [
+    #    (import "/lib/configuration.nix" { device = "pine64-pinephone" })
+    #];
+
     nixpkgs.hostPlatform = "aarch64-linux";
 
     hardware = {
         enableRedistributableFirmware = true;
+        #firmware = [ config.mobile.device.firmware ];
     };
+
+    environment.systemPackages = [
+        pkgs.firefox
+        pkgs.thunderbird
+        (pkgs.callPackage "${inputs.mobile-nixos}/devices/pine64-pinephone/firmware" { })
+    ];
+
     boot.kernelModules = [
         "rtw88_8723cs" # Wifi Card
         "ax88179_178a" # USB Hub
