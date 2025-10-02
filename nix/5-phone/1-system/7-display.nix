8d7210cdd6d513efff1216507ca2468083e9dab9
BeatLink <github@beatlink.simplelogin.com>
2025-10-01 20:24:55 -0500

    Update display

--- nix/5-phone/1-system/7-display.nix
+++ nix/5-phone/1-system/7-display.nix
@@ -1,10 +1,21 @@
 { pkgs, lib, ... }:
 {
-    services.xserver.desktopManager.phosh = {
-        enable = true;
-        user = "beatlink";
-        group = "beatlink";
-        phocConfig.xwayland = "immediate";
+    services = {
+        displayManager.logToFile = false;
+        displayManager.logToJournal = false;
+        xserver = {
+            enable = true; # Enables X11 Server
+            displayManager.lightdm = {
+                enable = true; # Enables LightDM Login Manager
+                greeters.mobile.enable = true;
+            };
+            desktopManager.phosh = {
+                enable = true;
+                user = "beatlink";
+                group = "beatlink";
+                phocConfig.xwayland = "immediate";
+            };
+        };
     };
     # unpatched gnome-initial-setup is partially broken in small screens
     #services.gnome.gnome-initial-setup.enable = false;
