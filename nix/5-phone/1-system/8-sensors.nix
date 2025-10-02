782d642f39d2b714295254ca5b9bc90f40b4c3a2
BeatLink <github@beatlink.simplelogin.com>
2025-10-02 07:20:59 -0500

    Update sensors

--- nix/5-phone/1-system/8-sensors.nix
+++ nix/5-phone/1-system/8-sensors.nix
@@ -1,4 +1,9 @@
 {
     hardware.sensor.iio.enable = true;
+    services.udev.extraHwdb = ''
+        # PinePhone MPU6050 accelerometer orientation fix (+90° clockwise)
+        sensor:modalias:of:NaccelerometerT_null_Cinvensense,mpu6050:*
+        ACCEL_MOUNT_MATRIX=0, 1, 0; -1, 0, 0; 0, 0, 1
+    '';
 
 }
