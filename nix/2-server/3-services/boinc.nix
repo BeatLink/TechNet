b488ab591d47e479029b8b6b1a56a79ca2381232
BeatLink <github@beatlink.simplelogin.com>
2025-10-11 10:09:24 -0500

    Update boinc

--- nix/2-server/3-services/boinc.nix
+++ nix/2-server/3-services/boinc.nix
@@ -1,4 +1,6 @@
+{ inputs, config, ... }:
 {
+    sops.secrets.boinc_env.sopsFile = "${inputs.self}/secrets/2-server.yaml";
     virtualisation.arion.projects.boinc = {
         serviceName = "boinc";
         settings = {
@@ -7,11 +9,14 @@
                     image = "boinc/client";
                     container_name = "boinc";
                     restart = "always";
-                    volumes = [ 
+                    volumes = [
                         "/Storage/Services/BOINC:/var/lib/boinc"
                     ];
-                    expose = [
-                        "31416" 
+                    env_file = [
+                        config.sops.secrets.boinc_env.path
+                    ];
+                    ports = [
+                        "31416:31416"
                     ];
                     networks = [
                         "nginx-proxy-manager_public"
