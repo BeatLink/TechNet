975aaf7da3f14f785902cadceae8cf0d468f6745
BeatLink <github@beatlink.simplelogin.com>
2025-10-13 11:03:17 -0500

    Fix freshrss

--- nix/2-server/3-services/freshrss.nix
+++ nix/2-server/3-services/freshrss.nix
@@ -1,12 +1,34 @@
 { inputs, config, ... }:
 {
-    sops.secrets.freshrss_password.sopsFile = "${inputs.self}/secrets/2-server.yaml";
+    sops.secrets.freshrss_password = {
+        sopsFile = "${inputs.self}/secrets/2-server.yaml";
+        owner = "freshrss";
+        group = "freshrss";
+    };
 
-    services.freshrss = {
-        enable = true;
-        baseUrl = "https://freshrss.heimdall.technet";
-        dataDir = "/Storage/Services/FreshRSS/data";
-        defaultUser = "beatlink";
-        passwordFile = config.sops.secrets.freshrss_password.path;
+    sops.secrets.https_certificate = {
+        sopsFile = "${inputs.self}/secrets/2-server.yaml";
+        owner = "nginx";
+        group = "nginx";
+    };
+    sops.secrets.https_certificate_key = {
+        sopsFile = "${inputs.self}/secrets/2-server.yaml";
+        owner = "nginx";
+        group = "nginx";
+    };
+    services = {
+        freshrss = {
+            enable = true;
+            baseUrl = "https://freshrss.heimdall.technet";
+            dataDir = "/Storage/Services/FreshRSS/data";
+            defaultUser = "beatlink";
+            passwordFile = config.sops.secrets.freshrss_password.path;
+        };
+        nginx.virtualHosts.freshrss = {
+            serverName = "freshrss.heimdall.technet";
+            addSSL = true;
+            sslCertificate = config.sops.secrets.https_certificate.path;
+            sslCertificateKey = config.sops.secrets.https_certificate_key.path;
+        };
     };
 }
