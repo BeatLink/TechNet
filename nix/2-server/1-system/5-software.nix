9afa1cb0813b522e45ec31b56c7c0e9c27a76be2
BeatLink <github@beatlink.simplelogin.com>
2025-10-16 09:17:50 -0500

    Change software update times

--- nix/2-server/1-system/5-software.nix
+++ nix/2-server/1-system/5-software.nix
@@ -6,6 +6,6 @@
 {
     system = {
         stateVersion = "24.11"; # Sets the base version. Don't change unless reinstalling everything
-        autoUpgrade.dates = "22:00";
+        autoUpgrade.dates = "02:00";
     };
 }
