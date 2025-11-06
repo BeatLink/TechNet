fc4418f146d66b9b45fcae0060b2a5a1436ddbdf
BeatLink <github@beatlink.simplelogin.com>
2025-11-06 06:32:05 -0500

    Fix keepassxc

--- nix/3-laptop/4-apps/core/keepassxc.nix
+++ nix/3-laptop/4-apps/core/keepassxc.nix
@@ -1,21 +1,25 @@
-{lib, ...}:
+{ lib, ... }:
 {
-    services.gnome.gnome-keyring.enable  = lib.mkForce false;
+    services.gnome.gnome-keyring.enable = lib.mkForce false;
     security.pam.services.login.kwallet.enable = false;
-    home-manager.users.beatlink = { pkgs, ... }: {
-        services.gnome-keyring.enable = lib.mkForce false;
-        home = {
-            packages = with pkgs; [ keepassxc ];
-            persistence."/Storage/Apps/Core/KeePassXC" = {
-                directories = [
-                    ".config/keepassxc"
-                    ".cache/keepassxc"                   
-                ];
-                allowOther = true;
-            };
-            file = {
-                ".config/autostart/org.keepassxc.KeePassXC.desktop".source = "${pkgs.keepassxc}/share/applications/org.keepassxc.KeePassXC.desktop";
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            services.gnome-keyring.enable = lib.mkForce false;
+            home = {
+                packages = with pkgs; [ keepassxc ];
+                persistence."/Storage/Apps/Core/KeePassXC" = {
+                    directories = [
+                        ".config/keepassxc"
+                        ".cache/keepassxc"
+                        ".mozilla/native-messaging-hosts"
+                    ];
+                    allowOther = true;
+                };
+                file = {
+                    ".config/autostart/org.keepassxc.KeePassXC.desktop".source =
+                        "${pkgs.keepassxc}/share/applications/org.keepassxc.KeePassXC.desktop";
+                };
             };
         };
-    };
 }
