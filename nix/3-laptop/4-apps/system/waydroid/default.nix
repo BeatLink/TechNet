56e1d13fac2486e8aea55c2d84884cfc82fc6ee1
BeatLink <github@beatlink.simplelogin.com>
2025-11-28 02:04:02 -0500

    Work on Waydroid

--- nix/3-laptop/4-apps/system/waydroid/default.nix
+++ nix/3-laptop/4-apps/system/waydroid/default.nix
@@ -1,3 +1,18 @@
+{ pkgs, ... }:
 {
     virtualisation.waydroid.enable = true;
+    environment = {
+        systemPackages = with pkgs; [
+            weston # Waydroid requirement
+        ];
+        persistence."/Storage/System/WayDroid/system".directories = [ "/var/lib/waydroid" ];
+    };
+    home-manager.users.beatlink = {
+        home.persistence."/Storage/Apps/System/Waydroid/user" = {
+            directories = [
+                ".local/share/waydroid"
+            ];
+            allowOther = true;
+        };
+    };
 }
