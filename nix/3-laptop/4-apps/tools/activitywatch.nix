bb716a0277b6f895dc8612f583dc0583e785a2b8
BeatLink <github@beatlink.simplelogin.com>
2025-12-03 10:49:46 -0500

    Work on Activitywatch

--- nix/3-laptop/4-apps/tools/activitywatch.nix
+++ nix/3-laptop/4-apps/tools/activitywatch.nix
@@ -1,17 +1,36 @@
 {
-    home-manager.users.beatlink = {
-        services.activitywatch = {
-            enable = true;
-        };
-        home = {
-            persistence."/Storage/Apps/Tools/ActivityWatch" = {
-                directories = [
-                    ".cache/activitywatch"
-                    ".config/activitywatch"
-                    ".local/share/activitywatch"
-                ];
-                allowOther = true;
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            services.activitywatch = {
+                enable = true;
+                watchers = {
+                    aw-watcher-afk = {
+                        package = pkgs.activitywatch;
+                        settings = {
+                            timeout = 300;
+                            poll_time = 2;
+                        };
+                    };
+
+                    aw-watcher-window = {
+                        package = pkgs.activitywatch;
+                        settings = {
+                            poll_time = 1;
+                            exclude_title = true;
+                        };
+                    };
+                };
+            };
+            home = {
+                persistence."/Storage/Apps/Tools/ActivityWatch" = {
+                    directories = [
+                        ".cache/activitywatch"
+                        ".config/activitywatch"
+                        ".local/share/activitywatch"
+                    ];
+                    allowOther = true;
+                };
             };
         };
-    };
 }
