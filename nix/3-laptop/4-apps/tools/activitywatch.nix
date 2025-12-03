c0de8b0f747009da6e53e870dfd59504c310e7bc
BeatLink <github@beatlink.simplelogin.com>
2025-12-03 10:40:02 -0500

    Fix activitywatch

--- nix/3-laptop/4-apps/tools/activitywatch.nix
+++ nix/3-laptop/4-apps/tools/activitywatch.nix
@@ -1,13 +1,14 @@
 {
-    services.activitywatch = {
-        enable = true;
-    };
     home-manager.users.beatlink = {
+        services.activitywatch = {
+            enable = true;
+        };
         home = {
             persistence."/Storage/Apps/Tools/ActivityWatch" = {
                 directories = [
                     ".cache/activitywatch"
                     ".config/activitywatch"
+                    ".local/share/activitywatch"
                 ];
                 allowOther = true;
             };
