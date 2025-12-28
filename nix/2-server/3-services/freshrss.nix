1f0517f1dc2e67d56cb70d1a85728658c4973d98
BeatLink <github@beatlink.simplelogin.com>
2025-12-28 13:41:19 -0500

    Fix freshrss

--- nix/2-server/3-services/freshrss.nix
+++ nix/2-server/3-services/freshrss.nix
@@ -13,11 +13,6 @@
                     container_name = "freshrss";
                     hostname = "freshrss";
                     restart = "always";
-                    logging = {
-                        options = {
-                            max-size = "10m";
-                        };
-                    };
                     volumes = [
                         "/Storage/Services/FreshRSS/data:/data"
                         "/Storage/Services/FreshRSS/extensions:/extensions"
@@ -26,10 +21,7 @@
                     environment = {
                         TZ = "America/Jamaica";
                         CRON_MIN = "3,33";
-                        TRUSTED_PROXY = [
-                            "172.16.0.1/12"
-                            "192.168.0.1/16"
-                        ];
+                        TRUSTED_PROXY = "172.16.0.1/12 192.168.0.1/16";
                     };
                     healthcheck = {
                         test = [
@@ -38,7 +30,6 @@
                         ];
                         timeout = "10s";
                         start_period = "60s";
-                        start_interval = "11s";
                         interval = "75s";
                         retries = 3;
                     };
