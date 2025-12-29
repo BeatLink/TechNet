7973a3bf90e02e1fb8ad2dee7781fb54cbf42076
BeatLink <github@beatlink.simplelogin.com>
2025-12-29 18:45:36 -0500

    Fix scrcpy

--- nix/3-laptop/4-apps/technet/scrcpy.nix
+++ nix/3-laptop/4-apps/technet/scrcpy.nix
@@ -1,19 +1,33 @@
-{
-    home-manager.users.beatlink = {
-        home.file = {
-            #".local/share/icons/home-assistant.png".source = ./home-assistant.png;
+# Android ADB
 
-            ".local/share/applications/scrcpy.desktop".text = ''
-                [Desktop Entry]
-                Name=ScrCpy
-                Exec=scrcpy --tcpip=10.100.100.5 --render-driver=opengles2
-                Comment=
-                Terminal=false
-                PrefersNonDefaultGPU=false
-                Icon=phone
-                Categories=Network
-                Type=Application
-            '';
+# https://nixos.wiki/wiki/Android
+{
+    home-manager.users.beatlink =
+        { pkgs, ... }:
+        {
+            home = {
+                file = {
+                    #".local/share/icons/home-assistant.png".source = ./home-assistant.png;
+
+                    ".local/share/applications/scrcpy.desktop".text = ''
+                        [Desktop Entry]
+                        Name=ScrCpy
+                        Exec=${pkgs.scrcpy}/bin/scrcpy --tcpip=10.100.100.5 --render-driver=opengles2
+                        Comment=
+                        Terminal=false
+                        PrefersNonDefaultGPU=false
+                        Icon=phone
+                        Categories=Network
+                        Type=Application
+                    '';
+                };
+                persistence."/Storage/Apps/TechNet/Scrcpy" = {
+                    directories = [
+                        ".android"
+                    ];
+                    allowOther = true;
+                };
+
+            };
         };
-    };
 }
