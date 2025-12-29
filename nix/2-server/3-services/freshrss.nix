c379fce1c246ed0cde9995de99bd0b02dd5b35cb
BeatLink <github@beatlink.simplelogin.com>
2025-12-28 20:21:08 -0500

    Fix freshrss

--- nix/2-server/3-services/freshrss.nix
+++ nix/2-server/3-services/freshrss.nix
@@ -1,6 +1,11 @@
 { inputs, ... }:
 {
-
+    # README: Feed updates will fail if IPV6 is disabled on the host system. This can be solved by setting the following
+    #   ./data/config.php
+    #         'curl_options' => array (
+    #               CURLOPT_DNS_SERVERS => '8.8.8.8,1.1.1.1',
+    #               CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
+    #           ),
     sops.secrets.freshrss_password = {
         sopsFile = "${inputs.self}/secrets/2-server.yaml";
     };
@@ -22,6 +27,8 @@
                         TZ = "America/Jamaica";
                         CRON_MIN = "3,33";
                         TRUSTED_PROXY = "172.16.0.1/12 192.168.0.1/16";
+                        BASE_URL = "https://freshrss.heimdall.technet";
+                        SERVER_DNS = "freshrss.heimdall.technet";
                     };
                     healthcheck = {
                         test = [
@@ -38,10 +45,6 @@
                     ];
                     networks = [
                         "nginx-proxy-manager_public"
-                    ];
-                    dns = [
-                        "8.8.8.8"
-                        "1.1.1.1"
                     ];
                 };
             };
