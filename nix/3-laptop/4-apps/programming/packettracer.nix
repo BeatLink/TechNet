2d5c5e9d84f836c40e48a6ae6b2a714cee5acc8f
BeatLink <github@beatlink.simplelogin.com>
2026-01-13 10:13:15 -0500

    fix home manager, disable packettracer

--- nix/3-laptop/4-apps/programming/packettracer.nix
+++ nix/3-laptop/4-apps/programming/packettracer.nix
@@ -1,9 +1,16 @@
 {
+    nixpkgs.config.permittedInsecurePackages = [
+        "ciscoPacketTracer8-8.2.2"
+    ];
     home-manager.users.beatlink =
         { pkgs, ... }:
         {
+            nixpkgs.config.permittedInsecurePackages = [
+                "ciscoPacketTracer8-8.2.2"
+            ];
             home = {
-                packages = with pkgs; [ ciscoPacketTracer8 ];
+
+                packages = with pkgs; [ (pkgs-unstable.ciscoPacketTracer8.override { packetTracerSource = /path/to/packettracer.deb; }) ];
                 persistence."/Storage/Apps/Programming/PacketTracer" = {
                     directories = [
                     ];
