e4e770db88385c95b464fd415aff9b0e76bf640c
BeatLink <git@beatlink.simplelogin.com>
2026-01-26 20:11:39 -0500

    Add github ssh key

--- nix/0-common/3-services/1-ssh.nix
+++ nix/0-common/3-services/1-ssh.nix
@@ -103,6 +103,12 @@
                     "odin-boot"
                 ];
             };
+            "github.com" = {
+                publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+                hostNames = [
+                    "github.com"
+                ];
+            };
         };
         extraConfig = ''
 
