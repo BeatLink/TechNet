6dfce7a41d573a83f6187be7491ecdb717b6324a
BeatLink <git@beatlink.simplelogin.com>
2026-02-03 19:14:16 -0500

    Update Certificates

--- nix/0-common/1-system/7-security/1-certificates.nix
+++ nix/0-common/1-system/7-security/1-certificates.nix
@@ -7,16 +7,35 @@
     security.pki.certificates = [
         ''
             -----BEGIN CERTIFICATE-----
-            MIIBszCCAWWgAwIBAgIIDyT0Mq0rsn8wBQYDK2VwMF8xCzAJBgNVBAYTAkpNMREw
-            DwYDVQQIEwhLaW5nc3RvbjEQMA4GA1UEChMHVGVjaE5ldDErMCkGA1UEAxMiVGVj
-            aE5ldCBSb290IENlcnRpZmljYXRlIEF1dGhvcml0eTAeFw0yNjAyMDMyMzM1MDBa
-            Fw0zNjAyMDMyMzM1MDBaMF8xCzAJBgNVBAYTAkpNMREwDwYDVQQIEwhLaW5nc3Rv
-            bjEQMA4GA1UEChMHVGVjaE5ldDErMCkGA1UEAxMiVGVjaE5ldCBSb290IENlcnRp
-            ZmljYXRlIEF1dGhvcml0eTAqMAUGAytlcAMhAHm4ea7xqqCACEOCosTl+PyxcQLI
-            P7PvuERRFy8Rhiauoz8wPTAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBQpanCl
-            fgkDcSfW6fZBaLJNFJBwEjALBgNVHQ8EBAMCAQYwBQYDK2VwA0EAuD2ofrsVYrKH
-            hhByVvgNkLybyjtj2oa9fKg/kZpdQEHARD4AMsV5gd6U5WfsGSC8TjeCounkGXAt
-            dCWSMxRlCQ==
+            MIIFWTCCA0GgAwIBAgIIdlXGX3YfeGIwDQYJKoZIhvcNAQEMBQAwTDELMAkGA1UE
+            BhMCSk0xETAPBgNVBAgTCEtpbmdzdG9uMRAwDgYDVQQKEwdUZWNoTmV0MRgwFgYD
+            VQQDEw9UZWNoTmV0IFJvb3QgQ0EwHhcNMjYwMjA0MDAwNjAwWhcNMzYwMjA0MDAw
+            NjAwWjBMMQswCQYDVQQGEwJKTTERMA8GA1UECBMIS2luZ3N0b24xEDAOBgNVBAoT
+            B1RlY2hOZXQxGDAWBgNVBAMTD1RlY2hOZXQgUm9vdCBDQTCCAiIwDQYJKoZIhvcN
+            AQEBBQADggIPADCCAgoCggIBALMBV4Y+X6HyN4Oip+Aq/W2uHLzpfjBBxo8iFegR
+            W6tCqyh5tH3o001eUKB96DBAi89eknKa8PxLAET2ae38ZUipOR1T0kuVXaGVyMYj
+            cC4E0DKuGiFayMojDvS7Jrz1Xru4l+07Zm0lbQe+I0yCO2xAak1zwh513B8cttNn
+            Aq1tLEL8N1oJElE5musIzWSNUT4978rC+XRr8VLUfAi0Xy41SxyRNp85pQS063hm
+            Jp9TPsn2vtgkBzWH9eNjrxDfSbltaSebLt4yHC6jvrlM9jg4el0jC3/14C70QOPT
+            kG5ql/7tEIqK4Nwe2+6A3aeeMtq8VefKnzJ2kJP9sCWIgxh/+fq74FP6rSjdNNWa
+            Dm3LsKLI6l6tEkN7utoo7mfmnBBJfJXIO7i6kjLmLM9gzem8CZnYQzHTObmugZNT
+            Krssq41YqhxGpfk4JjOKDrdxyAcPvqLXkr904cqEcWLkM6ehRA40Png4aUQf1/od
+            uxcwiBThbJCUVJzfYC1Ocl4BS5EtoxDxjeC2N4qIaeO+rUnlhCtns3S+slarLzm3
+            jyoUufxRhUZVo1ZDJ/ctOZd8YkrhO5GFo/m6xH+VW9F/kjO+eGinnsYRIkrAS4XI
+            JxOD0iBlaurIXeDxV51C0IYItTnfl7yOxrJfSii5fEjYaXRbTe0W3Gasg7Qqesdo
+            YhmpAgMBAAGjPzA9MA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFGTto0C5t3EM
+            ABxZnkBEIYA+l+KRMAsGA1UdDwQEAwIBBjANBgkqhkiG9w0BAQwFAAOCAgEAU2ok
+            wcieEN0wMTJF7YtWJkNj6dHSDVNXcfpct5DrTOSCLiKHo/olPecAyyagufGUZ4Rj
+            Awg5bjZvwriG5jJ+0h9MLhf+iYr/Dnj6cTZu+eodBb5YO37N2dHB3HPy3Pg0xB6R
+            iQe+Uus35TEXWO296ixXGT2rV/jP1KjTd72YqxDrqZZvFzsaDlj1R9V6I3eewHpR
+            BIW+uGbiYICQ9KAsrsioj/IblS9OZqDFOoDUZBgrOxs8eIMBJo9+/Z3Mn3CnBAjO
+            OISBDwnDwO5Y7h0lBehq34u70hQC8HeZ6xhLpsbgYExET28uz+bfeAZFn1F7/3an
+            EkE3xiPRNUUirt+LJoCW0S5q4wn0d/b/7jywlEYIdgmrloxnZT9vk5Vz/2WLMjao
+            Rjc7Gvhv9eTxm5n8m7aqXXcfPoyGd6NqtpR609wyQsIMSst6dCZgVg0rwNdKM3lK
+            HmRbnCA4F8axj6Jn8nQnNrTnNpswySO1Ft3PsGwU+KeuhvJevcfex1lsvEX8HjT3
+            yLarPQEodt7yxzy0BxisI4iC6eK26K2ELv0pUwTrIruhixbuJ9wxlvtQWQ8mK62s
+            jCAihoo+pZqrGO+0YQz6jzaRrRKcCAK3Cpt5buPMWa5N8EM1gspZEbeDovMmM++T
+            nDYVp9D3nk4qw9bH5j/bLK18qpYJONpP4aTdDdQ=
             -----END CERTIFICATE-----
         ''
     ];
