876c8e999f2c03fc5ef7c3b81eb441a6c7c30a6c
BeatLink <git@beatlink.simplelogin.com>
2026-02-04 14:40:29 -0500

    Update networking

--- nix/1-backup-server/1-system/6-networking.nix
+++ nix/1-backup-server/1-system/6-networking.nix
@@ -98,7 +98,7 @@
                         # Server
                         PublicKey = "SLW2DFKk+Cf5K5KZl0OLYrEGyqTCqYHBKV2mTA3W2hQ=";
                         AllowedIPs = [ "10.100.100.0/24" ];
-                        Endpoint = "72.252.37.234:51820";
+                        Endpoint = "72.252.111.124:51820";
                         PersistentKeepalive = 25;
                     }
                 ];
