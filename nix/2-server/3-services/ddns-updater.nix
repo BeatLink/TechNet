d2a6e0480785364c0439d5c3c6eae582a08721ce
BeatLink <git@beatlink.simplelogin.com>
2026-02-17 00:30:26 -0500

    Fix ddns updater

--- nix/2-server/3-services/ddns-updater.nix
+++ nix/2-server/3-services/ddns-updater.nix
@@ -1,13 +1,30 @@
 {
-    services.ddns-updater = {
-        enable = true;
-        environment = {
-            SERVER_ENABLED="no";
-            CONFIG_FILEPATH = "/Storage/Services/DDNS/config.json";
-            PERIOD = "5m";
+    virtualisation.arion.projects.ddns-updater = {
+        serviceName = "ddns-updater";
+        settings = {
+            services = {
+                ddns-updater.service = {
+                    image = "ghcr.io/qdm12/ddns-updater";
+                    container_name = "ddns-updater";
+                    restart = "always";
+                    volumes = [
+                        "/Storage/Services/DDNS/data:/updater/data"
+                    ];
+                    environment = {
+                    };
+                    expose = [
+                        "8000"
+                    ];
+                    networks = [
+                        "nginx-proxy-manager_public"
+                    ];
+                };
+            };
+            networks = {
+                nginx-proxy-manager_public = {
+                    external = true;
+                };
+            };
         };
     };
-    environment.persistence."/Storage/Services/DDNS/data".directories = [ 
-        { directory = "/var/lib/private/ddns-updater"; mode = "0700"; }
-    ];
 }
