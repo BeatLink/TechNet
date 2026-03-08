6c1a732b3ff981fe77dce41d622f8eb0c5d9d463
BeatLink <git@beatlink.simplelogin.com>
2026-03-08 16:08:00 -0500

    Add github access token to flake

--- nix/0-common/1-system/5-software/1-flakes.nix
+++ nix/0-common/1-system/5-software/1-flakes.nix
@@ -1,11 +1,20 @@
 # Enable Flakes #################################################################################################################################
-{ inputs, ... }:
+{ inputs, config, ... }:
 {
+
+    sops.secrets.github_access_token = {
+        sopsFile = "${inputs.self}/secrets/0-common.yaml";
+    };
+
     nix = {
-        extraOptions = ''experimental-features = nix-command flakes''; # Enables Flakes
+        extraOptions = ''
+            experimental-features = nix-command flakes
+            !include ${config.sops.secrets.github_access_token.path}
+        '';
         registry.nixpkgs.flake = inputs.nixpkgs;
         nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ]; # Configures nix to use nixpkgs from flakes, fixes pesky errors in nix-shell
     };
+    
     programs.command-not-found.enable = false;
     home-manager.users.beatlink = {
         programs.command-not-found.enable = false;
