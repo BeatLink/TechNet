f6abbf6b497345915df0005eeff1bb03f29a1e65
BeatLink <git@beatlink.simplelogin.com>
2026-03-08 16:13:14 -0500

    Fix github access token

--- nix/0-common/1-system/5-software/1-flakes.nix
+++ nix/0-common/1-system/5-software/1-flakes.nix
@@ -2,14 +2,14 @@
 { inputs, config, ... }:
 {
 
-    sops.secrets.github_access_token = {
+    sops.secrets.github_access_token_conf = {
         sopsFile = "${inputs.self}/secrets/0-common.yaml";
     };
 
     nix = {
         extraOptions = ''
             experimental-features = nix-command flakes
-            !include ${config.sops.secrets.github_access_token.path}
+            !include ${config.sops.secrets.github_access_token_conf.path}
         '';
         registry.nixpkgs.flake = inputs.nixpkgs;
         nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ]; # Configures nix to use nixpkgs from flakes, fixes pesky errors in nix-shell
