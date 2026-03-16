dfcf4d462898139f01bca06f54657d700cba558b
BeatLink <git@beatlink.simplelogin.com>
2026-03-15 21:12:39 -0500

    Replace uptime-kuma with nixos services

--- nix/2-server/3-services/uptime-kuma.nix
+++ nix/2-server/3-services/uptime-kuma.nix
@@ -1,33 +1,9 @@
 {
-    virtualisation.arion.projects.uptime-kuma = {
-        serviceName = "uptime-kuma";
-        settings = {
-            services = {
-                uptime-kuma.service = {
-                    image = "louislam/uptime-kuma:1";
-                    container_name = "uptime-kuma";
-                    restart = "always";
-                    volumes = [ 
-                        "/Storage/Services/Uptime-Kuma:/app/data"
-                        "/var/run/docker.sock:/var/run/docker.sock"
-                    ];
-                    expose = [
-                        "3001" 
-                    ];
-                    dns = [
-                        "10.100.100.1"
-                        "192.168.0.2"
-                    ];
-                    networks = [
-                        "nginx-proxy-manager_public"
-                    ];
-                };
-            };
-            networks = {
-                nginx-proxy-manager_public = {
-                    external = true;
-                };
-            };
-        };
+    services.uptime-kuma = {
+        enable = true;
+        port = 3001;
+        dataDir = "/Storage/Services/Uptime-Kuma";
     };
+    networking.firewall.allowedTCPPorts = [ 3001 ];
+    users.users.uptime-kuma.extraGroups = [ "docker" ];
 }
