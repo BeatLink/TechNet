a21e2523158ef6954e1c8510c455a0359429424d
BeatLink <git@beatlink.simplelogin.com>
2026-03-15 21:16:11 -0500

    Fix uptime kuma

--- nix/2-server/3-services/uptime-kuma.nix
+++ nix/2-server/3-services/uptime-kuma.nix
@@ -1,8 +1,10 @@
 {
     services.uptime-kuma = {
         enable = true;
-        port = 3001;
-        settings.DATA_DIR = "/Storage/Services/Uptime-Kuma";
+        settings = {
+            DATA_DIR = "/Storage/Services/Uptime-Kuma";
+            PORT = 3001;
+        };
     };
     networking.firewall.allowedTCPPorts = [ 3001 ];
     users.users.uptime-kuma.extraGroups = [ "docker" ];
