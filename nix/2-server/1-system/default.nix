f37334c9d69f8c3e54cc0408a0bffd55fa241255
BeatLink <git@beatlink.simplelogin.com>
2026-03-25 17:30:00 -0500

    Update Server

--- nix/2-server/1-system/default.nix
+++ nix/2-server/1-system/default.nix
@@ -1,10 +1,11 @@
 {
     imports = [
         ./1-hardware
-        ./3-root-drive-disko.nix
-        ./4-data-drive.nix
-        ./5-software.nix
-        ./6-networking.nix
+        ./2-boot
+        ./3-filesystem
+        ./4-core
+        ./5-software
+        ./6-networking
         ./7-docker.nix
         ./8-borgmatic.nix
         ./9-utilities.nix
