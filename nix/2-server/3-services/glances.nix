d9c7266cf043c464390bf03f3c21c577a8b2e3c5
BeatLink <git@beatlink.simplelogin.com>
2026-03-25 17:38:01 -0500

    Update Glances

--- nix/2-server/3-services/glances.nix
+++ nix/2-server/3-services/glances.nix
@@ -4,40 +4,11 @@
 #
 
 {
-    networking.firewall.allowedTCPPorts = [ 61208 ]; 
-    virtualisation.arion.projects.glances = {
-        serviceName = "glances";
-        settings = {
-            services = {
-                glances.service = {
-                    image = "nicolargo/glances:latest-full";
-                    container_name = "glances";
-                    hostname = "Heimdall";
-                    restart = "always";
-                    environment = {
-                        "PUID" = "1000";
-                        "PGID" = "1000";
-                        "TZ" = "America/Jamaica";
-                        "GLANCES_OPT" = "-w -C /glances/conf/glances.conf";
-                    };
-                    privileged = true;
-                    network_mode =  "host";
-                    volumes = [ 
-                        "/Storage/Services/Glances/glances.conf:/glances/conf/glances.conf"
-                        "/var/run/docker.sock:/var/run/docker.sock:ro"
-                        "/etc/os-release:/etc/os-release:ro"
-                        # Volumes to monitor
-                        "/:/rootfs:ro"
-                        "/Storage:/storagefs:ro"
-                    ];
-
-                };
-            };
-            networks = {
-                nginx-proxy-manager_public = {
-                    external = true;
-                };
-            };
-        };
+    services.glances = {
+        enable = true;
+        extraArgs = [ 
+            "--webserver"
+            "--config /Storage/Services/Glances/glances.conf" 
+        ];
     };
 }
