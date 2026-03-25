0736a2bd411a3db7fcc00043bcec8bbce8678596
BeatLink <git@beatlink.simplelogin.com>
2026-03-25 18:25:08 -0500

    Fix Pihole

--- nix/2-server/3-services/pihole.nix
+++ nix/2-server/3-services/pihole.nix
@@ -19,9 +19,80 @@
 
 { config, inputs, ... }:
 {
+
+    services = {
+        pihole-web = {
+            enable = true;
+            hostName = "heimdall.technet";
+            ports = [ "82s" ];
+        };
+        pihole-ftl = {
+            enable = true;
+            lists = [
+                {
+                    url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+                }
+            ];
+            settings = {
+                dns = {
+                    upstreams = [
+                        "8.8.8.8"
+                        "8.8.4.4"
+                        "1.1.1.1"
+                        "1.0.0.1"
+                    ];
+                };
+                hosts = [
+                    "10.100.100.1  heimdall.technet"
+                    "10.100.100.2  odin.technet"
+                    "10.100.100.3  hela.technet"
+                    "10.100.100.4  thor.technet"
+                    "10.100.100.5  thorx.technet"
+                    "10.100.100.6  ragnarok.technet"
+                    "10.100.100.10 light-bedroom.technet"
+                    "10.100.100.11 light-kitchen.technet"
+                    "10.100.100.12 light-bathroom.technet"
+                    "10.100.100.13 light-bedroom-desk.technet"
+                    "10.100.100.14 light-outside.technet"
+                    "10.100.100.16 ir-fan.technet"
+                    "10.100.100.17 socket-fan.technet"
+                    "10.100.100.18 socket-ragnarok.technet"
+                    "10.100.100.19 sensor-bathroom.technet"
+                    "10.100.100.20 sensor-bedroom.technet"
+                ];
+                cnameRecords = [
+                    "blockurl.heimdall.technet,heimdall.technet"
+                    "calibre-web.heimdall.technet,heimdall.technet"
+                    "glances.heimdall.technet,heimdall.technet"
+                    "homeassistant.heimdall.technet,heimdall.technet"
+                    "openbooks.heimdall.technet,heimdall.technet"
+                    "pihole.heimdall.technet,heimdall.technet"
+                    "syncthing.heimdall.technet,heimdall.technet"
+                    "traccar.heimdall.technet,heimdall.technet"
+                    "www.heimdall.technet,heimdall.technet"
+                    "motioneye.heimdall.technet,heimdall.technet"
+                    "heimdall,heimdall.technet"
+                    "odin,odin.technet"
+                    "thor,thor.technet"
+                    "hela,hela.technet"
+                    "esphome.heimdall.technet,heimdall.technet"
+                    "trilium.heimdall.technet,heimdall.technet"
+                    "uptime-kuma.heimdall.technet,heimdall.technet"
+                    "ragnarok,ragnarok.technet"
+                    "trilium-sysadmin.heimdall.technet,heimdall.technet"
+                    "radicale.heimdall.technet,heimdall.technet"
+                    "freshrss.heimdall.technet,heimdall.technet"
+                    "qbittorrent.heimdall.technet,heimdall.technet"
+                    "drydock.heimdall.technet,heimdall.technet"
+                ];
+            };
+            stateDirectory = "/Storage/Services/PiHole/state";
+        };
+    };
+
     sops.secrets.pihole_env.sopsFile = "${inputs.self}/secrets/2-server.yaml";
     virtualisation.arion.projects.pihole = {
-        serviceName = "pihole";
+        serviceName = "pihole2";
         settings = {
             services = {
                 pihole.service = {
@@ -47,13 +118,13 @@
                         "80"
                     ];
                     ports = [
-                        "127.0.0.1:53:53/tcp"
-                        "127.0.0.1:53:53/udp"
-                        "192.168.0.2:53:53/tcp"
-                        "192.168.0.2:53:53/udp"
-                        "10.100.100.1:53:53/tcp"
-                        "10.100.100.1:53:53/udp"
-                        "82:443/tcp"
+                        "127.0.0.1:9986:53/tcp"
+                        "127.0.0.1:9986:53/udp"
+                        "192.168.0.2:9986:53/tcp"
+                        "192.168.0.2:9986:53/udp"
+                        "10.100.100.1:9986:53/tcp"
+                        "10.100.100.1:9986:53/udp"
+                        "89:443/tcp"
                     ];
                     networks = [
                         "nginx-proxy-manager_public"
