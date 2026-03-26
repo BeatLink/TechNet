098d0c2020c0b82408daf3a2da565007cbb3b6d1
BeatLink <git@beatlink.simplelogin.com>
2026-03-25 20:01:20 -0500

    Update Pihole

--- nix/2-server/3-services/pihole.nix
+++ nix/2-server/3-services/pihole.nix
@@ -31,6 +31,15 @@
             lists = [
                 {
                     url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+                    type = "block";
+                    enabled = true;
+                    description = "default blocklist";
+                }
+                {
+                    url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
+                    type = "block";
+                    enabled = true;
+                    description = "hagezi blocklist";
                 }
             ];
             settings = {
@@ -40,55 +49,71 @@
                         "8.8.4.4"
                         "1.1.1.1"
                         "1.0.0.1"
+
                     ];
+                    hosts = [
+                        "10.100.100.1  heimdall.technet"
+                        "10.100.100.2  odin.technet"
+                        "10.100.100.3  hela.technet"
+                        "10.100.100.4  thor.technet"
+                        "10.100.100.5  thorx.technet"
+                        "10.100.100.6  ragnarok.technet"
+                        "10.100.100.10 light-bedroom.technet"
+                        "10.100.100.11 light-kitchen.technet"
+                        "10.100.100.12 light-bathroom.technet"
+                        "10.100.100.13 light-bedroom-desk.technet"
+                        "10.100.100.14 light-outside.technet"
+                        "10.100.100.16 ir-fan.technet"
+                        "10.100.100.17 socket-fan.technet"
+                        "10.100.100.18 socket-ragnarok.technet"
+                        "10.100.100.19 sensor-bathroom.technet"
+                        "10.100.100.20 sensor-bedroom.technet"
+                    ];
+                    cnameRecords = [
+                        "blockurl.heimdall.technet,heimdall.technet"
+                        "calibre-web.heimdall.technet,heimdall.technet"
+                        "glances.heimdall.technet,heimdall.technet"
+                        "homeassistant.heimdall.technet,heimdall.technet"
+                        "openbooks.heimdall.technet,heimdall.technet"
+                        "pihole.heimdall.technet,heimdall.technet"
+                        "syncthing.heimdall.technet,heimdall.technet"
+                        "traccar.heimdall.technet,heimdall.technet"
+                        "www.heimdall.technet,heimdall.technet"
+                        "motioneye.heimdall.technet,heimdall.technet"
+                        "heimdall,heimdall.technet"
+                        "odin,odin.technet"
+                        "thor,thor.technet"
+                        "hela,hela.technet"
+                        "esphome.heimdall.technet,heimdall.technet"
+                        "trilium.heimdall.technet,heimdall.technet"
+                        "uptime-kuma.heimdall.technet,heimdall.technet"
+                        "ragnarok,ragnarok.technet"
+                        "trilium-sysadmin.heimdall.technet,heimdall.technet"
+                        "radicale.heimdall.technet,heimdall.technet"
+                        "freshrss.heimdall.technet,heimdall.technet"
+                        "qbittorrent.heimdall.technet,heimdall.technet"
+                        "drydock.heimdall.technet,heimdall.technet"
+                    ];
+                    domain = {
+                        name = "technet";
+                        local = "true";
+                    };
                 };
-                hosts = [
-                    "10.100.100.1  heimdall.technet"
-                    "10.100.100.2  odin.technet"
-                    "10.100.100.3  hela.technet"
-                    "10.100.100.4  thor.technet"
-                    "10.100.100.5  thorx.technet"
-                    "10.100.100.6  ragnarok.technet"
-                    "10.100.100.10 light-bedroom.technet"
-                    "10.100.100.11 light-kitchen.technet"
-                    "10.100.100.12 light-bathroom.technet"
-                    "10.100.100.13 light-bedroom-desk.technet"
-                    "10.100.100.14 light-outside.technet"
-                    "10.100.100.16 ir-fan.technet"
-                    "10.100.100.17 socket-fan.technet"
-                    "10.100.100.18 socket-ragnarok.technet"
-                    "10.100.100.19 sensor-bathroom.technet"
-                    "10.100.100.20 sensor-bedroom.technet"
-                ];
-                cnameRecords = [
-                    "blockurl.heimdall.technet,heimdall.technet"
-                    "calibre-web.heimdall.technet,heimdall.technet"
-                    "glances.heimdall.technet,heimdall.technet"
-                    "homeassistant.heimdall.technet,heimdall.technet"
-                    "openbooks.heimdall.technet,heimdall.technet"
-                    "pihole.heimdall.technet,heimdall.technet"
-                    "syncthing.heimdall.technet,heimdall.technet"
-                    "traccar.heimdall.technet,heimdall.technet"
-                    "www.heimdall.technet,heimdall.technet"
-                    "motioneye.heimdall.technet,heimdall.technet"
-                    "heimdall,heimdall.technet"
-                    "odin,odin.technet"
-                    "thor,thor.technet"
-                    "hela,hela.technet"
-                    "esphome.heimdall.technet,heimdall.technet"
-                    "trilium.heimdall.technet,heimdall.technet"
-                    "uptime-kuma.heimdall.technet,heimdall.technet"
-                    "ragnarok,ragnarok.technet"
-                    "trilium-sysadmin.heimdall.technet,heimdall.technet"
-                    "radicale.heimdall.technet,heimdall.technet"
-                    "freshrss.heimdall.technet,heimdall.technet"
-                    "qbittorrent.heimdall.technet,heimdall.technet"
-                    "drydock.heimdall.technet,heimdall.technet"
-                ];
+
+                webserver = {
+                    domain = "heimdall.technet";
+                };
+
             };
             stateDirectory = "/Storage/Services/PiHole/state";
+            logDirectory = "/Storage/Services/PiHole/logs";
         };
     };
+
+    systemd.tmpfiles.rules = [
+        # Type Path Mode User Group Age Argument
+        "f /etc/pihole/versions 0644 pihole pihole - -"
+    ];
 
     sops.secrets.pihole_env.sopsFile = "${inputs.self}/secrets/2-server.yaml";
     virtualisation.arion.projects.pihole = {
