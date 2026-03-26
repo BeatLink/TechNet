a6da9965f93e9da9ac93cf6293666498f103d78c
BeatLink <git@beatlink.simplelogin.com>
2026-03-25 21:00:39 -0500

    Fix glances

--- nix/2-server/3-services/glances.nix
+++ nix/2-server/3-services/glances.nix
@@ -1,15 +1,23 @@
 # Glances
 #
-# Glances is a dashboard and system monitor. 
+# Glances is a dashboard and system monitor.
 #
 
 {
     services.glances = {
         enable = true;
-        extraArgs = [ 
+        extraArgs = [
             "--webserver"
+            "--enable-plugin"
+            "smart"
             "--config"
-            "/Storage/Services/Glances/glances.conf" 
+            "/Storage/Services/Glances/glances.conf"
         ];
+        port = 61208;
+    };
+
+    nginx-vhosts.glances = {
+        domain = "glances.heimdall.technet";
+        port = 61208;
     };
 }
