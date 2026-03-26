3f90821a12c9401acef0a705d7f2f0e002f0f01f
BeatLink <git@beatlink.simplelogin.com>
2026-03-26 00:46:04 -0500

    Consolidate Nginx Firewall Rules

--- nix/2-server/3-services/nginx.nix
+++ nix/2-server/3-services/nginx.nix
@@ -4,4 +4,11 @@
         defaultHTTPListenPort = 80;
         defaultSSLListenPort = 443;
     };
+
+    networking.firewall = {
+        allowedTCPPorts = [
+            80 # Nginx Services
+            443 # Nginx Services (HTTPS)
+        ];
+    };
 }
