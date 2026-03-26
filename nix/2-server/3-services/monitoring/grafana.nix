55f02fefa76b876eeb0c0279507d762177b4ace9
BeatLink <git@beatlink.simplelogin.com>
2026-03-26 00:45:02 -0500

    Move Grafana Dashboard to own file

---
+++ nix/2-server/3-services/monitoring/grafana.nix
@@ -0,0 +1,27 @@
+# GRAFANA
+#
+# Visualization Dashboard for Prometheus
+
+{
+    services.grafana = {
+        enable = true;
+        settings = {
+            security.secret_key = "SW2YcwTIb9zpOOhoPsMm";
+            server = {
+                http_addr = "127.0.0.1";
+                http_port = 3000;
+                domain = "grafana.heimdall.technet";
+                root_url = "https://grafana.heimdall.technet/";
+                serve_from_sub_path = false;
+                enforce_domain = false;
+                enable_gzip = true;
+            };
+            analytics.reporting_enabled = false;
+        };
+    };
+    nginx-vhosts.grafana = {
+        domain = "grafana.heimdall.technet";
+        port = 3000;
+    };
+
+}
