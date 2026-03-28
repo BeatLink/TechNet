8b930837c1df0baf40ab2599467f0a30a3ec6502
BeatLink <git@beatlink.simplelogin.com>
2026-03-28 05:33:44 -0500

    Transfer qbittorrent to native nix

--- nix/2-server/3-services/qbittorrent.nix
+++ nix/2-server/3-services/qbittorrent.nix
@@ -2,47 +2,25 @@
 #
 # Qbittorrent is the torrent management server. The torrent server allows for automatic 24/7 downloading and setting of content.
 #
-# https://hub.docker.com/r/linuxserver/qbittorrent
 #
 
 {
-    virtualisation.arion.projects.qbittorrent = {
-        serviceName = "qbittorrent";
-        settings = {
-            services = {
-                qbittorrent.service = {
-                    image = "lscr.io/linuxserver/qbittorrent:latest";
-                    container_name = "qbittorrent";
-                    hostname = "qbittorrent";
-                    restart = "always";
-                    environment = {
-                        "PUID" = "1000";
-                        "PGID" = "1000";
-                        "WEBUI_PORT" = "8080";
-                        "TORRENTING_PORT" = "6881";
-                        "TZ" = "America/Jamaica";
-                    };
-                    volumes = [
-                        "/Storage/Services/Qbittorrent/config:/config"
-                        "/Storage/Files/Downloads:/downloads"
-                    ];
-                    expose = [
-                        "8080"
-                    ];
-                    ports = [
-                        "6881:6881"
-                        "6881:6881/udp"
-                    ];
-                    networks = [
-                        "nginx-proxy-manager_public"
-                    ];
-                };
-            };
-            networks = {
-                nginx-proxy-manager_public = {
-                    external = true;
-                };
-            };
-        };
+    services.qbittorrent = {
+        enable = true;
+        profileDir = "/Storage/Services/Qbittorrent/profile";
+        webuiPort = 9050;
+        torrentingPort = 6881;
+    };
+    networking.firewall = {
+        allowedTCPPorts = [
+            6881
+        ];
+        allowedUDPPorts = [
+            6881
+        ];
+    };
+    nginx-vhosts.qbittorrent = {
+        domain = "qbittorrent.heimdall.technet";
+        port = 9050;
     };
 }
