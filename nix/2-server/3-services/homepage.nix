701071655be3f21f38141f4db5a8a17a8dd26faf
BeatLink <git@beatlink.simplelogin.com>
2026-03-28 08:09:52 -0500

    Update HomePage

--- nix/2-server/3-services/homepage.nix
+++ nix/2-server/3-services/homepage.nix
@@ -1,5 +1,42 @@
-
 {
+
+    services.homepage-dashboard = {
+        enable = true;
+        allowedHosts = "homepage.heimdall.technet,127.0.0.1:9011";
+        listenPort = 9011;
+        bookmarks = [
+            {
+                Social = [
+                    {
+                        Reddit = [
+                            {
+                                abbr = "RE";
+                                href = "https://reddit.com/";
+                            }
+                        ];
+                    }
+                ];
+            }
+            {
+                Entertainment = [
+                    {
+                        YouTube = [
+                            {
+                                abbr = "YT";
+                                href = "https://youtube.com/";
+                            }
+                        ];
+
+                    }
+                ];
+            }
+        ];
+    };
+
+    nginx-vhosts.homepage = {
+        domain = "homepage.heimdall.technet";
+        port = 9011;
+    };
     virtualisation.arion.projects.homepage = {
         serviceName = "homepage";
         settings = {
@@ -8,12 +45,12 @@
                     image = "ghcr.io/gethomepage/homepage:latest";
                     container_name = "homepage";
                     restart = "always";
-                    volumes = [ 
+                    volumes = [
                         "/Storage/Services/Homepage/config:/app/config"
                         "/var/run/docker.sock:/var/run/docker.sock"
                     ];
                     expose = [
-                        "3000" 
+                        "3000"
                     ];
                     networks = [
                         "nginx-proxy-manager_public"
