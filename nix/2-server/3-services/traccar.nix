a6da79148b251c5e165f7cfa03c5a3e079b959ca
BeatLink <git@beatlink.simplelogin.com>
2026-04-01 19:12:17 -0500

    Switch traccar to native

--- nix/2-server/3-services/traccar.nix
+++ nix/2-server/3-services/traccar.nix
@@ -1,39 +1,14 @@
 {
-    virtualisation.arion.projects.traccar = {
-        serviceName = "traccar";
+    services.traccar = {
+        enable = true;
         settings = {
-            services = {
-                traccar.service = {
-                    image = "traccar/traccar:latest";
-                    container_name = "traccar";
-                    restart = "always";
-                    volumes = [ 
-                        "/Storage/Services/Traccar/logs:/opt/traccar/logs:rw"
-                        "/Storage/Services/Traccar/traccar.xml:/opt/traccar/conf/traccar.xml:ro"
-                        "/Storage/Services/Traccar/data:/opt/traccar/data:rw"
-                    ];
-                    environment = {
-                        "PUID" = "1000";
-                        "PGID" = "1000";
-                        "APP_BASE_URL" = "traccar.heimdall.technet";
-                    };
-                    expose = [
-                        "8082" 
-                    ];
-                    ports = [
-                      "5013:5013"
-                      "5013:5013/udp"
-                    ];
-                    networks = [
-                        "nginx-proxy-manager_public"
-                    ];
-                };
-            };
-            networks = {
-                nginx-proxy-manager_public = {
-                    external = true;
-                };
-            };
+            http.port = "8082";
+            web.url = "traccar.heimdall.technet";
         };
+    };
+    environment.persistence."/Storage/Services/Traccar".directories = [ "/var/lib/private/traccar" ];
+    nginx-vhosts.traccar = {
+        domain = "traccar.heimdall.technet";
+        port = 8082;
     };
 }
