5501b507deb8e383da2aa81e3bcb67960f4fe4a6
BeatLink <git@beatlink.simplelogin.com>
2026-04-03 12:07:33 -0500

    Finish switching radicale to native

--- nix/2-server/3-services/radicale.nix
+++ nix/2-server/3-services/radicale.nix
@@ -27,50 +27,6 @@
             };
         };
     };
-    /*
-      virtualisation.arion = {
-          backend = "docker";
-          projects.radicale = {
-              serviceName = "radicale";
-              settings = {
-                  services = {
-                      radicale.service = {
-                          image = "ghcr.io/kozea/radicale:latest";
-                          container_name = "radicale";
-                          restart = "always";
-                          volumes = [
-                              "/Storage/Services/Radicale/data:/var/lib/radicale"
-                          ];
-                          environment = {
-                              "TZ" = "America/Jamaica";
-                              "RADICALE_CONFIG" = "/var/lib/radicale/radicale.cfg";
-                          };
-                          ports = [
-                              "5232:5232"
-                          ];
-                          networks = [
-                              "nginx-proxy-manager_public"
-                          ];
-                          healthcheck = {
-                              test = [
-                                  "CMD-SHELL"
-                                  "wget --spider --quiet --tries=1 http://127.0.0.1:5232/.well-known/carddav || exit 1"
-                              ];
-                              interval = "30s";
-                              retries = 3;
-                              timeout = "5s";
-                          };
-                      };
-                  };
-                  networks = {
-                      nginx-proxy-manager_public = {
-                          "external" = true;
-                      };
-                  };
-              };
-          };
-      };
-    */
     nginx-vhosts.radicale = {
         domain = "radicale.heimdall.technet";
         port = 5232;
