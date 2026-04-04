252e99b53729c4bdf85d4145186aa7b1bce5e937
BeatLink <git@beatlink.simplelogin.com>
2026-04-04 13:48:28 -0500

    Fix laptop

--- nix/3-laptop/1-system/1-hardware-configuration.nix
+++ nix/3-laptop/1-system/1-hardware-configuration.nix
@@ -7,17 +7,21 @@
                 "usbhid" # For USB Devices
                 "mt7921e" # Wi-Fi Drivers
                 "ideapad_laptop" # Lenovo Drivers (Function Keys, Battery Management, etc)
-                "ryzen_smu"
             ];
         };
-        kernelModules = [ "kvm-amd" "ryzen_smu" ]; # Virtualization for VMs
+        kernelModules = [
+            "kvm-amd"
+        ]; # Virtualization for VMs
         kernelParams = [
             "amd_pstate=active" # Enables Power Management for AMD CPUs
             "pcie_aspm=off"
         ];
     };
     hardware = {
-        cpu.amd.updateMicrocode = true; # Updates the CPU Microcode
+        cpu.amd = {
+            updateMicrocode = true; # Updates the CPU Microcode
+            ryzen-smu.enable = true;
+        };
         enableRedistributableFirmware = true; # Enable firmware with a license allowing redistribution.
     };
     nixpkgs.hostPlatform = "x86_64-linux"; # This laptop has a 64 bit architecture
