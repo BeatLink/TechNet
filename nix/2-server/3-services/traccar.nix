dc010ff86262e5b71fda0a3d502c004200f1a41b
BeatLink <git@beatlink.simplelogin.com>
2026-04-05 07:41:08 -0500

    Change traccar port

--- nix/2-server/3-services/traccar.nix
+++ nix/2-server/3-services/traccar.nix
@@ -2,13 +2,15 @@
     services.traccar = {
         enable = true;
         settings = {
-            http.port = "8082";
-            web.url = "traccar.heimdall.technet";
+            web = {
+                port = "9280";
+                url = "traccar.heimdall.technet";
+            };
         };
     };
     environment.persistence."/Storage/Services/Traccar".directories = [ "/var/lib/private/traccar" ];
     nginx-vhosts.traccar = {
         domain = "traccar.heimdall.technet";
-        port = 8082;
+        port = 9280;
     };
 }
