b072af7ff947144576813b8f137cdc5b24d389c5
BeatLink <git@beatlink.simplelogin.com>
2026-04-05 22:59:27 -0500

    Add display for frigate

--- nix/2-server/1-system/display.nix
+++ nix/2-server/1-system/display.nix
@@ -1,1 +1,31 @@
-{ boot.kernelParams = [ "consoleblank=60" ]; }
+{ pkgs, ... }:
+{
+    # Blanks the screen when idle
+    boot.kernelParams = [ "consoleblank=60" ];
+
+    # Needed for Webcam
+    environment.systemPackages = with pkgs; [
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
+        ffmpegthumbnailer # Core video thumbnailer
+        ffmpeg # Needed to decode most video formats
+        ffmpeg-full
+    ];
+    hardware = {
+        intel-gpu-tools.enable = true;
+        graphics = {
+            enable = true;
+            extraPackages = with pkgs; [
+                # VA-API decode
+                intel-media-driver
+                intel-vaapi-driver
+                # Compute
+                intel-compute-runtime-legacy1
+                # VDPAU
+                libvdpau-va-gl
+            ];
+        };
+    };
+}
