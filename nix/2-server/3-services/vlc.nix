781d09a453d66c92d1c1b67548f63f9e32dc1fa7
BeatLink <git@beatlink.simplelogin.com>
2026-04-05 23:00:24 -0500

    Update VLC

--- nix/2-server/3-services/vlc.nix
+++ nix/2-server/3-services/vlc.nix
@@ -8,7 +8,6 @@
     networking.firewall = {
         allowedTCPPorts = [
             4212
-            # 4213
         ];
     };
     sops.secrets.vlc_env.sopsFile = "${inputs.self}/secrets/2-server.yaml";
@@ -43,43 +42,5 @@
             };
             wantedBy = [ "multi-user.target" ];
         };
-        /*vlc-video = {
-            description = "Converts USB Webcam Stream to RSTP for Home Security";
-            after = [
-                "network.target"
-                "pipewire.service"
-            ];
-            wants = [
-                "network.target"
-                "pipewire.service"
-            ];
-
-            serviceConfig = {
-                Type = "simple";
-                User = "beatlink"; # Change to the user that has audio access
-                Group = "beatlink";
-                SupplementaryGroups = [
-                    "audio"
-                    "video"
-                ];
-
-                # Fetch the secret at runtime using environment file from sops-nix
-                EnvironmentFile = config.sops.secrets.vlc_env.path;
-
-                ExecStart = ''
-                    ${pkgs.vlc}/bin/cvlc \
-                    v4l2:///dev/video0:width=1280:height=720:fps=30 \
-                    :input-slave=pulse:// \
-                    --avcodec-hw=vaapi \
-                    --sout "#transcode{vcodec=h264,acodec=mp4a,ab=128,samplerate=48000}:rtp{sdp=rtsp://10.100.100.1:4213/webcam}" \
-                    --no-sout-all \
-                    --sout-keep \
-                    --sout-rtsp-user "$VLC_RTSP_USER" \
-                    --sout-rtsp-pwd "$VLC_RTSP_PASS"
-                '';
-                Restart = "always";
-            };
-            wantedBy = [ "multi-user.target" ];
-        };*/
     };
 }
