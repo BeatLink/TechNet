66f04905486ed9ec5ed9de7f5434e4515dd1c838
BeatLink <git@beatlink.simplelogin.com>
2026-04-07 16:51:17 -0500

    Update MQTT

--- nix/2-server/3-services/mosquitto.nix
+++ nix/2-server/3-services/mosquitto.nix
@@ -3,9 +3,26 @@
         enable = true;
         listeners = [
             {
-                acl = [ "pattern readwrite #" ];
-                omitPasswordAuth = true;
-                settings.allow_anonymous = true;
+                address = "127.0.0.1";
+                port = 1883;
+                #acl = [ "pattern readwrite #" ];
+                users = {
+                    homeassistant = {
+                        acl = [
+                            "readwrite #"
+                        ];
+                        hashedPassword = "$7$101$JK9EsqBDvLxRjMPu$luaDg9rqveBv6MHKandjD41EbyR35WqLPRVlNDJjXDFizGzuQJ8EjUzO+PLYP7TnU2AdBcw69W2bb/L15vPbzw==";
+                    };
+                    frigate = {
+                        acl = [
+                            "readwrite #"
+                        ];
+                        hashedPassword = "$7$101$+xsfm3AJVBJe7pb8$QSxNsvFw8utqzzvaiZ0GXlp8HZQB1z0lj+EvRwbGwUbWJdkKNfUCNWrq90Vm+i/39csFiLuItsBAAYp+jEYeVA==";
+                    };
+                };
+                #omitPasswordAuth = true;
+                #settings.allow_anonymous = true;
+
             }
         ];
     };
