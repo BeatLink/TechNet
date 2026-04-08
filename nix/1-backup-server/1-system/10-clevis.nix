e9bdf8c9a25313a5525b0519375a48ab514fcba1
BeatLink <git@beatlink.simplelogin.com>
2026-04-08 08:13:41 -0500

    Migrate Backup Server Credentials

--- nix/1-backup-server/1-system/10-clevis.nix
+++ nix/1-backup-server/1-system/10-clevis.nix
@@ -1,7 +1,7 @@
 { config, inputs, ... }:
 {
     sops.secrets.clevis_key = {
-        sopsFile = "${inputs.self}/secrets/1-backup-server.yaml";
+        sopsFile = "${inputs.self}/secrets/1-backup-server/clevis.yaml";
     };
 
     boot.initrd = {
