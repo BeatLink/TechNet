1e52537d1788f9f32115bf5a1c181cb4b7b93fc7
BeatLink <git@beatlink.simplelogin.com>
2026-04-07 23:03:54 -0500

    Remove Nginx Proxy MAnager

--- nix/2-server/3-services/networking/default.nix
+++ nix/2-server/3-services/networking/default.nix
@@ -2,7 +2,6 @@
     imports = [
         ./14-device-online.nix
         ./ddns-updater.nix
-        ./nginx-proxy-manager.nix
         ./nginx-vhosts.nix
         ./nginx.nix
         ./pi-hole.nix
