ca8d7034c1c78f74376aa70c14f40256daf22bed
BeatLink <git@beatlink.simplelogin.com>
2026-04-08 12:41:08 -0500

    Migrate phone credentials

--- nix/5-phone/2-users/beatlink.nix
+++ nix/5-phone/2-users/beatlink.nix
@@ -1,7 +1,7 @@
 { inputs, config, ... }:
 {
     sops.secrets.beatlink_hashed_password_phone = {
-        sopsFile = "${inputs.self}/secrets/4-phone.yaml";
+        sopsFile = "${inputs.self}/secrets/4-phone/users.yaml";
         neededForUsers = true;
     };
     users.users."beatlink".hashedPasswordFile = config.sops.secrets.beatlink_hashed_password_phone.path;
