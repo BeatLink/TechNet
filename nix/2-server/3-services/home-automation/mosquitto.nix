995c2a4cb00e2788145348683c09d6b29ab6e341
BeatLink <git@beatlink.simplelogin.com>
2026-04-07 21:08:30 -0500

    Fix mosquitto

--- nix/2-server/3-services/home-automation/mosquitto.nix
+++ nix/2-server/3-services/home-automation/mosquitto.nix
@@ -7,11 +7,11 @@
                 port = 1883;
                 users = {
                     homeassistant = {
-                        acl = [ "readwrite frigate" ];
+                        acl = [ "readwrite #" ];
                         hashedPassword = "$7$101$JK9EsqBDvLxRjMPu$luaDg9rqveBv6MHKandjD41EbyR35WqLPRVlNDJjXDFizGzuQJ8EjUzO+PLYP7TnU2AdBcw69W2bb/L15vPbzw==";
                     };
                     frigate = {
-                        acl = [ "readwrite frigate" ];
+                        acl = [ "readwrite #" ];
                         hashedPassword = "$7$101$+xsfm3AJVBJe7pb8$QSxNsvFw8utqzzvaiZ0GXlp8HZQB1z0lj+EvRwbGwUbWJdkKNfUCNWrq90Vm+i/39csFiLuItsBAAYp+jEYeVA==";
                     };
                 };
