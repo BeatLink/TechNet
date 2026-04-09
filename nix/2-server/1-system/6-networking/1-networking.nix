98ef18ddbf24ae572647504e4c7c4ae2426b52a1
BeatLink <git@beatlink.simplelogin.com>
2026-04-09 09:44:47 -0500

    Fix Server Networking

--- nix/2-server/1-system/6-networking/1-networking.nix
+++ nix/2-server/1-system/6-networking/1-networking.nix
@@ -52,7 +52,11 @@
 
         };
     };
-    sops.secrets.wireguard_private_key.sopsFile = "${inputs.self}/secrets/2-server/wireguard.yaml";
+    sops.secrets.wireguard_private_key = {
+        sopsFile = "${inputs.self}/secrets/2-server/wireguard.yaml";
+        owner = "systemd-network";
+        group = "systemd-network";
+    };
     boot = {
         kernel.sysctl."net.ipv4.conf.all.forwarding" = true; # Enables routing between peers for wireguard
         initrd = {
