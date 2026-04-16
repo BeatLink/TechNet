90a2b4be1d01149ce9be2dec4d15d376a4debd98
BeatLink <git@beatlink.simplelogin.com>
2026-04-16 08:30:10 -0500

    Remove trilium sysadmin. Upgrade trilium server

--- nix/2-server/3-services/personal-info-and-files/trilium.nix
+++ nix/2-server/3-services/personal-info-and-files/trilium.nix
@@ -1,9 +1,14 @@
+{ inputs, pkgs, ... }:
+let
+    trilium = inputs.trilium-notes.packages.${pkgs.stdenv.hostPlatform.system}.server;
+in
 {
     services.trilium-server = {
         enable = true;
         instanceName = "Heimdall";
         port = 8080;
         dataDir = "/Storage/Services/Trilium/data";
+        package = trilium;
     };
 
     nginx-vhosts.trilium = {
