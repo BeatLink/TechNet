ce395756754ce07469fcec67b93a6112a01400e4
BeatLink <git@beatlink.simplelogin.com>
2026-04-21 07:59:20 -0500

    Fix ddns

--- nix/2-server/3-services/networking/ddns-updater.nix
+++ nix/2-server/3-services/networking/ddns-updater.nix
@@ -4,11 +4,13 @@
         enable = true;
         environment = {
             SERVER_ENABLED = "yes";
-            CONFIG_FILEPATH = "/Storage/Services/DDNS/config.json";
-            PERIOD = "5m";
             LISTENING_ADDRESS = "127.0.0.1:9420";
+            RESOLVER_ADDRESS = "8.8.8.8:53"; 
         };
     };
+
+    environment.persistence."/Storage/Services/DDNS".directories = [ "/var/lib/private/ddns-updater" ];
+
     nginx-vhosts.ddns = {
         domain = "ddns.heimdall.technet";
         port = 9420;
