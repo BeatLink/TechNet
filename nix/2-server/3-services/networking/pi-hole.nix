93bc7da4e46cb8b820979cf8f2b4407d24051214
BeatLink <git@beatlink.simplelogin.com>
2026-04-21 07:59:28 -0500

    Update Pihole

--- nix/2-server/3-services/networking/pi-hole.nix
+++ nix/2-server/3-services/networking/pi-hole.nix
@@ -3,10 +3,9 @@
 # Pi-Hole acts as an ad-blocking system for the TechNet. More importantly, it also acts as the DNS server for all docker services and hosts
 # in the TechNet, allowing them to be accessed via the .technet domain
 #
-# More info at https://github.com/pi-hole/docker-pi-hole/ and https://docs.pi-hole.net/
-#
 # Links:
 #     - https://github.com/pi-hole/pi-hole/
+#     - https://docs.pi-hole.net/
 #
 # Device List
 #     - Heimdall - heimdall.technet
@@ -98,35 +97,6 @@ in
             enable = true;
             openFirewallDNS = true;
             openFirewallDHCP = true;
-            /*
-              lists = [
-                  {
-                      url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-                      enabled = true;
-                      description = "default blocklist";
-                  }
-                  {
-                      url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
-                      enabled = true;
-                      description = "hagezi blocklist";
-                  }
-                  {
-                      url = "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt";
-                      enabled = true;
-                      description = "KADhosts - Polish malware/spam domains";
-                  }
-                  {
-                      url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts";
-                      enabled = true;
-                      description = "FadeMind - spam hosts";
-                  }
-                  {
-                      url = "https://v.firebog.net/hosts/static/w3kbl.txt";
-                      enabled = true;
-                      description = "Firebog - suspicious domains";
-                  }
-              ];
-            */
             settings = {
                 dns = {
                     upstreams = [
@@ -195,53 +165,5 @@ in
     systemd.services.pihole = {
         after = [ "unbound.service" ];
         requires = [ "unbound.service" ];
-    };
-
-    virtualisation.arion.projects.pihole = {
-        serviceName = "pihole2";
-        settings = {
-            services = {
-                pihole.service = {
-                    image = "pihole/pihole:latest";
-                    container_name = "pihole";
-                    restart = "always";
-                    volumes = [
-                        "/Storage/Services/PiHole/etc-pihole:/etc/pihole"
-                        "/Storage/Services/PiHole/etc-dnsmasq.d:/etc/dnsmasq.d"
-                    ];
-                    env_file = [
-                        config.sops.secrets.pihole_env.path
-                    ];
-                    capabilities = {
-                        SYS_NICE = true;
-                        SYS_TIME = true;
-                    };
-                    environment = {
-                        "TZ" = "America/Jamaica";
-                        "WEB_PORT" = "80";
-                    };
-                    expose = [
-                        "80"
-                    ];
-                    ports = [
-                        "127.0.0.1:9998:53/tcp"
-                        "127.0.0.1:9986:53/udp"
-                        "192.168.0.2:9986:53/tcp"
-                        "192.168.0.2:9986:53/udp"
-                        "10.100.100.1:9986:53/tcp"
-                        "10.100.100.1:9986:53/udp"
-                        "89:443/tcp"
-                    ];
-                    networks = [
-                        "nginx-proxy-manager_public"
-                    ];
-                };
-            };
-            networks = {
-                nginx-proxy-manager_public = {
-                    external = true;
-                };
-            };
-        };
     };
 }
