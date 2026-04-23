663e5a57955243640addcdedd5678730a6b14740
BeatLink <git@beatlink.simplelogin.com>
2026-04-23 07:00:06 -0500

    Fix Ollama

--- nix/2-server/3-services/ai/ollama.nix
+++ nix/2-server/3-services/ai/ollama.nix
@@ -2,16 +2,15 @@
 {
     services.ollama = {
         enable = true;
-        package = pkgs.ollama-vulkan;
         user = "ollama";
         group = "ollama";
         home = "/Storage/Services/Ollama/data";
         loadModels = [
-            "dolphin3"
-            "gemma3"
-            "gemma3:27b"
+            #"dolphin3"
+            #"gemma3"
+            #"gemma3:27b"
             "deepseek-r1:latest"
-            "deepseek-r1:1.5b"
+            #"deepseek-r1:1.5b"
         ];
         syncModels = true;
     };
@@ -20,6 +19,9 @@
         enable = true;
         port = 9770;
     };
+    environment.persistence."/Storage/Services/Ollama/open-webui".directories = [
+        "/var/lib/private/open-webui"
+    ];
 
     nginx-vhosts.ollama = {
         domain = "ollama.heimdall.technet";
