bc42e0b465283398fd7c6b0a105ac35ce93b134e
BeatLink <git@beatlink.simplelogin.com>
2026-05-17 01:24:57 -0500

    Fix laptop ollama

--- nix/3-laptop/4-apps/technet/ollama.nix
+++ nix/3-laptop/4-apps/technet/ollama.nix
@@ -1,13 +1,13 @@
-{
+{pkgs, ...}:{
     services.ollama = {
         enable = true;
         home = "/Storage/Apps/TechNet/Ollama/data";
         user = "ollama";
         group = "ollama";
+        package = pkgs.ollama-cuda;
         loadModels = [
             "gemma4:e2b"
         ];
         syncModels = true;
-        acceleration = "cuda"; # enables GPU support
     };
 }
