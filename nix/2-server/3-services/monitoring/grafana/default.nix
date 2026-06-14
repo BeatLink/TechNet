85abf0563159a45960476b1ba633206d8d91dffb
BeatLink <git@beatlink.simplelogin.com>
2026-06-14 10:25:14 -0500

    Relocate Grafana dashboards

---
+++ nix/2-server/3-services/monitoring/grafana/default.nix
@@ -0,0 +1,43 @@
+# GRAFANA
+#
+# Visualization Dashboard for Prometheus
+
+{ pkgs, lib, ... }:
+{
+    services.grafana = {
+        enable = true;
+        dataDir = "/Storage/Services/Grafana/data";
+
+        provisioning.dashboards.settings.providers = [
+            {
+                name = "Declarative Dashboards";
+                options.path = pkgs.writeTextDir "dashboards" (builtins.toJSON (import ./dashboards/updates.nix));
+            }
+        ];
+
+        settings = {
+            security.secret_key = "SW2YcwTIb9zpOOhoPsMm";
+            server = {
+                http_addr = "127.0.0.1";
+                http_port = 3000;
+                domain = "grafana.heimdall.technet";
+                root_url = "https://grafana.heimdall.technet/";
+                serve_from_sub_path = false;
+                enforce_domain = false;
+                enable_gzip = true;
+            };
+            analytics.reporting_enabled = false;
+        };
+    };
+
+    systemd.tmpfiles.rules = [
+        "d /Storage/Services/Grafana 0750 grafana grafana - -"
+        "Z /Storage/Services/Grafana 0750 grafana grafana - -"
+    ];
+
+    nginx-vhosts.grafana = {
+        domain = "grafana.heimdall.technet";
+        port = 3000;
+    };
+
+}
