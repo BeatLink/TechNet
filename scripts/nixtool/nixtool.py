8231d226f2d7cab63b9893ee7e67681f92658d12
BeatLink <git@beatlink.simplelogin.com>
2026-06-13 23:12:56 -0500

    Update nixtool

--- scripts/nixtool/nixtool.py
+++ scripts/nixtool/nixtool.py
@@ -8,27 +8,27 @@ An App to show the current time.
 
 import pathlib
 import json
-import asyncio
-import subprocess
-
-from datetime import datetime
 
 from textual import on, work
 from textual.app import App, ComposeResult
 
-from textual.containers import CenterMiddle, Center, Middle, Container, Vertical, Horizontal, HorizontalGroup
-from textual.widgets import Header, ContentSwitcher, Label, Digits, OptionList, RichLog, Button
+from textual.containers import Container
+from textual.widgets import Header, ContentSwitcher, Label, OptionList, Button
 from textual.widgets.option_list import Option
 from textual.widget import Widget
 from textual.events import Focus
 from textual.message import Message
 from textual.reactive import reactive
-from textual.theme import Theme
 
+from nixtool.theme import white_blue_theme
+from nixtool.command_runner import CommandRunner
+
+# Constants
+script_folder = pathlib.Path(__file__).parent
+config_file = script_folder / "nixtool-config.json"
 
 
 COMMANDS_TITLE = "Select a command"
-
 COMMANDS_DICT = {
     "Run All Tasks": "run_all",
     "Run Nix Flake Update": "flake_update",
@@ -36,11 +36,19 @@ COMMANDS_DICT = {
     "Run Nixos Rebuild": "rebuild",
     "Preview Old Generations": "preview_generations",
     "Remove Old Generations & GC": "purge_generations_gc",
-    "Run Nix Garbage Collection": "gc"
+    "Run Nix Garbage Collection": "gc",
+    "Run Installation and Formatting Commands": "run_installer"
+}
+
+INSTALLER_TITLE = "Select an installation command"
+INSTALLER_DICT = {
+    "Install NixOS": "install",
+    "Format Data Drive (Backup Server)": "format_data_drive_backup",
+    "Format Data Drive (Server)": "format_data_drive_server",
+    "Format SD Card (Phone)": "format_sd_card_phone"
 }
 
 REBUILD_TITLE = "Select a NixOS Rebuild action"
-
 REBUILD_DICT = {
     "switch - Activate config and save to bootloader": "switch",
     "test - Activate config but reset next boot": "test",
@@ -52,252 +60,6 @@ REBUILD_DICT = {
 
 HOST_TITLE = "Select Hosts"
 
-white_blue_theme = Theme(
-    name="white_blue",
-    primary="#00ACFF",
-    secondary="#00ACFF",
-    accent="#B48EAD",
-    foreground="#D8DEE9",
-    background="#001B29",
-    success="#00FF00",
-    warning="#EBCB8B",
-    error="#FF0000",
-    surface="#001B29",
-    panel="#434C5E",
-    dark=True,
-    variables={    
-        "block-cursor-text-style": "none",
-        "footer-key-foreground": "#88C0D0",
-        "input-selection-background": "#81a1c1 35%",
-    },
-)
-
-script_folder = pathlib.Path(__file__).parent
-config_file = script_folder / "nixtool-config.json"
-
-class CommandRunner(Widget):
-    DEFAULT_CSS = """
-        #container {
-            width: 97.5%;
-            height: 95%;
-        }
-
-        HorizontalGroup {
-            align: center middle;
-        }
-
-        #label {
-            color: $primary;
-            text-style: bold;
-        }
-
-        #logview {
-            height: 100%;
-            border: round $primary;
-            background: transparent;
-        }
-
-        #message {
-            text-align: center;
-            text-style: bold;
-            align: center middle;
-            content-align: center middle;
-        }
-
-        #message.success {
-            color: $success;
-        }
-
-        #message.error {
-            color: $error;
-        }
-
-        #start, #return {
-            text-style: bold;
-            align: center middle;
-            content-align: center middle;
-        }
-
-        .borders {
-            border: round orange;
-        }
-
-        .invisible {
-            display: none
-        }
-    """
-
-    can_focus = True
-    command_queue = []
-    final_returncode = 0
-    
-    """class Selected(Message):
-        Sent when the command has been selected.
-        def __init__(self, widget: Widget, command: str) -> None:
-            super().__init__()
-            self.selected_command = command
-            self.widget = widget
-        @property
-        def control(self) -> Widget:
-            Returns the widget
-            return self.widget
-
-    """
-    
-    def compose(self) -> ComposeResult:
-        self.container = Container(id="container")
-        self.label = Label(id="label")
-        self.logview = RichLog(id="logview")
-        self.message = Label(id="message", classes="invisible")
-        self.message.loading = True
-        self.start_button = Button("Start", id="start", variant="primary")
-        self.start_button.compact = True
-        self.start_button.flat = True
-        self.return_button = Button("Return", id="return", variant="primary")
-        self.return_button.compact = True
-        self.return_button.flat = True
-        with self.container:
-            with Center():
-                yield Center(self.label)
-                yield CenterMiddle(self.logview)
-                yield Center(self.message)
-                with HorizontalGroup():
-                    yield self.return_button
-                    yield self.start_button
-
-
-    def on_focus(self, event: Focus):
-        self.start_button.focus()
-
-
-    def load_command_queue(self, command_queue):
-        # Clear UI
-        self.label.update(f"Ready To Start")
-        self.label.refresh()
-        self.logview.clear()
-        self.message.update("")
-        self.message.loading = True
-        self.message.remove_class("success", "error")
-        self.message.refresh()
-        self.start_button.remove_class("invisible")
-        self.return_button.remove_class("invisible")
-        self.command_queue = command_queue
-
-        self.logview.write("The following commands will now be executed\n")
-        for command in self.command_queue:
-            self.logview.write(f"- {command}")
-        self.logview.write("\nPress Start to begin")
-
-
-    @on(Button.Pressed, "#start")
-    def start(self, event: Button.Pressed):
-        self.run_command()
-        self.return_button.focus()
-
-    @work(exclusive=True)
-    async def run_command(self):
-        self.final_returncode = 0
-        self.start_button.add_class("invisible")
-        self.message.remove_class("invisible")
-        for index, command in enumerate(self.command_queue):
-            command_message = f"Running command {index+1} of {len(self.command_queue)}: {command}"
-            self.label.update(command_message)
-            self.logview.write("\n--------------------------------------------------------------------------\n")
-            self.logview.write(f">>> {command_message} <<<")
-            process = await asyncio.create_subprocess_shell(
-                command,
-                cwd=script_folder.parent,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT
-            )
-            while True:
-                line = await process.stdout.readline()
-                if not line:
-                    break
-                self.logview.write(line.decode().rstrip())
-            await process.wait()
-            self.final_returncode += int(process.returncode)
-            if int(process.returncode) == 0:
-                self.logview.write(f"\n>>> Command succeeded with return code {process.returncode} <<")
-            else:
-                self.logview.write(f"\n>>> Command failed with return code {process.returncode} <<<")
-
-        # When process is finished show the end message        
-        if int(self.final_returncode) == 0:
-            self.message.update(f"All commands succeeded!")
-            self.message.add_class("success")
-        else:
-            self.message.update(f"One or more commands have failed! Please check the logs")
-            self.message.add_class("error")
-        self.message.loading = False
-        self.message.remove_class("invisible")
-        self.message.refresh()
-        self.return_button.remove_class("invisible")
-        self.refresh()
-
-
-class OptionsWidget(Widget):
-    DEFAULT_CSS = """
-        #container {
-            width: auto;
-            height: auto;
-        }
-        #label {
-            width: 100%;
-            height: auto;
-            text-align: center;
-            color: $primary;
-            text-style: bold;
-        }
-        #list {
-            width: auto;
-            height: auto;
-            margin: 0;
-            border: round $primary;
-            background: transparent;
-        }
-    """
-    can_focus = True
-    title = reactive("")
-    options = reactive ({}, recompose=True)
-    
-    class Selected(Message):
-
-        def __init__(self, widget: Widget, key: str, value: str) -> None:
-            super().__init__()
-            self.widget = widget
-            self.key = key
-            self.value = value
-
-        @property
-        def control(self) -> Widget:
-            return self.widget
-            
-
-    def compose(self) -> ComposeResult:
-        options = [
-            Option(key, id=value)
-            for key, value
-            in self.options.items()
-        ]
-        self.container = Container(id="container")
-        self.label = Label(self.title, id="label")
-        self.list = OptionList(*options, id="list")
-        with self.container:
-            yield self.label
-            yield self.list
-
-    def on_focus(self, event: Focus):
-        self.list.focus()
-
-    def on_option_list_option_selected(self, event: OptionList.OptionSelected):
-        self.post_message(
-            self.Selected(
-                self,
-                event.option.prompt,
-                event.option.id
-            )
-        )
 
 
 class NixOSManager(App):
@@ -337,6 +99,7 @@ class NixOSManager(App):
     
     config = reactive({})
     command = ""
+    installer_command = ""
     rebuild_action = ""
     host = {
         "hostname": "",
@@ -349,26 +112,36 @@ class NixOSManager(App):
         self.header.tall = True
         self.content_switcher = ContentSwitcher(initial="command-menu")
         self.content_switcher.loading = True
+
         self.command_menu = OptionsWidget(id="command-menu")
         self.command_menu.title = COMMANDS_TITLE
         self.command_menu.options = COMMANDS_DICT
+
+        self.installer_menu = OptionsWidget(id="installer-menu")
+        self.installer_menu.title = INSTALLER_TITLE
+        self.installer_menu.options = INSTALLER_DICT
+
         self.rebuild_menu = OptionsWidget(id="rebuild-menu")
         self.rebuild_menu.title = REBUILD_TITLE
         self.rebuild_menu.options = REBUILD_DICT
+        
         self.host_menu = OptionsWidget(id="host-menu")
         self.host_menu.title = HOST_TITLE
         self.host_menu.options = {}
+
         self.command_runner = CommandRunner(id="command-runner")
+
         yield self.header
         with self.content_switcher:
             yield self.command_menu
+            yield self.installer_menu
             yield self.rebuild_menu
             yield self.host_menu
             yield self.command_runner
 
 
     def on_mount(self) -> None:
-        self.title = "TechNet Updater"
+        self.title = "NixTool"
         self.sub_title = "CLI tool for managing flake based NixOS installations"
         self.register_theme(white_blue_theme)  
         self.theme = "white_blue"
@@ -394,10 +167,19 @@ class NixOSManager(App):
         elif self.command == "rebuild" or self.command == "run_all":
             self.content_switcher.current = "rebuild-menu"
             self.rebuild_menu.focus()
+        elif self.command == "run_installer":
+            self.content_switcher.current = "installer_menu"
+            self.installer_menu.focus()
         else:
             self.content_switcher.current = "host-menu"
             self.host_menu.focus()
-                
+
+    @on(OptionsWidget.Selected, "#installer-menu")
+    def process_installer_action(self, selected: OptionsWidget.Selected):
+        self.installer_command = selected.value
+        self.content_switcher.current = "host-menu"
+        self.host_menu.focus()
+
     @on(OptionsWidget.Selected, "#rebuild-menu")
     def process_rebuild_action(self, selected: OptionsWidget.Selected):
         self.rebuild_action = selected.value
@@ -455,7 +237,6 @@ class NixOSManager(App):
                 except Exception as e:
                     self.command_queue.append(f"echo 'Error processing module target at {config_path.name}: {str(e)}'")
 
-
         def build_command(hostname):
             return (
                 "nixos-rebuild "
@@ -478,7 +259,6 @@ class NixOSManager(App):
             ssh_cmd = f"ssh {self.config['user']}@{self.config['hosts'][hostname]} '{remote_cmd}'"
             return ssh_cmd
 
-
         def remove_generations_command(hostname):
             remote_cmd = (
                 "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations old "
@@ -487,7 +267,6 @@ class NixOSManager(App):
             )
             ssh_cmd = f"ssh {self.config['user']}@{self.config['hosts'][hostname]} '{remote_cmd}'"
             return ssh_cmd
-
 
         def gc_command(hostname):
             remote_cmd = (
@@ -534,7 +313,6 @@ class NixOSManager(App):
         self.command_queue = []
         self.content_switcher.current = "command-menu"
         self.command_menu.focus()
-
 
 
 if __name__ == "__main__":
