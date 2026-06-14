b6435e0bf962b72aa0fa6fd969eb9e74e947f8a7
BeatLink <git@beatlink.simplelogin.com>
2026-06-14 00:58:39 -0500

    Update nixtool.py

--- scripts/nixtool/nixtool.py
+++ scripts/nixtool/nixtool.py
@@ -24,13 +24,7 @@ from nixtool.theme import white_blue_theme
 from nixtool.command_runner import CommandRunner
 from nixtool.host_selector import HostSelector
 from nixtool.options_widget import OptionsWidget
-from nixtool.commands import (
-    COMMANDS_TITLE, COMMANDS_DICT,
-    INSTALLER_TITLE, INSTALLER_DICT,
-    REBUILD_TITLE, REBUILD_DICT,
-    HOST_TITLE,
-    MenuCommand, GenerationsPurgeCommand, GenerationsGCCommand
-)
+from nixtool.commands import all_commands, HOST_TITLE
 
 # Constants
 script_folder = pathlib.Path(__file__).parent
@@ -72,9 +66,9 @@ class NixOSManager(App):
     """
     
     config = reactive({})
-    command = ""
-    installer_command = ""
-    rebuild_action = ""
+    current_cmd = reactive({})
+    selected_vars = reactive({})
+    current_var = ""
     host = {
         "hostname": "",
         "host_url": ""
@@ -88,16 +82,10 @@ class NixOSManager(App):
         self.content_switcher.loading = True
 
         self.command_menu = OptionsWidget(id="command-menu")
-        self.command_menu.title = COMMANDS_TITLE
-        self.command_menu.options = COMMANDS_DICT
+        self.command_menu.title = all_commands.get("title", "Select a command")
+        self.command_menu.options = all_commands.get("commands", [])
 
-        self.installer_menu = OptionsWidget(id="installer-menu")
-        self.installer_menu.title = INSTALLER_TITLE
-        self.installer_menu.options = INSTALLER_DICT
-
-        self.rebuild_menu = OptionsWidget(id="rebuild-menu")
-        self.rebuild_menu.title = REBUILD_TITLE
-        self.rebuild_menu.options = REBUILD_DICT
+        self.variable_menu = OptionsWidget(id="variable-menu")
         
         self.host_selector = HostSelector(config_file, id="host-selector")
 
@@ -106,8 +94,7 @@ class NixOSManager(App):
         yield self.header
         with self.content_switcher:
             yield self.command_menu
-            yield self.installer_menu
-            yield self.rebuild_menu
+            yield self.variable_menu
             yield self.host_selector
             yield self.command_runner
 
@@ -128,57 +115,81 @@ class NixOSManager(App):
 
     @on(OptionsWidget.Selected, "#command-menu")
     def process_command(self, selected: OptionsWidget.Selected):
-        print(f"command selected {selected.value}")
-        self.command = str(selected.value)
-        cmd_obj = COMMANDS_DICT[self.command]
+        idx = int(selected.value)
+        self.current_cmd = all_commands['commands'][idx]
+        self.selected_vars = {}
+        self.check_next_step()
 
-        if isinstance(cmd_obj, MenuCommand):
-            self.content_switcher.current = cmd_obj.target_menu
-            if cmd_obj.target_menu == "rebuild-menu":
-                self.rebuild_menu.focus()
-            elif cmd_obj.target_menu == "installer-menu":
-                self.installer_menu.focus()
-        elif self.command in ["flake_update", "export_dconf"]:
-            self.prepare_command_queue()
-        else:
-            self.content_switcher.current = "host-selector"
-            self.host_selector.focus()
-
-    @on(OptionsWidget.Selected, "#installer-menu")
-    def process_installer_action(self, selected: OptionsWidget.Selected):
-        self.installer_command = str(selected.value)
-        self.content_switcher.current = "host-selector"
-        self.host_selector.focus()
-
-    @on(OptionsWidget.Selected, "#rebuild-menu")
-    def process_rebuild_action(self, selected: OptionsWidget.Selected):
-        self.rebuild_action = str(selected.value)
-        self.content_switcher.current = "host-selector"
-        self.host_selector.focus()
+    @on(OptionsWidget.Selected, "#variable-menu")
+    def process_variable(self, selected: OptionsWidget.Selected):
+        self.selected_vars[self.current_var] = str(selected.value)
+        self.check_next_step()
 
     @on(HostSelector.Selected)
     def process_host(self, message: HostSelector.Selected):
         self.host = {"hostname": message.hostname, "host_url": message.host_url}
+        self.check_next_step()
+
+    def check_next_step(self):
+        if "menu_variables" in self.current_cmd:
+            for var_name, var_cfg in self.current_cmd["menu_variables"].items():
+                if var_name not in self.selected_vars:
+                    self.current_var = var_name
+                    self.variable_menu.title = var_cfg.get("title", f"Select {var_name}")
+                    self.variable_menu.options = var_cfg.get("options", {})
+                    self.content_switcher.current = "variable-menu"
+                    self.variable_menu.focus()
+                    return
+
+        needs_host = self.current_cmd.get("run_on_remote", False) or self.is_host_needed_recursive(self.current_cmd)
+        if needs_host and not self.host["hostname"]:
+            self.content_switcher.current = "host-selector"
+            self.host_selector.focus()
+            return
+
         self.prepare_command_queue()
+
+    def is_host_needed_recursive(self, cmd_dict):
+        cmds = cmd_dict.get("commands", [])
+        for item in cmds:
+            if isinstance(item, str) and ("<HOSTNAME>" in item or "<HOSTURL>" in item):
+                return True
+            if isinstance(item, dict) and (item.get("run_on_remote", False) or self.is_host_needed_recursive(item)):
+                return True
+        return False
 
     def prepare_command_queue(self):
         self.command_queue = []
-
-        if self.rebuild_action:
-            cmd_obj = REBUILD_DICT[self.rebuild_action]
-            if self.command == "run_all":
-                self.command_queue.append("nix flake update --refresh")
-                self.command_queue.extend(cmd_obj.get_queue(self))
-                self.command_queue.extend(GenerationsPurgeCommand().get_queue(self))
-                self.command_queue.extend(GenerationsGCCommand().get_queue(self))
-            else:
-                self.command_queue.extend(cmd_obj.get_queue(self))
-        elif self.installer_command:
-            self.command_queue.extend(INSTALLER_DICT[self.installer_command].get_queue(self))
+        is_batch = self.host.get("host_url") == "all"
+        
+        if is_batch and (self.current_cmd.get("run_on_remote") or self.is_host_needed_recursive(self.current_cmd)):
+            for hname, hurl in self.config["hosts"].items():
+                if hurl != "all":
+                    self.command_queue.extend(self.resolve_command_recursive(self.current_cmd, hname))
         else:
-            self.command_queue.extend(COMMANDS_DICT[self.command].get_queue(self))
+            self.command_queue.extend(self.resolve_command_recursive(self.current_cmd, self.host.get("hostname")))
 
         self.run_commands()
+
+    def resolve_command_recursive(self, cmd_dict, hostname):
+        queue = []
+        for item in cmd_dict.get("commands", []):
+            if isinstance(item, str):
+                queue.append(self.resolve_placeholders(item, hostname))
+            elif callable(item):
+                queue.extend(item(self.config.get("flake_path")))
+            elif isinstance(item, dict):
+                queue.extend(self.resolve_command_recursive(item, hostname))
+        return queue
+
+    def resolve_placeholders(self, cmd_str, hostname):
+        flake_path = self.config.get("flake_path", "")
+        user = self.config.get("user", "")
+        host_url = self.config["hosts"].get(hostname, "") if hostname else ""
+        replacements = {"<FLAKEPATH>": flake_path, "<HOSTNAME>": hostname or "", "<USER>": user, "<HOSTURL>": host_url}
+        for k, v in self.selected_vars.items(): replacements[f"<{k}>"] = v
+        for k, v in replacements.items(): cmd_str = cmd_str.replace(k, str(v))
+        return cmd_str
 
     def run_commands(self):
         self.content_switcher.current = "command-runner"
@@ -187,9 +198,8 @@ class NixOSManager(App):
 
     @on(Button.Pressed, "#return")
     def reset(self, event: Button.Pressed):
-        self.command = ""
-        self.rebuild_action = ""
-        self.installer_command = ""
+        self.current_cmd = {}
+        self.selected_vars = {}
         self.host = {
             "hostname": "",
             "host_url": ""
