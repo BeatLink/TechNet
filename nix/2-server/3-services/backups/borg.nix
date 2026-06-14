7708698a6e17d5b8ff09e647594e7114e67f8c03
BeatLink <git@beatlink.simplelogin.com>
2026-06-14 13:09:59 -0500

    Fix borg backups

--- nix/2-server/3-services/backups/borg.nix
+++ nix/2-server/3-services/backups/borg.nix
@@ -10,14 +10,20 @@
                 "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFERN9fyw16t2LvfrrZdO1CpY5ZWEolg2bY1ZF4WF2SU odin-borg-key"
             ];
             path = "/Storage/Files/Backups/Laptop/Vorta";
-            group = "beatlink";
+            group = "borg";
         };
         laptop-borgmatic = {
             authorizedKeys = [
                 "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhnn5URZ/2Dx4yFz4E2vfKhQGViGSDRgSixehg+wUXj odin-borg-repo-borgmatic"
             ];
             path = "/Storage/Files/Backups/Laptop/Borgmatic";
-            group = "beatlink";
+            group = "borg";
         };
     };
+
+    systemd.tmpfiles.rules = [
+        "Z /Storage/Files/Backups/Laptop/Vorta 0750 borg borg - -"
+        "Z /Storage/Files/Backups/Laptop/Borgmatic 0750 borg borg - -"
+    ];
+
 }
