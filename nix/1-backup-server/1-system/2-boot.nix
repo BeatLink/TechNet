86f92f13d14444505a2b528a7a5e5826e06005db
BeatLink <git@beatlink.simplelogin.com>
2026-06-28 15:45:11 -0500

    Fix ragnarok

--- nix/1-backup-server/1-system/2-boot.nix
+++ nix/1-backup-server/1-system/2-boot.nix
@@ -6,6 +6,7 @@
 {
     boot = {
         loader.efi.canTouchEfiVariables = false;                            # Leaves Tow-Boot alone
+        initrd.kernelModules = [ "simpledrm" ];                             # Use Tow-Boot framebuffer for early console output
         kernelParams =  [
             "console=tty1"
             "console=ttyS2,115200n8"
