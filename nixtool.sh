6e68aa2f3342c90b82f87ce6048dea7de0503274
BeatLink <git@beatlink.simplelogin.com>
2026-07-12 09:49:30 -0500

    Update Flake

--- nixtool.sh
+++ nixtool.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Path to the nixtool-config.json file as specified
-CONFIG_PATH="/Storage/Files/Projects/TechNet/Repository/nixtool-config.json"
+CONFIG_PATH="/Storage/Files/Projects/TechNet/nixtool-config.json"
 
 # Run the tool via Nix. Replace <GITHUB_URL> with your actual repo URL.
 nix run github:BeatLink/NixTool --no-write-lock-file --refresh -- --config "$CONFIG_PATH" "$@"
