3cbb6aeb76f90cdd30aef591a546e51dfd37cb77
BeatLink <git@beatlink.simplelogin.com>
2026-07-28 23:58:52 -0500

    fix tang

--- nix/3-laptop/1-system/18-tang.nix
+++ nix/3-laptop/1-system/18-tang.nix
@@ -1,7 +1,9 @@
+{ inputs, ... }:
 {
     technet.tang.server = {
         enable = true;
         persistenceRoot = "/Storage/System/Tang";
+        sopsFile = "${inputs.self}/secrets/3-laptop/tang.yaml";
         screensaver = {
             dbusName = "org.cinnamon.ScreenSaver";
             objectPath = "/org/cinnamon/ScreenSaver";
