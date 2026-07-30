c1d56be4a7845826d45c8d2516f62acc083014a0
BeatLink <git@beatlink.simplelogin.com>
2026-07-29 19:46:21 -0500

    update homemanager package management

--- nix/3-laptop/4-apps/tools/ventoy.nix
+++ nix/3-laptop/4-apps/tools/ventoy.nix
@@ -6,9 +6,6 @@
     home-manager.users.beatlink =
         { pkgs, ... }:
         {
-            nixpkgs.config.permittedInsecurePackages = [
-                "ventoy-gtk3-1.1.05"
-            ];
             home = {
                 packages = [ pkgs.ventoy-full-gtk ];
                 persistence."/Storage/Apps/Tools/Ventoy" = {
