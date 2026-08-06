3df6811ec594b9d1e3401ec899cf4e337a05ce87
BeatLink <git@beatlink.simplelogin.com>
2026-08-06 14:43:51 -0500

    Add Image Viewer for Thor

--- nix/5-phone/3-apps/media/images.nix
+++ nix/5-phone/3-apps/media/images.nix
@@ -1,4 +1,8 @@
+# XViewer ##############################
 { pkgs, ... }:
 {
-    environment.systemPackages = [ pkgs.loupe ];
+    home-manager.users.beatlink.home = {
+        packages = [ pkgs.xviewer ];
+        persistence."/Storage/Apps/Media/XViewer".directories = [ ".config/xviewer" ];
+    };
 }
