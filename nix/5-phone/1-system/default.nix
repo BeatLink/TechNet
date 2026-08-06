41046d6a483ce0a51f4d2bf41182b3f20a618ee5
BeatLink <git@beatlink.simplelogin.com>
2026-08-06 07:00:13 -0500

    Delete Waydroid

--- nix/5-phone/1-system/default.nix
+++ nix/5-phone/1-system/default.nix
@@ -14,7 +14,6 @@
         ./13-kernel.nix
         ./14-phosh-bump.nix
         ./15-data-drive.nix
-        ./16-waydroid.nix
         ./17-mobile-data.nix
         ./18-performance.nix
         ./19-keyboard-layout
