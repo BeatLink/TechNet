00ec99adf46e948618ae9e9032fa70373cbf644e
BeatLink <git@beatlink.simplelogin.com>
2026-08-08 21:32:22 -0500

    Update Nix Flake

--- nix/3-laptop/1-system/2-boot.nix
+++ nix/3-laptop/1-system/2-boot.nix
@@ -3,6 +3,7 @@
 # This section manages misc boot settings
 #
 
+{ lib, ... }:
 {
-    boot.loader.efi.canTouchEfiVariables = true;                            # Allows setting boot order, UEFI settings, etc
+    boot.loader.efi.canTouchEfiVariables = lib.mkForce true;                # Allows setting boot order, UEFI settings, etc. Forced over 0-common's false
 }
