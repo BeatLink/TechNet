0eef543bb46d4f0c983be2fd6f316fdb88660e0b
BeatLink <git@beatlink.simplelogin.com>
2026-08-09 19:25:59 -0500

    Add plymouth to all hosts

--- nix/0-common/1-system/2-boot.nix
+++ nix/0-common/1-system/2-boot.nix
@@ -1,11 +1,17 @@
-# Boot Loader ########################################################################################################################################
+# Boot ###############################################################################################################################################
+#
+# The boot loader plus the Plymouth splash and the quiet-boot settings that keep the kernel log off the screen behind it.
+#
 
-{ lib, ... }:
+{ lib, inputs, ... }:
 {
+    imports = [ inputs.nixos-plymouth.nixosModules.default ];
+
     boot = {
         initrd = {
             enable = true;
             systemd.enable = true;
+            verbose = false;
         };
         loader = {
             efi.canTouchEfiVariables = false; # Tow-Boot on the ARM boards and Heimdall's firmware break if written to; only the laptop overrides this
@@ -16,5 +22,17 @@
             grub.enable = false;
             timeout = 5;
         };
+
+        # Silent Boot ################################################################################################################################
+        consoleLogLevel = 0;
+        kernelParams = [
+            "quiet"
+            "splash"
+            "boot.shell_on_fail"
+            "loglevel=3"
+            "rd.systemd.show_status=false"
+            "rd.udev.log_level=3"
+            "udev.log_priority=3"
+        ];
     };
 }
