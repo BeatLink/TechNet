efbca1320d3be2eba927449f04d1870b358c511d
BeatLink <git@beatlink.simplelogin.com>
2026-08-13 08:51:57 -0500

    Disable syncthing

--- nix/5-phone/3-apps/technet/default.nix
+++ nix/5-phone/3-apps/technet/default.nix
@@ -2,8 +2,8 @@
     imports = [
         ./home-assistant.nix
         ./syncbus.nix
-        ./syncthing.nix
-        ./syncthing-web.nix
+        #./syncthing.nix
+        #./syncthing-web.nix
         ./trilium.nix
     ];
 }
