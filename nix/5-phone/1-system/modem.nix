ae7f8134aa51681cc3509b1f3723cccf8c528011
BeatLink <git@beatlink.simplelogin.com>
2026-08-18 10:58:04 -0500

    feat(phone): package QFirehose for modem firmware flashing
    Taken from mobile-nixos directly, which is the only packaging of it. Drops the long-dead u-boot PWRKEY quirk comment.

--- nix/5-phone/1-system/modem.nix
+++ nix/5-phone/1-system/modem.nix
@@ -1,12 +1,18 @@
-{ lib, ... }:
+# Modem
+#
+# The EG25-G is driven by eg25-manager. QFirehose flashes its firmware and is
+# packaged only by mobile-nixos, so it is taken from that input directly.
+#
 {
-    # Shut down the modem in early boot
-    #mobile.quirks.u-boot.additionalCommands = ''
-        # Properly shut off EG25 by pulling up PWRKEY.
-    #    gpio set 35
-    #    sleep 1
-    #    gpio clear 35
-    #'';
-
+    inputs,
+    lib,
+    pkgs,
+    ...
+}:
+{
     services.eg25-manager.enable = lib.mkDefault true;
+
+    environment.systemPackages = [
+        (pkgs.callPackage "${inputs.mobile-nixos}/devices/pine64-pinephone/overlay/qfirehose" { })
+    ];
 }
