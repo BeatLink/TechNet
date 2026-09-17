441365ad3f636a4e7e79f48c06d8ed00c4b08444
BeatLink <git@beatlink.simplelogin.com>
2026-09-17 17:33:56 -0500

    fix(packet-tracer): persist the pt folder it actually writes to

--- nix/3-laptop/4-apps/tools/packet-tracer.nix
+++ nix/3-laptop/4-apps/tools/packet-tracer.nix
@@ -6,10 +6,7 @@
             home = {
                 packages = with pkgs; [ cisco-packet-tracer_9 ];
                 persistence."/Storage/Apps/Tools/PacketTracer" = {
-                    directories = [
-                        ".config/Cisco"
-                        "Cisco Packet Tracer 9.0.1"
-                    ];
+                    directories = [ "pt" ];
                 };
             };
         };
